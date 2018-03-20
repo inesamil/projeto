c562786cfc8154364271ff381e2f0bb43c0d5b7e
isel42162 <42162@alunos.isel.ipl.pt>
2018-03-20 23:51:44 +0000

    Add views - categories, login, home

--- myapp/app.js
+++ myapp/app.js
@@ -1,15 +1,16 @@
-var express = require('express');
+const express = require('express')
 const hbs = require('hbs')
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index')
+const categories = require('./routes/categories')
+const users = require('./routes/users')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +33,7 @@ app.use( (req, res, next) => {
 
 // routes
 app.use('/', index);
+app.use('/categories', categories);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login', layout: '/layouts/loginLayout'});
+});
+
+
 module.exports = router;
