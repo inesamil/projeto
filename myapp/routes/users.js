23aee80d8a1c39f17acd8d75293795285843a790
isel42162 <42162@alunos.isel.ipl.pt>
2018-03-22 12:42:00 +0000

    Web App - profile page

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+/* GET user with username 'username' */ 
+router.get('/:username', function(req, res, next) {
+  res.render('profile', )
+})
 module.exports = router;
