2604fbf9bc57620c9ebd172e414c64d51f7f5ca9
isel42142 <42142@alunos.isel.ipl.pt>
2018-03-26 17:51:44 +0100

    Add views

--- Web/myapp/routes/products.js
+++ Web/myapp/routes/products.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET Lists */
+/* GET product name */
 router.get('/:name', function(req, res, next) {
     const context = {
         product: req.params.name,
@@ -41,5 +41,65 @@ router.get('/:name', function(req, res, next) {
           ]
     }
     res.render('products', context)
+})
+
+/* GET product name specify*/
+router.get('/:name/:specifyproduct', function(req, res, next) {
+    const context = {
+        product: req.params.name,
+        productName: req.params.specifyproduct,
+        units: 5,
+        quantity: '1L',
+        allergens: {
+            title: 'Allergens',
+            pdtAllergens: [
+                {
+                    allergen:'milk'
+                },
+                {
+                    allergen:'milk protein'
+                }
+            ]
+        },
+        expirationDate: {
+            title: 'Expiration Date',
+            pdtexpdate: [
+                {
+                    units: 2,
+                    text: 'units expire at',
+                    date:'23/03/2018'
+                },
+                {
+                    units: 1,
+                    text: 'units expire at',
+                    date:'27/03/2018'
+                }
+            ]
+        },
+        storages: {
+            title: 'Storage',
+            pdtstorages: [
+                {
+                    units: 2,
+                    text: 'units in the',
+                    local:'fridge'
+                },
+                {
+                    units: 1,
+                    text: 'units in the',
+                    date:'cabinet1'
+                }
+            ]
+        },
+        description: {
+            title: 'Description',
+            text: 'milk é bue bom, têm de experimentar!!!'
+        },
+        movements: {
+            title: 'Movements',
+            text: 'saída do frigorifico dia 13/03/2018 ás 18h00'
+        }
+    }
+    res.render('product', context)
 })
 module.exports = router;
