aae8d61ed14235ac9015da80658ad6a47b0156d1
nveloso <42181@alunos.isel.ipl.pt>
2018-03-28 16:55:50 +0100

    Add starter setup API project

---
+++ API/gis/src/main/java/pt/isel/ps/gis/GisApplication.java
@@ -0,0 +1,12 @@
+package pt.isel.ps.gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GisApplication.class, args);
+	}
+}
