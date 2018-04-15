73d6aa94e236b2561e5f498c3ea36281106cfbb9
nveloso <42181@alunos.isel.ipl.pt>
2018-04-16 00:41:28 +0100

    Add numrange user type and dialect

--- API/gis/src/main/java/pt/isel/ps/gis/model/numrangeType/NumrangePostgresDialect.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/numrangeType/NumrangePostgresDialect.java
@@ -1,4 +1,13 @@
 package pt.isel.ps.gis.model.numrangeType;
 
-public class NumrangePostgresDialect {
+import org.hibernate.dialect.PostgreSQL95Dialect;
+
+import java.sql.Types;
+
+public class NumrangePostgresDialect extends PostgreSQL95Dialect {
+
+    public NumrangePostgresDialect() {
+        super();
+        registerColumnType(Types.JAVA_OBJECT, "numrange");
+    }
 }
--- API/gis/src/main/java/pt/isel/ps/gis/model/numrangeType/NumrangeUserType.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/numrangeType/NumrangeUserType.java
@@ -1,4 +1,96 @@
 package pt.isel.ps.gis.model.numrangeType;
 
-public class NumrangeUserType {
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.usertype.UserType;
+import pt.isel.ps.gis.model.Numrange;
+
+import java.io.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class NumrangeUserType implements UserType {
+
+    @Override
+    public int[] sqlTypes() {
+        return new int[]{Types.JAVA_OBJECT};
+    }
+
+    @Override
+    public Class<Numrange> returnedClass() {
+        return Numrange.class;
+    }
+
+    @Override
+    public boolean equals(Object x, Object y) throws HibernateException {
+        if (x == null)
+            return y == null;
+        return x.equals(y);
+    }
+
+    @Override
+    public int hashCode(Object x) throws HibernateException {
+        return x.hashCode();
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner) throws HibernateException, SQLException {
+        final String cellContent = rs.getString(names[0]);
+        if (cellContent == null) {
+            return null;
+        }
+        String[] split = cellContent.split("[\\[,\\]]");
+        try {
+            float minimum = Float.parseFloat(split[1]);
+            float maximum = Float.parseFloat(split[2]);
+            return new Numrange(minimum, maximum);
+        } catch (NumberFormatException ex) {
+            throw new RuntimeException("Failed to parse String to Number: " + ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session) throws HibernateException, SQLException {
+        // TODO
+    }
+
+    @Override
+    public Object deepCopy(Object value) throws HibernateException {
+        try {
+            // use serialization to create a deep copy
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(value);
+            oos.flush();
+            oos.close();
+            bos.close();
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bos.toByteArray());
+            return new ObjectInputStream(bais).readObject();
+        } catch (ClassNotFoundException | IOException ex) {
+            throw new HibernateException(ex);
+        }
+    }
+
+    @Override
+    public boolean isMutable() {
+        return true;
+    }
+
+    @Override
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (Serializable) this.deepCopy(value);
+    }
+
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return this.deepCopy(cached);
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return this.deepCopy(original);
+    }
 }
