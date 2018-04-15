23f3cfdaef164231e060b29a990d4fd783b0848f
nveloso <42181@alunos.isel.ipl.pt>
2018-04-15 19:12:43 +0100

    Add query to category repository

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/CategoryRepository.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/CategoryRepository.java
@@ -1,12 +1,17 @@
 package pt.isel.ps.gis.dal.repositories;
 
-import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.scheduling.annotation.Async;
 import pt.isel.ps.gis.model.Category;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
 
-    @Procedure(procedureName = "get_categories_by_name")
-    Category functionGetCategoriesByName(@Param("name") String name);
+    @Async
+    @Query(value = "SELECT * FROM public.\"category\" WHERE category_name LIKE :name || '%'", nativeQuery = true)
+    CompletableFuture<Stream<Category>> getCategoriesByName(@Param("name") String name);
 }
