923a84e0610947d0d2471065b37b8781a97bb739
nveloso <42181@alunos.isel.ipl.pt>
2018-04-16 23:37:27 +0100

    Update product repository

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/ProductRepository.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/ProductRepository.java
@@ -2,16 +2,13 @@ package pt.isel.ps.gis.dal.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.scheduling.annotation.Async;
 import pt.isel.ps.gis.model.Product;
 import pt.isel.ps.gis.model.ProductId;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Stream;
+import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, ProductId> {
 
-    @Async
     @Query(value = "SELECT * FROM public.\"product\" WHERE product_name LIKE :name || '%';", nativeQuery = true)
-    CompletableFuture<Stream<Product>> getProductsByName(String name);
+    List<Product> getProductsByName(String name);
 }
