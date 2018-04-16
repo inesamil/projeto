b421a743eb27440ecb30d26ea7b15fec8cf5877c
isel42162 <42162@alunos.isel.ipl.pt>
2018-04-16 18:43:02 +0100

    Add UserServiceImpl

--- API/gis/src/main/java/pt/isel/ps/gis/bll/UserServiceImpl.java
+++ API/gis/src/main/java/pt/isel/ps/gis/bll/UserServiceImpl.java
@@ -1,4 +1,51 @@
 package pt.isel.ps.gis.bll;
 
-public class UserServiceImpl {
+import org.springframework.stereotype.Service;
+import pt.isel.ps.gis.dal.repositories.UsersRepository;
+import pt.isel.ps.gis.exceptions.EntityException;
+import pt.isel.ps.gis.exceptions.EntityNotFoundException;
+import pt.isel.ps.gis.model.Users;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UsersRepository usersRepository;
+
+    public UserServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public boolean existsUserByUserId(String username) {
+        return usersRepository.existsById(username);
+    }
+
+    @Override
+    public Optional<Users> getUserByUserId(String username) {
+        return usersRepository.findById(username);
+    }
+
+    @Override
+    public Users addUser(Users user) throws EntityException {
+        if (existsUserByUserId(user.getUsersUsername())){
+            throw new EntityException(String.format("Username %s is already in use.", user.getUsersUsername()));
+        }
+        return usersRepository.save(user);
+    }
+
+    @Override
+    public Users updateUser(Users user) throws EntityNotFoundException {
+        if (!existsUserByUserId(user.getUsersUsername()))
+            throw new EntityNotFoundException(String.format("User with username %s does not exist.", user.getUsersUsername()));
+        return usersRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(String username) throws EntityNotFoundException {
+        if (!existsUserByUserId(username))
+            throw new EntityNotFoundException(String.format("User with username %s does not exist.", username));
+        usersRepository.deleteById(username);   //TODO: delete
+    }
 }
