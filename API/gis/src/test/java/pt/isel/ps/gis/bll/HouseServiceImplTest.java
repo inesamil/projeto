1495f5d3ed74a295add928676f8481ce3514f5a4
isel42162 <42162@alunos.isel.ipl.pt>
2018-04-16 18:44:21 +0100

    Add HouseServiceImplTest

--- API/gis/src/test/java/pt/isel/ps/gis/bll/HouseServiceImplTest.java
+++ API/gis/src/test/java/pt/isel/ps/gis/bll/HouseServiceImplTest.java
@@ -1,32 +1,52 @@
 package pt.isel.ps.gis.bll;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pt.isel.ps.gis.AppConfig;
+import pt.isel.ps.gis.dal.repositories.HouseRepository;
+import pt.isel.ps.gis.dal.repositories.Repository;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class HouseServiceImplTest {
 
-    @Test
-    public void existsHouseByHouseId() {
+    @Autowired
+    private HouseService houseService;
+
+    @Before
+    public void initialize() {
+        System.out.println(houseService.toString());
     }
 
     @Test
-    public void getHouseByHouseId() {
+    public void existsHouseByHouseIdTest() {
+        Long houseId = 1L;
+        assertFalse(houseService.existsHouseByHouseId(houseId));
     }
 
     @Test
-    public void getHousesByUserId() {
+    public void getHouseByHouseIdTest() {
     }
 
     @Test
-    public void addHouse() {
+    public void getHousesByUserIdTest() {
     }
 
     @Test
-    public void updateHouse() {
+    public void addHouseTest() {
     }
 
     @Test
-    public void deleteHouse() {
+    public void updateHouseTest() {
+    }
+
+    @Test
+    public void deleteHouseTest() {
     }
 }
