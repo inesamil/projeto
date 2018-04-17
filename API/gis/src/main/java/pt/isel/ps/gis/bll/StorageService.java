f4ef73065c4c9b1ebf1ac74c1b65edbd36223298
isel42162 <42162@alunos.isel.ipl.pt>
2018-04-17 14:26:01 +0100

    Add StorageServiceImpl

--- API/gis/src/main/java/pt/isel/ps/gis/bll/StorageService.java
+++ API/gis/src/main/java/pt/isel/ps/gis/bll/StorageService.java
@@ -1,11 +1,21 @@
 package pt.isel.ps.gis.bll;
 
+import pt.isel.ps.gis.exceptions.EntityException;
+import pt.isel.ps.gis.exceptions.EntityNotFoundException;
 import pt.isel.ps.gis.model.Storage;
 import pt.isel.ps.gis.model.StorageId;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface StorageService {
+
+    /**
+     * Verifica se um dado local de armazenamento existe através do seu ID
+     *
+     * @param storageId identificador do local de armazenamento
+     * @return true se o local de armazenamento existir, false caso contrário
+     */
+    boolean existsStorageByStorageId(StorageId storageId);
 
     /**
      * Obter um local de armazenamento através do seu ID
@@ -13,7 +23,7 @@ public interface StorageService {
      * @param storageId identificador do local de armazenamento
      * @return Storage
      */
-    Storage getStorageByStorageId(StorageId storageId);
+    Optional<Storage> getStorageByStorageId(StorageId storageId);
 
     /**
      * Listar todos os locais de armazenamento duma casa através do ID da casa
@@ -21,7 +31,7 @@ public interface StorageService {
      * @param houseId identificador da casa
      * @return List<Storage>
      */
-    List<Storage> getStorageByHouseId(Long houseId);
+    Iterable<Storage> getStorageByHouseId(Long houseId);
 
     /**
      * Adicionar um local de armazenamento numa casa
@@ -29,7 +39,7 @@ public interface StorageService {
      * @param storage local de armazenamento a adicionar
      * @return Storage
      */
-    Storage addStorage(Storage storage);
+    Storage addStorage(Storage storage) throws EntityException;
 
     /**
      * Atualizar um local de armazenamento duma casa
@@ -37,12 +47,12 @@ public interface StorageService {
      * @param storage local de armazenamento atualizado
      * @return Storage
      */
-    Storage updateStorage(Storage storage);
+    Storage updateStorage(Storage storage) throws EntityNotFoundException;
 
     /**
      * Remover um local de armazenamento duma casa
      *
      * @param storageId identifcador do local de armazenamento
      */
-    void deleteStorage(StorageId storageId);
+    void deleteStorage(StorageId storageId) throws EntityNotFoundException;
 }
---
+++ API/gis/src/main/java/pt/isel/ps/gis/bll/StorageServiceImpl.java
@@ -0,0 +1,60 @@
+package pt.isel.ps.gis.bll;
+
+import pt.isel.ps.gis.dal.repositories.StorageRepository;
+import pt.isel.ps.gis.exceptions.EntityException;
+import pt.isel.ps.gis.exceptions.EntityNotFoundException;
+import pt.isel.ps.gis.model.Storage;
+import pt.isel.ps.gis.model.StorageId;
+
+import java.util.Optional;
+
+public class StorageServiceImpl implements StorageService{
+
+    private final StorageRepository storageRepository;
+
+    public StorageServiceImpl(StorageRepository storageRepository) {
+        this.storageRepository = storageRepository;
+    }
+
+    @Override
+    public boolean existsStorageByStorageId(StorageId storageId) {
+        return storageRepository.existsById(storageId);
+    }
+
+    @Override
+    public Optional<Storage> getStorageByStorageId(StorageId storageId) {
+        return storageRepository.findById(storageId);
+    }
+
+    @Override
+    public Iterable<Storage> getStorageByHouseId(Long houseId) {
+        return storageRepository.findAllById_HouseId(houseId);
+    }
+
+    @Override
+    public Storage addStorage(Storage storage) throws EntityException {
+        Storage newStorage = storage;
+        if (storage.getId() != null) {
+            // É preciso garantir que storageId está a NULL, para ser feita inserção do novo local de armazenamento.
+            // Caso contrário, poderia ser atualizado um local de armazenamento já existente.
+            newStorage = new Storage(storage.getStorageName(), storage.getStorageTemperature());
+        }
+        return storageRepository.save(newStorage);
+    }
+
+    @Override
+    public Storage updateStorage(Storage storage) throws EntityNotFoundException {
+        if (!existsStorageByStorageId(storage.getId()))
+            throw new EntityNotFoundException(String.format("Storage with ID {%d, %d} does not exist.",
+                    storage.getId().getHouseId(), storage.getId().getStorageId()));
+        return storageRepository.save(storage);
+    }
+
+    @Override
+    public void deleteStorage(StorageId storageId) throws EntityNotFoundException {
+        if (!existsStorageByStorageId(storageId))
+            throw new EntityNotFoundException(String.format("Storage with ID {%d, %d} does not exist.",
+                    storageId.getHouseId(), storageId.getStorageId()));
+        storageRepository.deleteById(storageId);
+    }
+}
