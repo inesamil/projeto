0bcaddba8d8d4ade08c8f86286a50ec33d725afb
nveloso <42181@alunos.isel.ipl.pt>
2018-04-18 20:26:42 +0100

    Simple test

--- API/gis/src/main/java/pt/isel/ps/gis/S.java
+++ API/gis/src/main/java/pt/isel/ps/gis/S.java
@@ -3,21 +3,21 @@ package pt.isel.ps.gis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pt.isel.ps.gis.dal.repositories.ProductRepository;
-import pt.isel.ps.gis.dal.repositories.StockItemRepository;
+import pt.isel.ps.gis.dal.repositories.ListRepository;
 import pt.isel.ps.gis.exceptions.EntityException;
-import pt.isel.ps.gis.model.Product;
-import pt.isel.ps.gis.model.StockItem;
+import pt.isel.ps.gis.model.List;
 
 @RestController
 public class S {
 
     @Autowired
-    private ProductRepository repository;
+    private ListRepository repository;
 
     @GetMapping("")
     public void g() throws EntityException {
-        Product product = new Product(7, "Produto A", true, (short) 3, "week");
-        repository.insertProduct(product);
+        java.util.List<List> listsFiltered = repository.getListsFiltered(1L, false, "nuno", false);
+        for (List l : listsFiltered) {
+            System.out.println(l);
+        }
     }
 }
