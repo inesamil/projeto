dffe9f70a72edb536ecf511e2f3e7f2ea48edffa
nveloso <42181@alunos.isel.ipl.pt>
2018-04-18 21:49:57 +0100

    Update list repository custom impl - close result set

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/custom/ListRepositoryCustomImpl.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/custom/ListRepositoryCustomImpl.java
@@ -10,6 +10,7 @@ import javax.persistence.PersistenceContext;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 
 public class ListRepositoryCustomImpl implements ListRepositoryCustom {
@@ -33,28 +34,43 @@ public class ListRepositoryCustomImpl implements ListRepositoryCustom {
                     "FROM public.\"list\" JOIN public.\"userlist\" ON (public.\"list\".house_id = public.\"userlist\".house_id AND public.\"list\".list_id = public.\"userlist\".list_id) " +
                     "WHERE public.\"list\".house_id = ? AND list_type = 'user' AND list_shareable = ?;";
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                ps.setLong(1, houseId);
-                ps.setBoolean(2, system);
-                ps.setLong(3, houseId);
-                ps.setString(4, username);
-                ps.setLong(5, houseId);
-                ps.setBoolean(6, shared);
-                ResultSet resultSet = ps.executeQuery();
-                java.util.List<List> lists = new ArrayList<>();
-                while (resultSet.next()) {
-                    long house_id = resultSet.getLong(1);
-                    short list_id = resultSet.getShort(2);
-                    String list_name = resultSet.getString(3);
-                    String list_type = resultSet.getString(4);
-                    try {
-                        List list = new List(house_id, list_id, list_name, list_type);
-                        lists.add(list);
-                    } catch (EntityException e) {
-                        throw new SQLException(e.getMessage());
+                if (isNotNull(ps, 1, houseId))
+                    ps.setLong(1, houseId);
+                if (isNotNull(ps, 2, system))
+                    ps.setBoolean(2, system);
+                if (isNotNull(ps, 3, houseId))
+                    ps.setLong(3, houseId);
+                if (isNotNull(ps, 4, username))
+                    ps.setString(4, username);
+                if (isNotNull(ps, 5, houseId))
+                    ps.setLong(5, houseId);
+                if (isNotNull(ps, 6, shared))
+                    ps.setBoolean(6, shared);
+                try (ResultSet resultSet = ps.executeQuery()) {
+                    java.util.List<List> lists = new ArrayList<>();
+                    while (resultSet.next()) {
+                        long house_id = resultSet.getLong(1);
+                        short list_id = resultSet.getShort(2);
+                        String list_name = resultSet.getString(3);
+                        String list_type = resultSet.getString(4);
+                        try {
+                            List list = new List(house_id, list_id, list_name, list_type);
+                            lists.add(list);
+                        } catch (EntityException e) {
+                            throw new SQLException(e.getMessage());
+                        }
                     }
+                    return lists;
                 }
-                return lists;
             }
         });
+    }
+
+    private <T> boolean isNotNull(PreparedStatement ps, int idx, T t) throws SQLException {
+        if (t == null) {
+            ps.setNull(idx, Types.OTHER);
+            return false;
+        }
+        return true;
     }
 }
