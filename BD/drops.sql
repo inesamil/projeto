1b2bb8f4badc63531f0c9d5c5e97e864fa0b904c
isel42162 <42162@alunos.isel.ipl.pt>
2018-04-19 15:08:26 +0100

    Update drops.sql

--- BD/drops.sql
+++ BD/drops.sql
@@ -1,3 +1,19 @@
+-- DROP functions
+DROP FUNCTION IF EXISTS delete_house;
+DROP FUNCTION IF EXISTS delete_user_list;
+DROP FUNCTION IF EXISTS delete_stock_item;
+DROP FUNCTION IF EXISTS delete_storage;
+DROP FUNCTION IF EXISTS insert_user_list;
+DROP FUNCTION IF EXISTS insert_system_list;
+DROP FUNCTION IF EXISTS insert_product;
+DROP FUNCTION IF EXISTS insert_stock_item;
+DROP FUNCTION IF EXISTS generate_sku;
+DROP FUNCTION IF EXISTS insert_storage;
+DROP FUNCTION IF EXISTS get_lists_filtered;
+DROP FUNCTION IF EXISTS get_stock_items_filtered;
+DROP FUNCTION IF EXISTS get_movements_filtered;
+
+-- DROP tables
 DROP TABLE IF EXISTS public."expirationdate";
 DROP TABLE IF EXISTS public."date";
 DROP TABLE IF EXISTS public."stockitemallergy";
@@ -21,3 +37,6 @@ DROP TABLE IF EXISTS public."recipe";
 DROP TABLE IF EXISTS public."allergy";
 DROP TABLE IF EXISTS public."users";
 DROP TABLE IF EXISTS public."house";
+
+-- DROP schema
+DROP SCHEMA IF EXISTS public;
