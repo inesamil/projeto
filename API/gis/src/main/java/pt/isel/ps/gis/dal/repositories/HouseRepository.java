d5f7fd17636f623eeeb25183594a6ef422b2a218
nveloso <42181@alunos.isel.ipl.pt>
2018-04-20 00:23:33 +0100

    Update house repository

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/HouseRepository.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/HouseRepository.java
@@ -1,13 +1,20 @@
 package pt.isel.ps.gis.dal.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pt.isel.ps.gis.model.House;
 
+import java.util.List;
+
 @Repository
 public interface HouseRepository extends CrudRepository<House, Long> {
-
-    //TODO: findAllByUsersUsername(String username) // Retorna todas as casas do utilizador com username = @username
+    
+    @Query(value = "SELECT public.\"house\".house_id, public.\"house\".house_name, public.\"house\".house_characteristics " +
+            "FROM public.\"house\" JOIN public.\"userhouse\" ON public.\"house\".house_id = public.\"userhouse\".house_id " +
+            "WHERE public.\"userhouse\".users_username = :username", nativeQuery = true)
+    List<House> findAllByUsersUsername(@Param("username") String username);
 
     //TODO: deleteHouseById(Long houseId) // Apaga a casa com ID = @houseId e todo o conteúdo associado à casa
 }
