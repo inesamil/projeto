9690ffeab968436d8a046f92be34dd415c0f6988
nveloso <42181@alunos.isel.ipl.pt>
2018-04-20 19:09:28 +0100

    Add access to delete user function in java

---
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/UsersRepositoryCustom.java
@@ -0,0 +1,6 @@
+package pt.isel.ps.gis.dal.repositories;
+
+public interface UsersRepositoryCustom {
+
+    void deleteUserById(String username);
+}
---
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/custom/UsersRepositoryCustomImpl.java
@@ -0,0 +1,28 @@
+package pt.isel.ps.gis.dal.repositories.custom;
+
+import org.hibernate.Session;
+import pt.isel.ps.gis.dal.repositories.UsersRepositoryCustom;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.sql.CallableStatement;
+
+public class UsersRepositoryCustomImpl implements UsersRepositoryCustom {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void deleteUserById(String username) {
+        // TODO: testar
+        Session session = entityManager.unwrap(Session.class);
+        session.doWork(connection -> {
+            try (CallableStatement function = connection.prepareCall(
+                    "{call delete_user(?)}"
+            )) {
+                function.setString(1, username);
+                function.execute();
+            }
+        });
+    }
+}
