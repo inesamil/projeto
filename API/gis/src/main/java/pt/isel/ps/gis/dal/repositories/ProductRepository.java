dfb28d787e302fa2282ba898c9b782d7feb7d5a4
nveloso <42181@alunos.isel.ipl.pt>
2018-04-20 20:11:58 +0100

    Update product repository

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/ProductRepository.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package pt.isel.ps.gis.dal.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import pt.isel.ps.gis.model.Product;
 import pt.isel.ps.gis.model.ProductId;
 
@@ -10,5 +11,13 @@ import java.util.List;
 public interface ProductRepository extends CrudRepository<Product, ProductId>, ProductRepositoryCustom {
 
     @Query(value = "SELECT * FROM public.\"product\" WHERE product_name LIKE :name || '%';", nativeQuery = true)
-    List<Product> getProductsByName(String name);
+    List<Product> findProductsByName(String name);
+
+    List<Product> findAllById_CategoryId(Integer categoryId);
+
+    @Query(value = "SELECT * FROM public.\"product\" JOIN public.\"category\" ON " +
+            "public.\"product\".category_id = public.\"category\".category_id " +
+            "WHERE public.\"category\".category_id = :categoryId AND public.\"product\".product_name LIKE :name || '%';",
+            nativeQuery = true)
+    List<Product> findProductsByNameAndCategoryId(@Param("categoryId") Integer categoryId, @Param("name") String name);
 }
