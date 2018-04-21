82d445890db672aaef78bb14d53243489c6962fb
nveloso <42181@alunos.isel.ipl.pt>
2018-04-21 15:35:02 +0100

    Remove methods from stock item allergy repository

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/StockItemAllergyRepository.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/StockItemAllergyRepository.java
@@ -4,11 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import pt.isel.ps.gis.model.StockItemAllergy;
 import pt.isel.ps.gis.model.StockItemAllergyId;
 
-import java.util.List;
-
 public interface StockItemAllergyRepository extends CrudRepository<StockItemAllergy, StockItemAllergyId> {
-
-    List<StockItemAllergy> findAllById_HouseIdAndId_StockitemSku(Long houseId, String sku);
-
-    List<StockItemAllergy> findAllById_HouseIdAndId_AllergyAllergen(Long houseId, String allergen);
 }
