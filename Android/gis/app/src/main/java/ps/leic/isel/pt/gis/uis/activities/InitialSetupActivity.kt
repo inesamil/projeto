b015d333a5f76494ea36fac8f4e8b383de035974
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-02 18:52:38 +0100

    Update Initial Setup Activity. Refactor: only one setup activity now.

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/InitialSetupActivity.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/InitialSetupActivity.kt
@@ -3,17 +3,17 @@ package ps.leic.isel.pt.gis.uis.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_initial_setup2.*
+import kotlinx.android.synthetic.main.activity_initial_setup.*
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.utils.ExtraUtils
 
-class InitialSetup2Activity : AppCompatActivity() {
+class InitialSetupActivity : AppCompatActivity() {
 
     private lateinit var houseName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initial_setup2)
+        setContentView(R.layout.activity_initial_setup)
 
         houseName = intent.getStringExtra(ExtraUtils.HOUSE_NAME)
 
