2b04e03a5c6a84ac49a75e280caabc9dfa1c4705
nveloso <42181@alunos.isel.ipl.pt>
2018-05-04 22:22:45 +0100

    Update list controller

--- API/gis/src/main/java/pt/isel/ps/gis/controllers/ListController.java
+++ API/gis/src/main/java/pt/isel/ps/gis/controllers/ListController.java
@@ -1,25 +1,64 @@
 package pt.isel.ps.gis.controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pt.isel.ps.gis.bll.ListProductService;
 import pt.isel.ps.gis.bll.ListService;
+import pt.isel.ps.gis.exceptions.EntityException;
+import pt.isel.ps.gis.exceptions.NotFoundException;
+import pt.isel.ps.gis.model.List;
+import pt.isel.ps.gis.model.ListProduct;
+import pt.isel.ps.gis.model.outputModel.ListOutputModel;
 import pt.isel.ps.gis.model.outputModel.ListsOutputModel;
+import pt.isel.ps.gis.model.outputModel.ProductsListOutputModel;
+
+import java.util.Optional;
+
+import static pt.isel.ps.gis.utils.HeadersUtils.setCollectionContentType;
+import static pt.isel.ps.gis.utils.HeadersUtils.setSirenContentType;
 
 @RestController
 @RequestMapping("v1/houses/{house-id}/lists")
 public class ListController {
 
     private final ListService listService;
+    private final ListProductService listProductService;
 
-    public ListController(ListService listService) {
+    public ListController(ListService listService, ListProductService listProductService) {
         this.listService = listService;
+        this.listProductService = listProductService;
     }
 
     @GetMapping("")
     public ResponseEntity<ListsOutputModel> getLists(@PathVariable("house-id") long houseId) {
-        listService.getListsByHouseId()
+        // Qual usar?
+        return null;
+    }
+
+    @GetMapping("/{list-id}")
+    public ResponseEntity<ListOutputModel> getList(
+            @PathVariable("house-id") long houseId,
+            @PathVariable("list-id") short listId
+    ) throws EntityException, NotFoundException {
+        Optional<List> listOptional = listService.getListByListId(houseId, listId);
+        List list = listOptional.orElseThrow(NotFoundException::new);
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity<>(new ListOutputModel(list), setSirenContentType(headers), HttpStatus.OK);
+    }
+
+    @GetMapping("/{list-id}/products")
+    public ResponseEntity<ProductsListOutputModel> getProductsInList(
+            @PathVariable("house-id") long houseId,
+            @PathVariable("list-id") short listId
+    ) throws EntityException {
+        java.util.List<ListProduct> listProducts = listProductService.getListProductsByListId(houseId, listId);
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity<>(new ProductsListOutputModel(houseId, listId, listProducts),
+                setCollectionContentType(headers), HttpStatus.OK);
     }
 }
