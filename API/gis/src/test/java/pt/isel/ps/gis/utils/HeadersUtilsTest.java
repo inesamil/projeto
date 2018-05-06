a10c6af6bd5e7826b54292a0679fafd3d4c9de5a
nveloso <42181@alunos.isel.ipl.pt>
2018-05-06 15:16:15 +0100

    Add HeadersUtilsTest

---
+++ API/gis/src/test/java/pt/isel/ps/gis/utils/HeadersUtilsTest.java
@@ -0,0 +1,40 @@
+package pt.isel.ps.gis.utils;
+
+import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import static org.junit.Assert.assertEquals;
+import static pt.isel.ps.gis.utils.HeadersUtils.*;
+
+public class HeadersUtilsTest {
+
+    private final static String SIREN_MEDIA_TYPE = "application/vnd.siren+json";
+    private final static String JSON_HOME_MEDIA_TYPE = "application/home+json";
+    private final static String COLLECTION_JSON_MEDIA_TYPE = "application/vnd.collection+json";
+
+
+    @Test
+    public void test_set_siren_content_type() {
+        HttpHeaders headers = new HttpHeaders();
+        assertEquals(MediaType.valueOf(SIREN_MEDIA_TYPE), setSirenContentType(headers).getContentType());
+    }
+
+    @Test
+    public void test_set_json_home_content_type() {
+        HttpHeaders headers = new HttpHeaders();
+        assertEquals(MediaType.valueOf(JSON_HOME_MEDIA_TYPE), setJsonHomeContentType(headers).getContentType());
+    }
+
+    @Test
+    public void test_set_collection_content_type() {
+        HttpHeaders headers = new HttpHeaders();
+        assertEquals(MediaType.valueOf(COLLECTION_JSON_MEDIA_TYPE), setCollectionContentType(headers).getContentType());
+    }
+
+    @Test
+    public void test_set_problem_detail_content_type() {
+        HttpHeaders headers = new HttpHeaders();
+        assertEquals(MediaType.APPLICATION_PROBLEM_JSON, setProblemDetailContentType(headers).getContentType());
+    }
+}
