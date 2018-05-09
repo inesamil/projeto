0e967c88ded47bb59a53a69e007f6dcd0c22088b
nveloso <42181@alunos.isel.ipl.pt>
2018-05-09 16:56:36 +0100

    Update list repository custom impl

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/custom/ListRepositoryCustomImpl.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/custom/ListRepositoryCustomImpl.java
@@ -4,14 +4,14 @@ import org.hibernate.Session;
 import pt.isel.ps.gis.dal.repositories.ListRepositoryCustom;
 import pt.isel.ps.gis.exceptions.EntityException;
 import pt.isel.ps.gis.model.List;
+import pt.isel.ps.gis.model.SystemList;
+import pt.isel.ps.gis.model.UserList;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class ListRepositoryCustomImpl implements ListRepositoryCustom {
 
@@ -22,48 +22,87 @@ public class ListRepositoryCustomImpl implements ListRepositoryCustom {
     public java.util.List<List> findListsFiltered(final Long houseId, final Boolean system, final String username, final Boolean shared) {
         Session session = entityManager.unwrap(Session.class);
         return session.doReturningWork(connection -> {
-            String sql = "SELECT public.\"list\".house_id, public.\"list\".list_id, public.\"list\".list_name, public.\"list\".list_type " +
-                    "FROM public.\"list\" " +
-                    "WHERE public.\"list\".house_id = ? AND list_type = CASE WHEN ? = true THEN 'system' ELSE null END " +
-                    "UNION " +
-                    "SELECT public.\"list\".house_id, public.\"list\".list_id, public.\"list\".list_name, public.\"list\".list_type " +
-                    "FROM public.\"list\" JOIN public.\"userlist\" ON (public.\"list\".house_id = public.\"userlist\".house_id AND public.\"list\".list_id = public.\"userlist\".list_id) " +
-                    "WHERE public.\"list\".house_id = ? AND list_type = 'user' AND users_username = ? " +
-                    "UNION " +
-                    "SELECT public.\"list\".house_id, public.\"list\".list_id, public.\"list\".list_name, public.\"list\".list_type " +
-                    "FROM public.\"list\" JOIN public.\"userlist\" ON (public.\"list\".house_id = public.\"userlist\".house_id AND public.\"list\".list_id = public.\"userlist\".list_id) " +
-                    "WHERE public.\"list\".house_id = ? AND list_type = 'user' AND list_shareable = (CASE WHEN ? = true THEN true ELSE null END);";
-            try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                if (isNotNull(ps, 1, houseId))
-                    ps.setLong(1, houseId);
-                if (isNotNull(ps, 2, system))
-                    ps.setBoolean(2, system);
-                if (isNotNull(ps, 3, houseId))
-                    ps.setLong(3, houseId);
-                if (isNotNull(ps, 4, username))
-                    ps.setString(4, username);
-                if (isNotNull(ps, 5, houseId))
-                    ps.setLong(5, houseId);
-                if (isNotNull(ps, 6, shared))
-                    ps.setBoolean(6, shared);
-                try (ResultSet resultSet = ps.executeQuery()) {
-                    java.util.List<List> lists = new ArrayList<>();
-                    while (resultSet.next()) {
-                        long house_id = resultSet.getLong(1);
-                        short list_id = resultSet.getShort(2);
-                        String list_name = resultSet.getString(3);
-                        String list_type = resultSet.getString(4);
-                        try {
-                            List list = new List(house_id, list_id, list_name, list_type);
-                            lists.add(list);
-                        } catch (EntityException e) {
-                            throw new SQLException(e.getMessage());
-                        }
-                    }
-                    return lists;
-                }
+            try {
+                java.util.List<List> systemListsFiltered = findSystemListsFiltered(connection, houseId, system);
+                java.util.List<List> userListsFiltered = findUserListsFiltered(connection, houseId, username, shared);
+                java.util.List<List> lists = new ArrayList<>();
+                Stream
+                        .of(systemListsFiltered, userListsFiltered)
+                        .forEach(lists::addAll);
+                return lists;
+            } catch (EntityException e) {
+                throw new SQLException(e.getMessage());
             }
         });
+    }
+
+    private java.util.List<List> findSystemListsFiltered(
+            final Connection connection,
+            final Long houseId,
+            final Boolean system
+    ) throws SQLException, EntityException {
+        String sql = "SELECT public.\"list\".house_id, public.\"list\".list_id, public.\"list\".list_name, " +
+                "public.\"list\".list_type FROM public.\"list\" WHERE public.\"list\".house_id = ? AND " +
+                "list_type = CASE WHEN ? = true THEN 'system' ELSE null END;";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            if (isNotNull(ps, 1, houseId))
+                ps.setLong(1, houseId);
+            if (isNotNull(ps, 2, system))
+                ps.setBoolean(2, system);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                java.util.List<List> systemLists = new ArrayList<>();
+                while (resultSet.next()) {
+                    long house_id = resultSet.getLong(1);
+                    short list_id = resultSet.getShort(2);
+                    String list_name = resultSet.getString(3);
+                    String list_type = resultSet.getString(4);
+                    List list = new List(house_id, list_id, list_name, list_type);
+                    list.setSystemlist(new SystemList(house_id, list_id, list_name));
+                    systemLists.add(list);
+                }
+                return systemLists;
+            }
+        }
+    }
+
+    private java.util.List<List> findUserListsFiltered(
+            final Connection connection,
+            final Long houseId,
+            final String username,
+            final Boolean shared
+    ) throws SQLException, EntityException {
+        String sql = "SELECT public.\"list\".house_id, public.\"list\".list_id, public.\"list\".list_name, public.\"list\".list_type, public.\"userlist\".users_username, public.\"userlist\".list_shareable " +
+                "FROM public.\"list\" JOIN public.\"userlist\" ON (public.\"list\".house_id = public.\"userlist\".house_id AND public.\"list\".list_id = public.\"userlist\".list_id) " +
+                "WHERE public.\"list\".house_id = ? AND list_type = 'user' AND users_username = ? " +
+                "UNION " +
+                "SELECT public.\"list\".house_id, public.\"list\".list_id, public.\"list\".list_name, public.\"list\".list_type, public.\"userlist\".users_username, public.\"userlist\".list_shareable " +
+                "FROM public.\"list\" JOIN public.\"userlist\" ON (public.\"list\".house_id = public.\"userlist\".house_id AND public.\"list\".list_id = public.\"userlist\".list_id) " +
+                "WHERE public.\"list\".house_id = ? AND list_type = 'user' AND list_shareable = (CASE WHEN ? = true THEN true ELSE null END);";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            if (isNotNull(ps, 1, houseId))
+                ps.setLong(1, houseId);
+            if (isNotNull(ps, 2, username))
+                ps.setString(2, username);
+            if (isNotNull(ps, 3, houseId))
+                ps.setLong(3, houseId);
+            if (isNotNull(ps, 4, shared))
+                ps.setBoolean(4, shared);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                java.util.List<List> userLists = new ArrayList<>();
+                while (resultSet.next()) {
+                    long house_id = resultSet.getLong(1);
+                    short list_id = resultSet.getShort(2);
+                    String list_name = resultSet.getString(3);
+                    String list_type = resultSet.getString(4);
+                    String list_username = resultSet.getString(5);
+                    boolean list_shareable = resultSet.getBoolean(6);
+                    List list = new List(house_id, list_id, list_name, list_type);
+                    list.setUserlist(new UserList(house_id, list_id, list_name, list_username, list_shareable));
+                    userLists.add(list);
+                }
+                return userLists;
+            }
+        }
     }
 
     /**
