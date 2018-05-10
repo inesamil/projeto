ef694391292d7bc84a775ca51c9f1768e8343863
nveloso <42181@alunos.isel.ipl.pt>
2018-05-10 15:06:33 +0100

    Add house input model

---
+++ API/gis/src/main/java/pt/isel/ps/gis/model/inputModel/HouseInputModel.java
@@ -0,0 +1,41 @@
+package pt.isel.ps.gis.model.inputModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class HouseInputModel {
+
+    @JsonProperty("house-name")
+    private String name;
+
+    @JsonProperty("babies-number")
+    private Short babiesNumber;
+
+    @JsonProperty("children-number")
+    private Short childrenNumber;
+
+    @JsonProperty("adults-number")
+    private Short adultsNumber;
+
+    @JsonProperty("seniors-number")
+    private Short seniorsNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public Short getBabiesNumber() {
+        return babiesNumber;
+    }
+
+    public Short getChildrenNumber() {
+        return childrenNumber;
+    }
+
+    public Short getAdultsNumber() {
+        return adultsNumber;
+    }
+
+    public Short getSeniorsNumber() {
+        return seniorsNumber;
+    }
+}
