188e89000932fc3acf386d71039800b067d9bfa5
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-20 20:45:59 +0100

    Update Link

--- API/gis/src/main/java/pt/isel/ps/gis/hypermedia/siren/components/subentities/Link.java
+++ API/gis/src/main/java/pt/isel/ps/gis/hypermedia/siren/components/subentities/Link.java
@@ -1,15 +1,20 @@
 package pt.isel.ps.gis.hypermedia.siren.components.subentities;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"rel", "class", "href"})
 public class Link {
+    public String[] rel;
+    @JsonProperty("class")
+    public String[] klass;
+    public String href;
 
-    private String[] rel;
-    private String href;
-
-    public Link(String[] rel, String href) {
+    public Link(String[] rel, String[] klass, String href) {
         this.rel = rel;
+        this.klass = klass;
         this.href = href;
     }
 
