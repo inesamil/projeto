f578697f5472c226bd4fe86a26eb45fa2e542959
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-20 22:12:37 +0100

    Update CategoriesOutputModel

--- API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/CategoriesOutputModel.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/CategoriesOutputModel.java
@@ -2,68 +2,109 @@ package pt.isel.ps.gis.model.outputModel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.Collection;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Data;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Item;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Link;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Query;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import pt.isel.ps.gis.hypermedia.siren.components.subentities.*;
 import pt.isel.ps.gis.model.Category;
 import pt.isel.ps.gis.utils.UriBuilderUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"class", "properties", "entities", "actions", "links"})
 public class CategoriesOutputModel {
 
+    private final static String ENTITY_CLASS = "categories";
+
+    @JsonProperty(value = "class")
+    private final String[] klass;
     @JsonProperty
-    private final Collection collection;
+    private final Map<String, Object> properties;
+    @JsonProperty
+    private final Entity[] entities;
+    @JsonProperty
+    private final Action[] actions;
+    @JsonProperty
+    private final Link[] links;
 
     public CategoriesOutputModel(List<Category> categories) {
-        this.collection = initCollection(categories);
+        this.klass = initKlass();
+        this.properties = initProperties(categories);
+        this.entities = initEntities(categories);
+        this.actions = initActions();
+        this.links = initLinks();
     }
 
-    private Collection initCollection(List<Category> categories) {
+    private String[] initKlass() {
+        return new String[]{ENTITY_CLASS, "collection"};
+    }
+
+    private Map<String,Object> initProperties(List<Category> categories) {
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("size", categories.size());
+
+        return properties;
+    }
+
+    private Entity[] initEntities(List<Category> categories) {
+        Entity[] entities = new Entity[categories.size()];
+        for (int i = 0; i < categories.size(); ++i) {
+            Category category = categories.get(i);
+            String categoryUri = UriBuilderUtils.buildCategoryUri(category.getCategoryId());
+
+            HashMap<String, Object> properties = new HashMap<>();
+            properties.put("category-id", category.getCategoryId());
+            properties.put("category-name", category.getCategoryName());
+
+            entities[i] = new Entity(new String[]{"category"}, new String[]{"item"}, properties, null);
+        }
+        return entities;
+    }
+
+    private Action[] initActions() {
+        // Type
+        String type = "application/json";
+
+        //URIs
+        String categoriesUri = UriBuilderUtils.buildCategoriesUri();
+
+        // POST allergy
+        Action postCategory = new Action(
+                "add-category",
+                "Add Category",
+                Method.POST,
+                categoriesUri,
+                type,
+                new Field[]{
+                        new Field("category-name", Field.Type.text, null, "Name")
+                }
+        );
+
+        // DELETE allergies
+        Action deleteCategories = new Action(
+                "delete-categories",
+                "Delete Categories",
+                Method.DELETE,
+                categoriesUri,
+                null,
+                null
+        );
+
+        return new Action[]{postCategory, deleteCategories};
+    }
+
+    private Link[] initLinks() {
         //URIs
         String indexUri = UriBuilderUtils.buildIndexUri();
         String categoriesUri = UriBuilderUtils.buildCategoriesUri();
 
-        // Version
-        String version = "1.0";
+        //Link-index
+        Link indexLink = new Link(new String[]{"index"}, new String[]{"index"}, indexUri);
+        // Link-self
+        Link self = new Link(new String[]{"self"}, new String[]{ENTITY_CLASS, "collection"}, categoriesUri);
 
-        // Link
-        Link[] links = new Link[]{
-                new Link("index", indexUri)
-        };
 
-        // Items
-        Item[] items = mapItems(categories);
-
-        Query[] queries = new Query[]{
-                new Query(categoriesUri, "search", "Search by name", new Data[]{
-                        new Data("name", null, "Name")
-                })
-        };
-
-        return new Collection(version, categoriesUri, links, items, queries);
-    }
-
-    private Item[] mapItems(List<Category> categories) {
-        int categoriesSize = categories.size();
-        Item[] items = new Item[categoriesSize];
-        for (int i = 0; i < categoriesSize; i++) {
-            Category category = categories.get(i);
-            int categoryId = category.getCategoryId();
-            items[i] = new Item(
-                    UriBuilderUtils.buildCategoryUri(categoryId),
-                    new Data[]{
-                            new Data("category-id", category.getCategoryId(), "ID"),
-                            new Data("category-name", category.getCategoryName(), "Name")
-                    },
-                    new Link[]{
-                            new Link("products-category", UriBuilderUtils.buildProductsCategoryUri(categoryId))
-                    }
-            );
-        }
-        return items;
+        return new Link[]{indexLink, self};
     }
 }
