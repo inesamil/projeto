02c89f17440cec3e6c081db41bcc85358acefca6
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-20 20:46:54 +0100

    Update AllergiesStockItemOutputModel

--- API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/AllergiesStockItemOutputModel.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/AllergiesStockItemOutputModel.java
@@ -2,59 +2,111 @@ package pt.isel.ps.gis.model.outputModel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.Collection;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Data;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Item;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Link;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import pt.isel.ps.gis.hypermedia.siren.components.subentities.*;
 import pt.isel.ps.gis.model.Allergy;
 import pt.isel.ps.gis.utils.UriBuilderUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"class", "properties", "entities", "actions", "links"})
 public class AllergiesStockItemOutputModel {
 
+    private final static String ENTITY_CLASS = "allergies-stock-item";
+
+    @JsonProperty(value = "class")
+    private final String[] klass;
     @JsonProperty
-    private final Collection collection;
+    private final Map<String, Object> properties;
+    @JsonProperty
+    private final Entity[] entities;
+    @JsonProperty
+    private final Action[] actions;
+    @JsonProperty
+    private final Link[] links;
 
     public AllergiesStockItemOutputModel(long houseId, String sku, List<Allergy> allergies) {
-        this.collection = initCollection(houseId, sku, allergies);
+        this.klass = initKlass();
+        this.properties = initProperties(allergies);
+        this.entities = initEntities(houseId, sku, allergies);
+        this.actions = initActions(houseId, sku);
+        this.links = initLinks(houseId, sku, allergies);
     }
 
-    private Collection initCollection(long houseId, String sku, List<Allergy> allergies) {
+    private String[] initKlass() {
+        return new String[]{ENTITY_CLASS, "collection"};
+    }
+
+    private Map<String, Object> initProperties(List<Allergy> allergies) {
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("size", allergies.size());
+
+        return properties;
+    }
+
+    private Entity[] initEntities(long houseId, String sku, List<Allergy> allergies) {
+        Entity[] entities = new Entity[allergies.size()];
+        for (int i = 0; i < allergies.size(); ++i) {
+            Allergy allergy = allergies.get(i);
+
+            HashMap<String, Object> properties = new HashMap<>();
+            properties.put("house-id", houseId);
+            properties.put("stock-item-id", sku);
+            properties.put("allergy_allergen", allergy.getAllergyAllergen());
+
+            entities[i] = new Entity(new String[]{"allergy"}, new String[]{"item"}, properties, null);
+        }
+        return entities;
+    }
+
+    private Action[] initActions(long houseId, String sku) {
+        // Type
+        String type = "application/json";
+
         //URIs
-        String stockItemUri = UriBuilderUtils.buildStockItemUri(houseId, sku);
         String allergiesStockItemUri = UriBuilderUtils.buildAllergiesStockItemUri(houseId, sku);
 
-        // Version
-        String version = "1.0";
+        // POST allergy
+        Action postAllergy = new Action(
+                "add-allergy",
+                "Add Allergy",
+                Method.POST,
+                allergiesStockItemUri,
+                type,
+                new Field[]{
+                        new Field("allergy-allergen", Field.Type.text, null, "Name")
+                }
+        );
 
-        // Link
-        Link[] links = new Link[]{
-                new Link("stock-item", stockItemUri)
-        };
+        // DELETE allergies
+        Action deleteAllergies = new Action(
+                "delete-allergies",
+                "Delete Allergies",
+                Method.DELETE,
+                allergiesStockItemUri,
+                null,
+                null
+        );
 
-        // Items
-        Item[] items = mapItems(houseId, sku, allergies);
-
-        return new Collection(version, allergiesStockItemUri, links, items);
+        return new Action[]{postAllergy, deleteAllergies};
     }
 
-    private Item[] mapItems(long houseId, String sku, List<Allergy> allergies) {
-        int allergiesSize = allergies.size();
-        Item[] items = new Item[allergiesSize];
-        for (int i = 0; i < allergiesSize; i++) {
-            Allergy allergy = allergies.get(i);
-            items[i] = new Item(
-                    null,
-                    new Data[]{
-                            new Data("house-id", houseId, "House ID"),
-                            new Data("stock-item-id", sku, "Stock Item ID"),
-                            new Data("allergy-allergen", allergy.getAllergyAllergen(), "Allergen")
-                    },
-                    null
-            );
-        }
-        return items;
+    private Link[] initLinks(long houseId, String sku, List<Allergy> allergies) {
+        //URIs
+        String indexUri = UriBuilderUtils.buildIndexUri();
+        String allergiesStockItemUri = UriBuilderUtils.buildAllergiesStockItemUri(houseId, sku);
+        String stockItemUri = UriBuilderUtils.buildStockItemUri(houseId, sku);
+
+        //Link-author-user
+        Link indexLink = new Link(new String[]{"index"}, new String[]{"index"}, indexUri);
+        //Link-related-stockItem
+        Link stockItemLink = new Link(new String[]{"stock-item"}, new String[]{"related"}, stockItemUri);
+        // Link-self
+        Link self = new Link(new String[]{"self"}, new String[]{ENTITY_CLASS, "collection"}, allergiesStockItemUri);
+
+        return new Link[]{self, indexLink, stockItemLink};
     }
 }
