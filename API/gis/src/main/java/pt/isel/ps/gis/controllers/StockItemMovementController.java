71402a52a83142522b6dc566294b4a3abf311d4c
nveloso <42181@alunos.isel.ipl.pt>
2018-05-23 17:41:44 +0100

    add post to stock item movement controller

--- API/gis/src/main/java/pt/isel/ps/gis/controllers/StockItemMovementController.java
+++ API/gis/src/main/java/pt/isel/ps/gis/controllers/StockItemMovementController.java
@@ -3,10 +3,7 @@ package pt.isel.ps.gis.controllers;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pt.isel.ps.gis.bll.HouseService;
 import pt.isel.ps.gis.bll.StockItemMovementService;
 import pt.isel.ps.gis.exceptions.BadRequestException;
@@ -49,6 +46,18 @@ public class StockItemMovementController {
             );
             movements = stockItemMovementService.getStockItemMovementsByHouseIdFiltered(houseId, filters);
         }
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity<>(new MovementsOutputModel(houseId, movements), setSirenContentType(headers),
+                HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<MovementsOutputModel> postMovement(
+            @PathVariable("house-id") long houseId
+    ) throws EntityException {
+        // TODO
+
+        List<StockItemMovement> movements = stockItemMovementService.getStockItemMovementsByHouseId(houseId);
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<>(new MovementsOutputModel(houseId, movements), setSirenContentType(headers),
                 HttpStatus.OK);
