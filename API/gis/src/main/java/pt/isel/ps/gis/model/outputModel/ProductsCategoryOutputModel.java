2e3924af1e19e8f7baa32118f056c3e69f1dca49
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-23 02:24:14 +0100

    Update ProductsCategoryOutputModel

--- API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/ProductsCategoryOutputModel.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/ProductsCategoryOutputModel.java
@@ -2,70 +2,81 @@ package pt.isel.ps.gis.model.outputModel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.Collection;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Data;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Item;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Link;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Query;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import pt.isel.ps.gis.hypermedia.siren.components.subentities.Action;
+import pt.isel.ps.gis.hypermedia.siren.components.subentities.Entity;
+import pt.isel.ps.gis.hypermedia.siren.components.subentities.Link;
 import pt.isel.ps.gis.model.Product;
 import pt.isel.ps.gis.utils.UriBuilderUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"class", "properties", "entities", "links"})
 public class ProductsCategoryOutputModel {
 
+    private final static String ENTITY_CLASS = "products-category";
+
+    @JsonProperty(value = "class")
+    private final String[] klass;
     @JsonProperty
-    private final Collection collection;
+    private final Map<String, Object> properties;
+    @JsonProperty
+    private final Entity[] entities;
+    @JsonProperty
+    private final Link[] links;
 
     // Need categoryId field because if products is empty, cannot know which is the categoryId
     public ProductsCategoryOutputModel(int categoryId, List<Product> products) {
-        this.collection = initCollection(categoryId, products);
+        this.klass = initKlass();
+        this.properties = initProperties(products);
+        this.entities = initEntities(categoryId, products);
+        this.links = initLinks(categoryId);
     }
 
-    private Collection initCollection(int categoryId, List<Product> products) {
-        //URIs
+    // Initters
+    private String[] initKlass() {
+        return new String[]{ENTITY_CLASS, "collection"};
+    }
+
+    private Map<String,Object> initProperties(List<Product> products) {
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("size", products.size());
+
+        return properties;
+    }
+
+    private Entity[] initEntities(int categoryId, List<Product> products) {
+        Entity[] entities = new Entity[products.size()];
+        for (int i = 0; i < products.size(); ++i) {
+            Product productsCategory = products.get(i);
+            int productId = productsCategory.getId().getProductId();
+
+            HashMap<String, Object> properties = new HashMap<>();
+            properties.put("product-id", productId);
+            properties.put("product-name", productsCategory.getProductName());
+            properties.put("product-edible", productsCategory.getProductEdible());
+            properties.put("product-shelflifetime", productsCategory.getProductShelflife() + " " + productsCategory.getProductShelflifetimeunit());
+
+            String productUri = UriBuilderUtils.buildProductUri(productsCategory.getCategoryByCategoryId().getCategoryId(), productId);
+
+            entities[i] = new Entity(new String[]{"movement"}, new String[]{"item"}, properties, null, productUri);
+        }
+        return entities;
+    }
+
+    private Link[] initLinks(int categoryId) {
+        // URIs
         String categoryUri = UriBuilderUtils.buildCategoryUri(categoryId);
         String productsCategoryUri = UriBuilderUtils.buildProductsCategoryUri(categoryId);
 
-        // Version
-        String version = "1.0";
+        // Link-self
+        Link self = new Link(new String[]{"self"}, new String[]{ENTITY_CLASS, "collection"}, productsCategoryUri);
+        //Link-related-category
+        Link categoryLink = new Link(new String[]{"related"}, new String[]{"category"}, categoryUri);
 
-        // Link
-        Link[] links = new Link[]{
-                new Link("category", categoryUri)
-        };
-
-        // Items
-        Item[] items = mapItems(categoryId, products);
-
-        Query[] queries = new Query[]{
-                new Query(productsCategoryUri, "search", "Search by name", new Data[]{
-                        new Data("name", null, "Name")
-                })
-        };
-
-        return new Collection(version, productsCategoryUri, links, items, queries);
-    }
-
-    private Item[] mapItems(int categoryId, List<Product> products) {
-        int productsSize = products.size();
-        Item[] items = new Item[productsSize];
-        for (int i = 0; i < productsSize; i++) {
-            Product product = products.get(i);
-            int productId = product.getId().getProductId();
-            items[i] = new Item(
-                    UriBuilderUtils.buildProductUri(categoryId, productId),
-                    new Data[]{
-                            new Data("product-id", productId, "ID"),
-                            new Data("product-name", product.getProductName(), "Name"),
-                            new Data("product-edible", product.getProductEdible(), "Edible"),
-                            new Data("product-shelflifetime", product.getProductShelflife() + " " +
-                                    product.getProductShelflifetimeunit(), "Shelf Life Time")
-                    },
-                    null
-            );
-        }
-        return items;
+        return new Link[]{self, categoryLink};
     }
 }
