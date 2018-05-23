8b7bce3da9377dfee591b8741b03f9cbd50f07d6
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-23 02:25:05 +0100

    Update MovementsOutputModel

--- API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/MovementsOutputModel.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/MovementsOutputModel.java
@@ -2,85 +2,107 @@ package pt.isel.ps.gis.model.outputModel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.Collection;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.*;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import pt.isel.ps.gis.hypermedia.siren.components.subentities.*;
 import pt.isel.ps.gis.model.StockItemMovement;
+import pt.isel.ps.gis.model.StockItemMovementId;
 import pt.isel.ps.gis.utils.UriBuilderUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"class", "properties", "entities", "actions", "links"})
 public class MovementsOutputModel {
 
+    private final static String ENTITY_CLASS = "movements";
+
+    @JsonProperty(value = "class")
+    private final String[] klass;
     @JsonProperty
-    private final Collection collection;
+    private final Map<String, Object> properties;
+    @JsonProperty
+    private final Entity[] entities;
+    @JsonProperty
+    private final Action[] actions;
+    @JsonProperty
+    private final Link[] links;
 
     public MovementsOutputModel(long houseId, List<StockItemMovement> movements) {
-        this.collection = initCollection(houseId, movements);
+        this.klass = initKlass();
+        this.properties = initProperties(movements);
+        this.entities = initEntities(houseId, movements);
+        this.actions = initActions(houseId);
+        this.links = initLinks(houseId);
     }
 
-    private Collection initCollection(long houseId, List<StockItemMovement> movements) {
-        //URIs
+    // Initters
+    private String[] initKlass() {
+        return new String[]{ENTITY_CLASS, "collection"};
+    }
+
+    private Map<String, Object> initProperties(List<StockItemMovement> movements) {
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("size", movements.size());
+
+        return properties;
+    }
+
+    private Entity[] initEntities(long houseId, List<StockItemMovement> movements) {
+        Entity[] entities = new Entity[movements.size()];
+        for (int i = 0; i < movements.size(); ++i) {
+            StockItemMovement stockItemMovement = movements.get(i);
+            StockItemMovementId stockItemMovementId = stockItemMovement.getId();
+
+            HashMap<String, Object> properties = new HashMap<>();
+            properties.put("house-id", houseId);
+            properties.put("storage-id", stockItemMovement.getStorage().getId());
+            properties.put("stock-item-id", stockItemMovement.getStockitem().getId());
+            properties.put("movement-datetime", stockItemMovementId.getStockitemmovementDatetime());
+            properties.put("movement-type", stockItemMovementId.getStockitemmovementType());
+            properties.put("movement-quantity", stockItemMovementId.getStockitemmovementQuantity());
+
+            entities[i] = new Entity(new String[]{"movement"}, new String[]{"item"}, properties, null, null);
+        }
+        return entities;
+    }
+
+    private Action[] initActions(long houseId) {
+        // Type
+        String type = "application/json";
+
+        // URIs
+        String movementsUri = UriBuilderUtils.buildMovementsUri(houseId);
+
+        // POST Movement
+        Action postMovement = new Action(
+                "add-movement",
+                "Add Movement",
+                Method.POST,
+                movementsUri,
+                type,
+                new Field[]{
+                        new Field("storage-id", Field.Type.number, null, "Storage ID"),
+                        new Field("stock-item-id", Field.Type.number, null, "StockItem ID"),
+                        new Field("movement-type", Field.Type.text, null, "Type"),
+                        new Field("movement-quantity", Field.Type.number, null, "Quantity")
+                }
+        );
+
+        return new Action[]{postMovement};
+    }
+
+    private Link[] initLinks(long houseId) {
+        // URIs
         String houseUri = UriBuilderUtils.buildHouseUri(houseId);
         String movementsUri = UriBuilderUtils.buildMovementsUri(houseId);
 
-        // Version
-        String version = "1.0";
+        // Link-self
+        Link self = new Link(new String[]{"self"}, new String[]{ENTITY_CLASS, "collection"}, movementsUri);
+        //Link-related-house
+        Link houseLink = new Link(new String[]{"related"}, new String[]{"house"}, houseUri);
 
-        // Link
-        Link[] links = new Link[]{
-                new Link("house", houseUri)
-        };
-
-        // Items
-        Item[] items = mapItems(houseId, movements);
-
-        Query[] queries = new Query[]{
-                new Query(movementsUri, "search", "Search by type and/or datetime and/or storage and/or " +
-                        "item", new Data[]{
-                        new Data("type", null, "Type"),
-                        new Data("datetime", null, "Datetime"),
-                        new Data("storage", null, "Storage"),
-                        new Data("stockitem", null, "Stock Item")
-                })
-        };
-
-        // Template
-        Template template = new Template(
-                new Data[]{
-                        new Data("storage-id", null, "Storage ID"),
-                        new Data("movement-type", null, "Type"),
-                        // TODO quantity and info?
-                        new Data("movement-quantity", null, "Quantity"),
-                        new Data("movement-info", null, "Info")
-                });
-
-        return new Collection(version, movementsUri, links, items, queries, template);
-    }
-
-    private Item[] mapItems(long houseId, List<StockItemMovement> movements) {
-        int movementsSize = movements.size();
-        Item[] items = new Item[movementsSize];
-        for (int i = 0; i < movementsSize; i++) {
-            StockItemMovement movement = movements.get(i);
-            short storageId = movement.getId().getStorageId();
-            String sku = movement.getId().getStockitemSku();
-            String datetime = movement.getId().getStockitemmovementDatetime();
-            boolean type = movement.getId().getStockitemmovementType();
-            items[i] = new Item(
-                    UriBuilderUtils.buildMovementsUri(houseId),
-                    new Data[]{
-                            new Data("house-id", houseId, "House ID"),
-                            new Data("storage-id", storageId, "Storage ID"),
-                            new Data("stock-item-id", sku, "Stock Item ID"),
-                            new Data("movement-datetime", datetime, "Datetime"),
-                            new Data("movement-type", type, "Type"),
-                            new Data("movement-quantity", movement.getId().getStockitemmovementQuantity(),
-                                    "Quantity")
-                    },
-                    null
-            );
-        }
-        return items;
+        return new Link[]{self, houseLink};
     }
 }
