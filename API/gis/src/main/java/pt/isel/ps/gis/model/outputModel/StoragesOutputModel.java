614d3e3cf4f9f6f13c5dedbdeb5509903020f8bf
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-23 02:21:12 +0100

    Update StoragesOutputModel

--- API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/StoragesOutputModel.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/StoragesOutputModel.java
@@ -2,69 +2,103 @@ package pt.isel.ps.gis.model.outputModel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.Collection;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Data;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Item;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Link;
-import pt.isel.ps.gis.hypermedia.collectionPlusJson.components.subentities.Template;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import pt.isel.ps.gis.hypermedia.siren.components.subentities.*;
 import pt.isel.ps.gis.model.Storage;
 import pt.isel.ps.gis.utils.UriBuilderUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"class", "properties", "entities", "actions", "links"})
 public class StoragesOutputModel {
 
+    private final static String ENTITY_CLASS = "storages";
+
+    @JsonProperty(value = "class")
+    private final String[] klass;
     @JsonProperty
-    private final Collection collection;
+    private final Map<String, Object> properties;
+    @JsonProperty
+    private final Entity[] entities;
+    @JsonProperty
+    private final Action[] actions;
+    @JsonProperty
+    private final Link[] links;
 
     public StoragesOutputModel(long houseId, List<Storage> storages) {
-        this.collection = initCollection(houseId, storages);
+        this.klass = initKlass();
+        this.properties = initProperties(storages);
+        this.entities = initEntities(houseId, storages);
+        this.actions = initActions(houseId);
+        this.links = initLinks(houseId);
     }
 
-    private Collection initCollection(long houseId, List<Storage> storages) {
+    // Initters
+    private String[] initKlass() {
+        return new String[]{ENTITY_CLASS, "collection"};
+    }
+
+    private Map<String,Object> initProperties(List<Storage> storages) {
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("size", storages.size());
+
+        return properties;
+    }
+
+    private Entity[] initEntities(long houseId, List<Storage> storages) {
+        Entity[] entities = new Entity[storages.size()];
+        for (int i = 0; i < storages.size(); ++i) {
+            Storage storage = storages.get(i);
+
+            HashMap<String, Object> properties = new HashMap<>();
+            properties.put("house-id", houseId);
+            properties.put("storage-id", storage.getId().getStorageId());
+            properties.put("storage-name", storage.getStorageName());
+
+            String storageUri = UriBuilderUtils.buildStorageUri(houseId, storage.getId().getStorageId());
+
+            entities[i] = new Entity(new String[]{"storage"}, new String[]{"item"}, properties, null, storageUri);
+        }
+        return entities;
+    }
+
+    private Action[] initActions(long houseId) {
+        // Type
+        String type = "application/json";
+
+        // URIs
+        String storagesUri = UriBuilderUtils.buildStoragesUri(houseId);
+
+        // POST Storage
+        Action postStorage = new Action(
+                "add-storage",
+                "Add Storage",
+                Method.POST,
+                storagesUri,
+                type,
+                new Field[]{
+                        new Field("storage-name", Field.Type.text, null, "Name"),
+                        new Field("storage-minimum-temperature", Field.Type.number, null, "Minimum Temperature"),
+                        new Field("storage-maximum-temperature", Field.Type.number, null, "Maximum Temperature")
+                }
+        );
+
+        return new Action[]{postStorage};
+    }
+
+    private Link[] initLinks(long houseId) {
         //URIs
         String houseUri = UriBuilderUtils.buildHouseUri(houseId);
         String storagesUri = UriBuilderUtils.buildStoragesUri(houseId);
 
-        // Version
-        String version = "1.0";
+        // Link-self
+        Link self = new Link(new String[]{"self"}, new String[]{ENTITY_CLASS, "collection"}, storagesUri);
+        //Link-related-house
+        Link houseLink = new Link(new String[]{"related"}, new String[]{"house"}, houseUri);
 
-        // Link
-        Link[] links = new Link[]{
-                new Link("house", houseUri)
-        };
-
-        // Items
-        Item[] items = mapItems(houseId, storages);
-
-        // Template
-        Template template = new Template(
-                new Data[]{
-                        new Data("storage-name", null, "Name"),
-                        new Data("storage-minimum-temperature", null, "Minimum Temperature"),
-                        new Data("storage-maximum-temperature", null, "Maximum Temperature")
-                });
-
-        return new Collection(version, storagesUri, links, items, template);
-    }
-
-    private Item[] mapItems(long houseId, List<Storage> storages) {
-        int storagesSize = storages.size();
-        Item[] items = new Item[storagesSize];
-        for (int i = 0; i < storagesSize; i++) {
-            Storage storage = storages.get(i);
-            short storageId = storage.getId().getStorageId();
-            items[i] = new Item(
-                    UriBuilderUtils.buildStorageUri(houseId, storageId),
-                    new Data[]{
-                            new Data("house-id", houseId, "House ID"),
-                            new Data("storage-id", storageId, "Storage ID"),
-                            new Data("storage-name", storage.getStorageName(), "Name")
-                    },
-                    null
-            );
-        }
-        return items;
+        return new Link[]{self, houseLink};
     }
 }
