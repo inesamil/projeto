3795481868fd77d4266da665d2f048c5674528ff
nveloso <42181@alunos.isel.ipl.pt>
2018-05-24 20:52:03 +0100

    Remove app config

--- API/gis/src/test/java/pt/isel/ps/gis/bll/HouseServiceImplTest.java
+++ API/gis/src/test/java/pt/isel/ps/gis/bll/HouseServiceImplTest.java
@@ -6,10 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import pt.isel.ps.gis.AppConfig;
-import pt.isel.ps.gis.dal.repositories.HouseRepository;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
