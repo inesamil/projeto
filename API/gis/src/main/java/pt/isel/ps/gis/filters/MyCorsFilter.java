04738ff94b4eec04318b63dbfcf4efb49856a503
nveloso <42181@alunos.isel.ipl.pt>
2018-05-24 20:51:19 +0100

    Add cors filter

---
+++ API/gis/src/main/java/pt/isel/ps/gis/filters/MyCorsFilter.java
@@ -0,0 +1,31 @@
+package pt.isel.ps.gis.filters;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+/*
+@Component
+public class MyCorsFilter extends CorsFilter {
+
+    public MyCorsFilter() {
+        super(getCorsConfigurationSource());
+    }
+
+    private static CorsConfigurationSource getCorsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedOrigin("http://localhost:55789");
+        config.addAllowedHeader("Authorization");
+        config.addAllowedHeader("Content-Type");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}*/
