39012f57bfe11f5d56ed89c8aac3341aa3377c06
nveloso <42181@alunos.isel.ipl.pt>
2018-05-24 20:53:46 +0100

    Update characteristics dto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/CharacteristicsDTO.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/CharacteristicsDTO.kt
@@ -2,12 +2,13 @@ package ps.leic.isel.pt.gis.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class CharacteristicsDTO(
-        val babiesNumber: Short,
-        val childrenNumber: Short,
-        val adultsNumber: Short,
-        val seniorsNumber: Short
+        @field:JsonProperty(value = "house_babiesNumber") val babiesNumber: Short,
+        @field:JsonProperty(value = "house_childrenNumber") val childrenNumber: Short,
+        @field:JsonProperty(value = "house_adultsNumber") val adultsNumber: Short,
+        @field:JsonProperty(value = "house_seniorsNumber") val seniorsNumber: Short
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
             parcel.readInt().toShort(),
