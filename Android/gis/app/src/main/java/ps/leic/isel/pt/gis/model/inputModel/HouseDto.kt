7bacd765c7d47fd6e9b4d72736cf35fc07e1d10a
nveloso <42181@alunos.isel.ipl.pt>
2018-05-24 20:54:00 +0100

    Update house and housesDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/HouseDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/HouseDto.kt
@@ -1,4 +1,34 @@
 package ps.leic.isel.pt.gis.model.inputModel
 
-class HouseDto {
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import ps.leic.isel.pt.gis.hypermedia.subentities.Action
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
+import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
+import ps.leic.isel.pt.gis.model.CharacteristicsDTO
+
+class HouseDto(siren: Siren) {
+
+    companion object {
+        val mapper: ObjectMapper = jacksonObjectMapper()
+    }
+
+    val houseId: Long
+    val name: String?
+    val characteristics: CharacteristicsDTO?
+    val actions: Array<Action>?
+    val links: Array<String>
+
+    init {
+        val properties = siren.properties
+        houseId = (properties?.get("house-id") as Int).toLong()
+        name = properties["house-name"] as String
+        val chars = properties["house-characteristics"]
+        characteristics = mapper.convertValue<CharacteristicsDTO>(chars, CharacteristicsDTO::class.java)
+        this.actions = siren.actions
+        siren.links?.forEach {
+            val house = it.klass?.find { it.equals("house") }
+            links.put(house, it.href)
+        }
+    }
 }
--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/HousesDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/HousesDto.kt
@@ -1,4 +1,27 @@
 package ps.leic.isel.pt.gis.model.inputModel
 
-class HousesDto {
+import ps.leic.isel.pt.gis.hypermedia.subentities.Action
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
+import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
+
+class HousesDto(siren: Siren) {
+
+    val houses: List<HouseDto> = siren.entities?.map {
+        // TODO change null with it.links
+        HouseDto(Siren(it.klass, it.properties, null, it.actions, null))
+    }.orEmpty()
+    val actions: HousesAction = HousesAction(siren.actions)
+    val links: HousesLink = HousesLink(siren.links)
+
+    class HousesAction(actions: Array<Action>?) {
+        val addHouse: Action? = actions?.find {
+            it.name == "add-house"
+        }
+    }
+
+    class HousesLink(links: Array<Link>?) {
+        val userLink: String? = links?.find {
+            it.klass?.contains("user") ?: false
+        }?.href
+    }
 }
