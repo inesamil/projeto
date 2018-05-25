ea974e72e5cc1e389c18187f6856be2a37dd6a6c
nveloso <42181@alunos.isel.ipl.pt>
2018-05-25 21:00:56 +0100

    Add HouseViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/HouseViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/HouseViewModel.kt
@@ -3,7 +3,7 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
-import ps.leic.isel.pt.gis.model.inputModel.HousesDto
+import ps.leic.isel.pt.gis.model.dtos.HousesDto
 import ps.leic.isel.pt.gis.repositories.HousesRepository
 import ps.leic.isel.pt.gis.request.Resource
 import ps.leic.isel.pt.gis.request.Service
