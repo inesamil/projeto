7a89a9f720451efac8833f6f570c0ccdf9029d7c
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-25 19:07:26 +0100

    Update Android App - HouseDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/HouseDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/HouseDto.kt
@@ -11,24 +11,43 @@ class HouseDto(siren: Siren) {
 
     companion object {
         val mapper: ObjectMapper = jacksonObjectMapper()
+        const val houseIdLabel: String = "house-id"
+        const val houseNameLabel: String = "house-name"
+        const val houseCharacteristicsLabel = "house-characteristics"
+        const val houseClassLabel: String = "house"
+        const val indexLabel: String = "index"
+        const val addHouseLabel: String = "add-house"
     }
 
     val houseId: Long
     val name: String?
     val characteristics: CharacteristicsDTO?
-    val actions: Array<Action>?
-    val links: Array<String>
+    val actions: HousesActions
+    val links: HouseLinks
 
     init {
         val properties = siren.properties
-        houseId = (properties?.get("house-id") as Int).toLong()
-        name = properties["house-name"] as String
-        val chars = properties["house-characteristics"]
-        characteristics = mapper.convertValue<CharacteristicsDTO>(chars, CharacteristicsDTO::class.java)
-        this.actions = siren.actions
-        siren.links?.forEach {
-            val house = it.klass?.find { it.equals("house") }
-            links.put(house, it.href)
+        houseId = (properties?.get(houseIdLabel) as Int).toLong()
+        name = properties[houseNameLabel] as String
+        val characteristics = properties[houseCharacteristicsLabel]
+        this.characteristics = mapper.convertValue<CharacteristicsDTO>(characteristics, CharacteristicsDTO::class.java)
+        actions = HousesActions(siren.actions)
+        links = HouseLinks(siren.links)
+    }
+
+    class HousesActions(actions: Array<Action>?) {
+        val addHouse: Action? = actions?.find {
+            it.name == addHouseLabel
         }
     }
+
+    class HouseLinks(links: Array<Link>?) {
+        val selfLink: String? = links?.find {
+            it.klass?.contains(houseClassLabel) ?: false
+        }?.href
+        val indexLink: String? = links?.find {
+            it.klass?.contains(indexLabel) ?: false
+        }?.href
+    }
+
 }
