e6ec6ec3a27ad8034a6418430dd91ae0a04bfa7d
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-25 19:43:40 +0100

    Update Android App - Update AllergiesDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/AllergiesDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/inputModel/AllergiesDto.kt
@@ -1,2 +1,32 @@
 package ps.leic.isel.pt.gis.model.inputModel
 
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
+import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
+
+class AllergiesDto(siren: Siren) {
+    val allergies: Array<String>
+    val links: AllergyLinks
+
+    init {
+        val entities = siren.entities
+        allergies = entities?.map {
+            it.properties?.get(allergenLabel) as String
+        }.orEmpty().toTypedArray()
+        links = AllergyLinks(siren.links)
+    }
+
+    class AllergyLinks(links: Array<Link>?) {
+        val selfLink: String? = links?.find {
+            it.klass?.contains(allergyClassLabel) ?: false
+        }?.href
+        val indexLink: String? = links?.find {
+            it.klass?.contains(indexLabel) ?: false
+        }?.href
+    }
+
+    companion object {
+        const val allergenLabel: String = "allergy-allergen"
+        const val allergyClassLabel: String = "allergy"
+        const val indexLabel: String = "index"
+    }
+}
