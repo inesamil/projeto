92a4d4eebc12ba2b68c361b89af52d657847bfc5
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-26 09:50:54 +0100

    Update ListsDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/ListsDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/ListsDto.kt
@@ -1,2 +1,40 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import ps.leic.isel.pt.gis.hypermedia.subentities.Action
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
+import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
+
+class ListsDto(siren: Siren) {
+    val lists: Array<ListDto>
+    val actions: ListsActions
+    val links: HousesLink
+
+    init {
+        lists = siren.entities?.map {
+            ListDto(Siren(it.klass, it.properties, null, null, it.links))
+        }.orEmpty().toTypedArray()
+        actions = ListsActions(siren.actions)
+        links = HousesLink(siren.links)
+    }
+
+    class ListsActions(actions: Array<Action>?) {
+        val addList: Action? = actions?.find {
+            it.name == addListLabel
+        }
+    }
+
+    class HousesLink(links: Array<Link>?) {
+        val selfLink: String? = links?.find {
+            it.klass?.contains(listsClassLabel) ?: false
+        }?.href
+        val houseLink: String? = links?.find {
+            it.klass?.contains(listsClassLabel) ?: false
+        }?.href
+    }
+
+    companion object {
+        const val listsClassLabel: String = "lists"
+        const val houseLabel: String = "house"
+        const val addListLabel: String = "add-list"
+    }
+}
