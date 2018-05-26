8ac11876e6f9449fa3c175e4aa14f760cce28ef5
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 20:39:40 +0100

    Update Service Locator

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/ServiceLocator.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/ServiceLocator.kt
@@ -5,20 +5,30 @@ import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import ps.leic.isel.pt.gis.httpRequest.HttpWebService
 import ps.leic.isel.pt.gis.httpRequest.HttpWebServiceImpl
+import ps.leic.isel.pt.gis.repositories.Repository
+import ps.leic.isel.pt.gis.repositories.RepositoryImpl
 
 object ServiceLocator {
 
     private var httpWebService: HttpWebService? = null
     private const val CREDENTIALS_TAG = "credentials"
+    private var repository: Repository? = null
 
-    fun getWebService(context: Context): HttpWebService? {
+    fun getWebService(context: Context): HttpWebService {
         if (httpWebService == null)
             httpWebService = HttpWebServiceImpl(context)
-        return httpWebService
+        return httpWebService as HttpWebService
     }
 
     fun getUserCredentials(context: Context): String {
         val mPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
         return mPreferences.getString(CREDENTIALS_TAG, "")
+    }
+
+    fun getRepository(context: Context) : Repository {
+        if (repository == null){
+            repository = RepositoryImpl(context)
+        }
+        return repository as Repository
     }
 }
