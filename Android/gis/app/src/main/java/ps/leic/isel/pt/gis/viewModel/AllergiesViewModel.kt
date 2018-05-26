74c866a7728f1714504e38de6de30f8b44c7d89e
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 20:40:11 +0100

    Update AllergiesViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/AllergiesViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/AllergiesViewModel.kt
@@ -3,22 +3,25 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.dtos.AllergiesDto
 import ps.leic.isel.pt.gis.repositories.AllergiesRepository
 import ps.leic.isel.pt.gis.repositories.Resource
 
-class AllergiesViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val allergiesRepository: AllergiesRepository = AllergiesRepositoryImpl(application)
+class AllergiesViewModel(private val app: Application) : AndroidViewModel(app) {
 
     private lateinit var allergies: LiveData<Resource<AllergiesDto>>
 
     fun init(url: String) {
         if (::allergies.isInitialized) return
-        allergies = allergiesRepository.getAllergies(url)
+        allergies = ServiceLocator.getRepository(app).get(AllergiesDto::class.java, url, TAG)
     }
 
     fun getAllergies(): LiveData<Resource<AllergiesDto>> {
         return allergies
+    }
+
+    companion object {
+        const val TAG: String = "AllergiesViewModel"
     }
 }
