858e442ed29f4e65e35029590f3234fa06f19a1a
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-26 10:38:58 +0100

    Update MemberDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/MemberDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/MemberDto.kt
@@ -1,7 +1,32 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
 import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
 
 class MemberDto(siren: Siren) {
+    val houseId: Long
+    val username: String
+    val administrator: Boolean
+    val links: MemberLynks
 
+    init {
+        val properties = siren.properties
+        houseId = (properties?.get(houseIdLabel) as Int).toLong()
+        username = properties?.get(usernameLabel) as String
+        administrator = properties?.get(adminstratorLabel) as Boolean
+        links = MemberLynks(siren.links)
+    }
+
+    class MemberLynks(links: Array<Link>?) {
+        val userLink: String? = links?.find {
+            it.klass?.contains(userLabel) ?: false
+        }?.href
+    }
+
+    companion object {
+        const val houseIdLabel: String = "house-id"
+        const val usernameLabel: String = "user-username"
+        const val adminstratorLabel: String = "household-administrator"
+        const val userLabel: String = "user"
+    }
 }
