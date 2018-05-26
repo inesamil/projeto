efee10a2cd87cf1cf63fa6c01da6a250672e1d5c
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 20:53:58 +0100

    Update StoragesViewModel - add cancel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/StoragesViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/StoragesViewModel.kt
@@ -3,22 +3,29 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.dtos.StoragesDto
 import ps.leic.isel.pt.gis.repositories.Resource
-import ps.leic.isel.pt.gis.repositories.StoragesRepository
 
-class StoragesViewModel(application: Application) : AndroidViewModel(application) {
+class StoragesViewModel(private val app: Application) : AndroidViewModel(app) {
 
-    private val storagesRepo: StoragesRepository = StoragesRepositoryImpl(application)
-
-    private lateinit var storages: LiveData<Resource<StoragesDto>>
+    private var storages: LiveData<Resource<StoragesDto>>? = null
 
     fun init(url: String) {
-        if (::storages.isInitialized) return
-        storages = storagesRepo.getStorages(url)
+        if (storages != null) return
+        storages = ServiceLocator.getRepository(app.applicationContext).get(StoragesDto::class.java, url, TAG)
     }
 
-    fun getStorages(): LiveData<Resource<StoragesDto>> {
+    fun getStorages(): LiveData<Resource<StoragesDto>>? {
         return storages
+    }
+
+    fun cancel() {
+        ServiceLocator.getRepository(app.applicationContext).cancelAllPendingRequests(TAG)
+        storages = null
+    }
+
+    companion object {
+        private const val TAG: String = "StoragesViewModel"
     }
 }
