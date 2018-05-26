410522c3cfb1ce042a79f83f3d48f0d42dd3a7bc
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-26 10:39:15 +0100

    Update HouseAllergyDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/HouseAllergyDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/HouseAllergyDto.kt
@@ -1,7 +1,46 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import ps.leic.isel.pt.gis.hypermedia.subentities.Action
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
 import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
 
 class HouseAllergyDto(siren: Siren) {
+    val houseId: Long
+    val allergen: String
+    val houseAllergiesNum: Short
+    val actions: HouseAllergyActions
+    val links: HouseAllergyLynks
 
+    init {
+        val properties = siren.properties
+        houseId = (properties?.get(houseIdLabel) as Int).toLong()
+        allergen = properties?.get(allergenLabel) as String
+        houseAllergiesNum = properties?.get(houseAllergiesNumLabel) as Short
+        actions = HouseAllergyActions(siren.actions)
+        links = HouseAllergyLynks(siren.links)
+    }
+
+    class HouseAllergyActions(actions: Array<Action>?) {
+        val updateHouseAllergy: Action? = actions?.find {
+            it.name == updateHouseAllergyLabel
+        }
+        val deleteHouseAllergy: Action? = actions?.find {
+            it.name == deleteHouseAllergyLabel
+        }
+    }
+
+    class HouseAllergyLynks(links: Array<Link>?) {
+        val stockItemAllergenLink: String? = links?.find {
+            it.klass?.contains(stockItemAllergenLabel) ?: false
+        }?.href
+    }
+
+    companion object {
+        const val houseIdLabel: String = "house-id"
+        const val allergenLabel: String = "allergy-allergen"
+        const val houseAllergiesNumLabel: String = "house-allergies-Num"
+        const val stockItemAllergenLabel: String = "stock-items-allergen"
+        const val updateHouseAllergyLabel: String = "update-house-allergy"
+        const val deleteHouseAllergyLabel: String = "delete-house-allergy"
+    }
 }
