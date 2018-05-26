90564089e95a006b96b3a5fa9dddb956921488dc
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 20:39:56 +0100

    Update HousesViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/HousesViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/HousesViewModel.kt
@@ -3,23 +3,26 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.dtos.HousesDto
 import ps.leic.isel.pt.gis.repositories.HousesRepository
 import ps.leic.isel.pt.gis.repositories.Resource
 import ps.leic.isel.pt.gis.repositories.implementations.HousesRepositoryImpl
 
-class HousesViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val housesRepo: HousesRepository = HousesRepositoryImpl(application)
+class HousesViewModel(private val app: Application) : AndroidViewModel(app) {
 
     private lateinit var houses: LiveData<Resource<HousesDto>>
 
     fun init(url: String) {
         if (::houses.isInitialized) return
-        houses = housesRepo.getHouses(url)
+        houses = ServiceLocator.getRepository(app.applicationContext).get(HousesDto::class.java, url, TAG)
     }
 
     fun getHouses(): LiveData<Resource<HousesDto>> {
         return houses
+    }
+
+    companion object {
+        const val TAG: String = "HousesViewModel"
     }
 }
