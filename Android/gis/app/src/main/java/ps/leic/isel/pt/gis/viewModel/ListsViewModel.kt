3a0ab4c709e4e6a4a9a5eac0749317e2bcd67275
nveloso <42181@alunos.isel.ipl.pt>
2018-05-26 20:55:50 +0100

    Update Lists ViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ListsViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ListsViewModel.kt
@@ -3,22 +3,30 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.dtos.ListsDto
-import ps.leic.isel.pt.gis.repositories.ListsRepository
 import ps.leic.isel.pt.gis.repositories.Resource
 
-class ListsViewModel(application: Application) : AndroidViewModel(application) {
+class ListsViewModel(private val app: Application) : AndroidViewModel(app) {
 
-    private val listsRepo: ListsRepository = ListsRepositoryImpl(application)
-
-    private lateinit var lists: LiveData<Resource<ListsDto>>
+    private var lists: LiveData<Resource<ListsDto>>? = null
 
     fun init(url: String) {
-        if (::lists.isInitialized) return
-        lists = listsRepo.getLists(url)
+        if (lists != null) return
+        lists = ServiceLocator.getRepository(app.applicationContext)
+                .get(ListsDto::class.java, url, TAG)
     }
 
-    fun getLists(): LiveData<Resource<ListsDto>> {
+    fun getLists(): LiveData<Resource<ListsDto>>? {
         return lists
+    }
+
+    fun cancel() {
+        ServiceLocator.getRepository(app.applicationContext).cancelAllPendingRequests(TAG)
+        lists = null
+    }
+
+    companion object {
+        private const val TAG = "ListsViewModel"
     }
 }
