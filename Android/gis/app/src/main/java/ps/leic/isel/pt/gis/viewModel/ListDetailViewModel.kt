3248ed5834d82a2548e4fbc213afa1b4580b0f66
nveloso <42181@alunos.isel.ipl.pt>
2018-05-26 20:56:01 +0100

    Update ListDetail ViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ListDetailViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ListDetailViewModel.kt
@@ -3,23 +3,30 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.dtos.ProductsListDto
-import ps.leic.isel.pt.gis.repositories.ListsRepository
 import ps.leic.isel.pt.gis.repositories.Resource
 
-class ListDetailViewModel(application: Application) : AndroidViewModel(application) {
+class ListDetailViewModel(private val app: Application) : AndroidViewModel(app) {
 
-    // TODO ver se é este repository
-    private val listsRepo: ListsRepository = ListsRepositoryImpl(application)
-
-    private lateinit var listDetail: LiveData<Resource<ProductsListDto>>
+    private var listDetail: LiveData<Resource<ProductsListDto>>? = null
 
     fun init(url: String) {
-        if (::listDetail.isInitialized) return
-        listDetail = listsRepo.getProductsList(url)
+        if (listDetail != null) return
+        listDetail = ServiceLocator.getRepository(app.applicationContext)
+                .get(ProductsListDto::class.java, url, TAG)
     }
 
-    fun getListDetail(): LiveData<Resource<ProductsListDto>> {
+    fun getListDetail(): LiveData<Resource<ProductsListDto>>? {
         return listDetail
+    }
+
+    fun cancel() {
+        ServiceLocator.getRepository(app.applicationContext).cancelAllPendingRequests(TAG)
+        listDetail = null
+    }
+
+    companion object {
+        private const val TAG = "ListDetailViewModel"
     }
 }
