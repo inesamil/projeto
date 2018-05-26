82c810687f33ea22b7435e37e5c8b4a17bb6f1f7
nveloso <42181@alunos.isel.ipl.pt>
2018-05-26 11:50:53 +0100

    Rename houseRepository to housesRepository

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/HousesRepository.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/HousesRepository.kt
@@ -3,7 +3,7 @@ package ps.leic.isel.pt.gis.repositories
 import android.arch.lifecycle.LiveData
 import ps.leic.isel.pt.gis.model.dtos.HousesDto
 
-interface HouseRepository {
+interface HousesRepository {
 
     fun getHouses(url: String): LiveData<Resource<HousesDto>>
 }
