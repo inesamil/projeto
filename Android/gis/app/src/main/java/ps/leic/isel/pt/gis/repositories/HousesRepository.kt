2248e4a3eafd190efcd16fd79d5d1d1116a5c1ce
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-26 14:46:46 +0100

    Update HousesRepository - Interface

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/HousesRepository.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/HousesRepository.kt
@@ -1,9 +1,26 @@
 package ps.leic.isel.pt.gis.repositories
 
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.model.dtos.HouseDto
 import ps.leic.isel.pt.gis.model.dtos.HousesDto
+import ps.leic.isel.pt.gis.model.dtos.MemberDto
+import ps.leic.isel.pt.gis.model.dtos.MembersDto
 
 interface HousesRepository {
 
     fun getHouses(url: String): LiveData<Resource<HousesDto>>
+
+    fun getHouse(url: String): LiveData<Resource<HouseDto>>
+
+    fun getUsersHouse(url: String): LiveData<Resource<MembersDto>>
+
+    fun postHouse(url: String): LiveData<Resource<HousesDto>>
+
+    fun putHouse(url: String): LiveData<Resource<HouseDto>>
+
+    fun putUserHouse(url: String): LiveData<Resource<MemberDto>>
+
+    fun deleteHouse(url: String): LiveData<Resource<HouseDto>>
+
+    fun deleteUserHouse(url: String): LiveData<Resource<MemberDto>>
 }
