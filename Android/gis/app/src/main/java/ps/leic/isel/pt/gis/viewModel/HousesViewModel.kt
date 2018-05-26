029759c7b35358e9400fb6574be38065de04470f
nveloso <42181@alunos.isel.ipl.pt>
2018-05-26 11:51:33 +0100

    Update houses view model

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/HousesViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/HousesViewModel.kt
@@ -4,13 +4,13 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import ps.leic.isel.pt.gis.model.dtos.HousesDto
-import ps.leic.isel.pt.gis.repositories.implementations.HousesRepository
+import ps.leic.isel.pt.gis.repositories.HousesRepository
 import ps.leic.isel.pt.gis.repositories.Resource
-import ps.leic.isel.pt.gis.httpRequest.HttpWebServiceImpl
+import ps.leic.isel.pt.gis.repositories.implementations.HousesRepositoryImpl
 
 class HousesViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val housesRepo: HousesRepository = HousesRepository(application)
+    private val housesRepo: HousesRepository = HousesRepositoryImpl(application)
 
     private var houses: LiveData<Resource<HousesDto>>? = null
 
