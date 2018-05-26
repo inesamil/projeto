4cd5795d78fb0e940bf46662b277df56f4566301
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-26 20:27:15 +0100

    Update ExpirationDateDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/ExpirationDateDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/ExpirationDateDto.kt
@@ -1,19 +1,8 @@
 package ps.leic.isel.pt.gis.model.dtos
 
-import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Siren
+import com.fasterxml.jackson.annotation.JsonProperty
 
-class ExpirationDateDto(siren: Siren) {
-    val expirationDate: String
-    val quantity: Short
-
-    init {
-        val properties = siren.properties
-        expirationDate = properties?.get(expirationDateLabel) as String
-        quantity = properties?.get(quantityLabel) as Short
-    }
-
-    companion object {
-        const val expirationDateLabel = "expiration-date"
-        const val quantityLabel = "quantity"
-    }
-}
+class ExpirationDateDto(@JsonProperty("expiration-date")
+                        val expirationDate: String?,
+                        @JsonProperty("quantity")
+                        val quantity: Short?)
