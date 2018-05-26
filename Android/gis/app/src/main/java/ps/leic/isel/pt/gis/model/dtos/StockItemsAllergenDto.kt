406024ef8b3453434b6cc6718d5003ecc63664ec
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 11:36:01 +0100

    Update StockItemsAllergenDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/StockItemsAllergenDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/StockItemsAllergenDto.kt
@@ -1,2 +1,30 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
+import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
+
+class StockItemsAllergenDto(siren: Siren) {
+    val stockItems: Array<StockItemDto>
+    val links: StockItemsLinks
+
+    init {
+        stockItems = siren.entities?.map {
+            StockItemDto(Siren(it.klass, it.properties, null, it.actions, it.links))
+        }.orEmpty().toTypedArray()
+        links = StockItemsLinks(siren.links)
+    }
+
+    class StockItemsLinks(links: Array<Link>?) {
+        val selfLink: String? = links?.find {
+            it.klass?.contains(stockItemsClassLabel) ?: false
+        }?.href
+        val houseAllergiesLink: String? = links?.find {
+            it.klass?.contains(houseAllergiesLabel) ?: false
+        }?.href
+    }
+
+    companion object {
+        const val stockItemsClassLabel: String = "stock-items"
+        const val houseAllergiesLabel: String = "house-allergies"
+    }
+}
