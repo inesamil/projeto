2db9fd52a6936bdfe8af24b0748bae0a4f00b739
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-26 20:26:00 +0100

    Update StockItemDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/StockItemDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/StockItemDto.kt
@@ -1,5 +1,8 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlinx.android.synthetic.*
 import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Link
 import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Siren
 
@@ -8,28 +11,37 @@ class StockItemDto(siren: Siren) {
     val sku: String
     val categoryId: Long
     val productId: Long
+    val productName: String
     val brand: String
     val conservationStorage: String
     val description: String
     val quantity: Short
     val segment: String
     val expirationsDate: Array<ExpirationDateDto>
+    val storages: Array<String>
     val links: StockItemLinks
 
     init {
         val properties = siren.properties
         houseId = (properties?.get(houseIdLabel) as Int).toLong()
-        sku = properties?.get(skuLabel) as String
-        categoryId = (properties?.get(categoryIdLabel) as Int).toLong()
-        productId = (properties?.get(productIdLabel) as Int).toLong()
-        brand = properties?.get(brandLabel) as String
-        conservationStorage = properties?.get(conservationStorageLabel) as String
-        description = properties?.get(descriptionLabel) as String
-        quantity = properties?.get(quantityLabel) as Short
-        segment = properties?.get(segmentLabel) as String
-        expirationsDate = siren.entities?.map {
-            ExpirationDateDto(Siren(it.klass, it.properties, null, null, null))
-        }.orEmpty().toTypedArray()
+        sku = properties.get(skuLabel) as String
+        categoryId = (properties.get(categoryIdLabel) as Int).toLong()
+        productId = (properties.get(productIdLabel) as Int).toLong()
+        productName = properties.get(productNameLabel) as String
+        brand = properties.get(brandLabel) as String
+        conservationStorage = properties.get(conservationStorageLabel) as String
+        description = properties.get(descriptionLabel) as String
+        quantity = properties.get(quantityLabel) as Short
+        segment = properties.get(segmentLabel) as String
+        val expirationDateElements = siren.entities?.find {
+            it.klass?.contains("expiration-dates") ?: false
+        }?.properties?.get("elements")
+        expirationsDate = mapper.convertValue<Array<ExpirationDateDto>>(expirationDateElements, Array<ExpirationDateDto>::class.java)
+
+        val storageElements = siren.entities?.find {
+            it.klass?.contains("storages") ?: false
+        }?.properties?.get("elements")
+        storages = mapper.convertValue<Array<String>>(storageElements, Array<String>::class.java)
         links = StockItemLinks(siren.links)
     }
 
@@ -47,6 +59,7 @@ class StockItemDto(siren: Siren) {
         const val skuLabel: String = "stock-item-sku"
         const val categoryIdLabel: String = "category-id"
         const val productIdLabel: String = "product-id"
+        const val productNameLabel: String = "product-name"
         const val brandLabel: String = "stock-item-brand"
         const val conservationStorageLabel: String = "stock-item-conservation-storage"
         const val descriptionLabel: String = "stock-item-description"
@@ -55,6 +68,7 @@ class StockItemDto(siren: Siren) {
         const val variety: String = "stock-item-variety"
         const val stockItemClassLabel: String = "stock-item"
         const val stockItemsLabel: String = "stock-items"
+        val mapper: ObjectMapper = jacksonObjectMapper()
     }
 
 }
