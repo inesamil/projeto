e8af02a7924ccd6d864855419a2189c779a82a59
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-26 09:53:23 +0100

    Update CategoryDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/CategoryDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/CategoryDto.kt
@@ -1,2 +1,35 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import ps.leic.isel.pt.gis.hypermedia.subentities.Link
+import ps.leic.isel.pt.gis.hypermedia.subentities.Siren
+
+class CategoryDto(siren: Siren) {
+    val categoryId: Long
+    val categoryName: String
+    val links: CategoryLinks
+
+    init {
+        val properties = siren.properties
+        categoryId = (properties?.get(categoryIdLabel) as Int).toLong()
+        categoryName = properties[categoryNameLabel] as String
+        links = CategoryLinks(siren.links)
+    }
+
+    class CategoryLinks(links: Array<Link>?) {
+        val selfLink: String? = links?.find {
+            it.klass?.contains(categoryClassLabel) ?: false
+        }?.href
+        val productCategoryLink: String? = links?.find {
+            it.klass?.contains(productsCategoryLabel) ?: false
+        }?.href
+    }
+
+    companion object {
+        const val categoryIdLabel: String = "category-id"
+        const val categoryNameLabel: String = "category-name"
+        const val categoryClassLabel: String = "category"
+        const val productsCategoryLabel: String = "products-category"
+    }
+}
