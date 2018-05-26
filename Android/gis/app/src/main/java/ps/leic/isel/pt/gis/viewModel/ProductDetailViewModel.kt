05044181daeed1b60e40c7e879e0432083e627e6
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 20:54:44 +0100

    Update ProductDetailViewModel - add cancel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ProductDetailViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ProductDetailViewModel.kt
@@ -3,22 +3,29 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.dtos.ProductDto
-import ps.leic.isel.pt.gis.repositories.ProductsRepository
 import ps.leic.isel.pt.gis.repositories.Resource
 
-class ProductDetailViewModel(application: Application) : AndroidViewModel(application) {
+class ProductDetailViewModel(private val app: Application) : AndroidViewModel(app) {
 
-    private val productsRepo: ProductsRepository = ProductsRepositoryImpl(application)
-
-    private lateinit var product: LiveData<Resource<ProductDto>>
+    private var product: LiveData<Resource<ProductDto>>? = null
 
     fun init(url: String) {
-        if (::product.isInitialized) return
-        product = productsRepo.getProduct(url)
+        if (product != null) return
+        product = ServiceLocator.getRepository(app.applicationContext).get(ProductDto::class.java, url, TAG)
     }
 
-    fun getProduct(): LiveData<Resource<ProductDto>> {
+    fun getProduct(): LiveData<Resource<ProductDto>>? {
         return product
+    }
+
+    fun cancel() {
+        ServiceLocator.getRepository(app.applicationContext).cancelAllPendingRequests(TAG)
+        product = null
+    }
+
+    companion object {
+        private const val TAG: String = "ProductDetailViewModel"
     }
 }
