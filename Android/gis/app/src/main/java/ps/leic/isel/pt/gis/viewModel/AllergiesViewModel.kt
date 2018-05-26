89db24cf0c957376113abb1a034009beef42017d
nveloso <42181@alunos.isel.ipl.pt>
2018-05-26 20:57:13 +0100

    Add cancel to Allergies ViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/AllergiesViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/AllergiesViewModel.kt
@@ -5,20 +5,24 @@ import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.dtos.AllergiesDto
-import ps.leic.isel.pt.gis.repositories.AllergiesRepository
 import ps.leic.isel.pt.gis.repositories.Resource
 
 class AllergiesViewModel(private val app: Application) : AndroidViewModel(app) {
 
-    private lateinit var allergies: LiveData<Resource<AllergiesDto>>
+    private var allergies: LiveData<Resource<AllergiesDto>>? = null
 
     fun init(url: String) {
-        if (::allergies.isInitialized) return
+        if (allergies != null) return
         allergies = ServiceLocator.getRepository(app).get(AllergiesDto::class.java, url, TAG)
     }
 
-    fun getAllergies(): LiveData<Resource<AllergiesDto>> {
+    fun getAllergies(): LiveData<Resource<AllergiesDto>>? {
         return allergies
+    }
+
+    fun cancel() {
+        ServiceLocator.getRepository(app.applicationContext).cancelAllPendingRequests(TAG)
+        allergies = null
     }
 
     companion object {
