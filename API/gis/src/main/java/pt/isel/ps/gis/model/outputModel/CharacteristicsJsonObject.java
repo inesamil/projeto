2372b9f92c294f117d13257458c361d086f76ffc
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 17:16:18 +0100

    Add CharacteristicsJsonObject

--- API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/CharacteristicsJsonObject.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/CharacteristicsJsonObject.java
@@ -2,21 +2,24 @@ package pt.isel.ps.gis.model.outputModel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import pt.isel.ps.gis.model.House;
+import pt.isel.ps.gis.model.Characteristics;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Characteristics {
+public class CharacteristicsJsonObject {
     @JsonProperty(value = "babies-number")
     private final short babiesNumber;
-    @JsonProperty(value = "babies-number")
-    private final short babiesNumber;
-    @JsonProperty(value = "babies-number")
-    private final short babiesNumber;
-    @JsonProperty(value = "babies-number")
-    private final short babiesNumber;
+    @JsonProperty(value = "children-number")
+    private final short childrenNumber;
+    @JsonProperty(value = "adults-number")
+    private final short adultsNumber;
+    @JsonProperty(value = "seniors-number")
+    private final short seniorsNumber;
 
     // Ctor
-    public Characteristics(pt.isel.ps.gis.model.Characteristics characteristics) {
+    public CharacteristicsJsonObject(Characteristics characteristics) {
         this.babiesNumber = characteristics.getHouse_babiesNumber();
+        this.childrenNumber = characteristics.getHouse_childrenNumber();
+        this.adultsNumber = characteristics.getHouse_adultsNumber();
+        this.seniorsNumber = characteristics.getHouse_seniorsNumber();
     }
 }
