684540898d3b1a869940f18526a6075481a8e3db
nveloso <42181@alunos.isel.ipl.pt>
2018-05-26 23:32:10 +0100

    Add product detail view model to product detail fragment

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/ProductDetailFragment.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/ProductDetailFragment.kt
@@ -1,13 +1,19 @@
 package ps.leic.isel.pt.gis.uis.fragments
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.model.ProductDTO
+import ps.leic.isel.pt.gis.model.dtos.ProductDto
+import ps.leic.isel.pt.gis.repositories.Status
 import ps.leic.isel.pt.gis.utils.ExtraUtils
+import ps.leic.isel.pt.gis.viewModel.ProductDetailViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -21,22 +27,36 @@ import ps.leic.isel.pt.gis.utils.ExtraUtils
 class ProductDetailFragment : Fragment() {
 
     private lateinit var product: ProductDTO
+    private var productDetailViewModel: ProductDetailViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             product = it.getParcelable(ExtraUtils.PRODUCT)
         }
+        productDetailViewModel = ViewModelProviders.of(this).get(ProductDetailViewModel::class.java)
+        val url = ""
+        productDetailViewModel?.init(url)
+        productDetailViewModel?.getProduct()?.observe(this, Observer {
+            if (it?.status == Status.SUCCESS)
+                onSuccess(it.data!!)
+            else if (it?.status == Status.ERROR)
+                onError(it.message)
+        })
+    }
+
+    private fun onSuccess(product: ProductDto) {
+        //TODO
+    }
+
+    private fun onError(error: String?) {
+        Log.v("APP_GIS", error)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view: View =  inflater.inflate(R.layout.fragment_product, container, false)
-
-        //TODO
-
-        return view
+        return inflater.inflate(R.layout.fragment_product, container, false)
     }
 
     override fun onStart() {
