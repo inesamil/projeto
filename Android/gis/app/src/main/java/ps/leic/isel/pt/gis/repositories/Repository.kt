b93e1b3c77dac5395c5442b4dcb305bddd7bf093
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-26 20:39:30 +0100

    Update Repository - add cancelAllPendingRequests

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/Repository.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/Repository.kt
@@ -3,8 +3,14 @@ package ps.leic.isel.pt.gis.repositories
 import android.arch.lifecycle.LiveData
 
 interface Repository {
+
     fun <T> create(c: Class<T>, url: String, body: String, tag: String): LiveData<Resource<T>>
+
     fun <T> get(c: Class<T>, url: String, tag: String): LiveData<Resource<T>>
+
     fun <T> update(c: Class<T>, url: String, body: String, tag: String): LiveData<Resource<T>>
+
     fun <T> delete(c: Class<T>, url: String, tag: String): LiveData<Resource<T>>
+
+    fun cancelAllPendingRequests(tag: String)
 }
--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/RepositoryImpl.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/RepositoryImpl.kt
@@ -1,21 +1,21 @@
 package ps.leic.isel.pt.gis.repositories
 
-import android.app.Application
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
+import android.content.Context
 import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.httpRequest.HttpWebService
 
-class RepositoryImpl(val application: Application) : Repository {
+class RepositoryImpl(private val applicationContext: Context) : Repository {
 
     override fun <T> create(c: Class<T>, url: String, body: String, tag: String): LiveData<Resource<T>> {
         val data = MutableLiveData<Resource<T>>()
         data.value = Resource.loading()
         val headers = mutableMapOf<String, String>()
         headers.put("Content-Type", "application/json") //TODO
-        val authorization = ServiceLocator.getUserCredentials(application.applicationContext)
+        val authorization = ServiceLocator.getUserCredentials(applicationContext)
         // headers.put("Authorization", authorization)
-        ServiceLocator.getWebService(application.applicationContext)?.fetch(
+        ServiceLocator.getWebService(applicationContext).fetch(
                 HttpWebService.Method.POST, url, body, headers, c, {
             data.value = Resource.success(it)
         }, {
@@ -28,9 +28,9 @@ class RepositoryImpl(val application: Application) : Repository {
         val data = MutableLiveData<Resource<T>>()
         data.value = Resource.loading()
         val headers = mutableMapOf<String, String>()
-        val authorization = ServiceLocator.getUserCredentials(application.applicationContext)
+        val authorization = ServiceLocator.getUserCredentials(applicationContext)
         // headers.put("Authorization", authorization)
-        ServiceLocator.getWebService(application.applicationContext)?.fetch(
+        ServiceLocator.getWebService(applicationContext).fetch(
                 HttpWebService.Method.GET, url, null, headers, c, {
             data.value = Resource.success(it)
         }, {
@@ -44,9 +44,9 @@ class RepositoryImpl(val application: Application) : Repository {
         data.value = Resource.loading()
         val headers = mutableMapOf<String, String>()
         headers.put("Content-Type", "application/json") //TODO
-        val authorization = ServiceLocator.getUserCredentials(application.applicationContext)
+        val authorization = ServiceLocator.getUserCredentials(applicationContext)
         // headers.put("Authorization", authorization)
-        ServiceLocator.getWebService(application.applicationContext)?.fetch(
+        ServiceLocator.getWebService(applicationContext).fetch(
                 HttpWebService.Method.PUT, url, body, headers, c, {
             data.value = Resource.success(it)
         }, {
@@ -59,14 +59,18 @@ class RepositoryImpl(val application: Application) : Repository {
         val data = MutableLiveData<Resource<T>>()
         data.value = Resource.loading()
         val headers = mutableMapOf<String, String>()
-        val authorization = ServiceLocator.getUserCredentials(application.applicationContext)
+        val authorization = ServiceLocator.getUserCredentials(applicationContext)
         // headers.put("Authorization", authorization)
-        ServiceLocator.getWebService(application.applicationContext)?.fetch(
+        ServiceLocator.getWebService(applicationContext).fetch(
                 HttpWebService.Method.DELETE, url, null, headers, c, {
             data.value = Resource.success(it)
         }, {
             data.value = Resource.error(it)
         }, tag)
         return data
+    }
+
+    override fun cancelAllPendingRequests(tag: String) {
+        ServiceLocator.getWebService(applicationContext).cancelAll(tag)
     }
 }
