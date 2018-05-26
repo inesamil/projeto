21fec2577868999f949fcf6c18643813d3042760
nveloso <42181@alunos.isel.ipl.pt>
2018-05-26 23:39:15 +0100

    Add list detail view model to list detail fragment

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/ListDetailFragment.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/ListDetailFragment.kt
@@ -1,9 +1,12 @@
 package ps.leic.isel.pt.gis.uis.fragments
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +14,11 @@ import kotlinx.android.synthetic.main.fragment_list.view.*
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.model.ListDTO
 import ps.leic.isel.pt.gis.model.ListProductDTO
+import ps.leic.isel.pt.gis.model.dtos.ProductsListDto
+import ps.leic.isel.pt.gis.repositories.Status
 import ps.leic.isel.pt.gis.uis.adapters.ListDetailAdapter
 import ps.leic.isel.pt.gis.utils.ExtraUtils
+import ps.leic.isel.pt.gis.viewModel.ListDetailViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -29,32 +35,48 @@ class ListDetailFragment : Fragment(), ListDetailAdapter.OnItemClickListener {
     private lateinit var listProducts: Array<ListProductDTO>
 
     private var listener: OnListDetailFragmentInteractionListener? = null
+    private var listDetailViewModel: ListDetailViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             list = it.getParcelable(ExtraUtils.LIST)
-            //TODO: get listproducts in the list
-            listProducts = arrayOf(
-                    ListProductDTO(1, 1, 1, 1, "Leite", "Mimosa", 1),
-                    ListProductDTO(1, 2, 1, 3, "Iogurtes", "Danone", 4)
-            )
         }
+        listDetailViewModel = ViewModelProviders.of(this).get(ListDetailViewModel::class.java)
+        val url = ""
+        listDetailViewModel?.init(url)
+        listDetailViewModel?.getListDetail()?.observe(this, Observer {
+            if (it?.status == Status.SUCCESS)
+                onSuccess(it.data!!)
+            else if (it?.status == Status.ERROR)
+                onError(it.message)
+        })
+        //TODO: get listproducts in the list
+        listProducts = arrayOf(
+                ListProductDTO(1, 1, 1, 1, "Leite", "Mimosa", 1),
+                ListProductDTO(1, 2, 1, 3, "Iogurtes", "Danone", 4)
+        )
+    }
+
+    private fun onSuccess(productsList: ProductsListDto) {
+        // Set Adapter
+        val adapter = ListDetailAdapter(productsList.productsList)
+        view?.let {
+            it.listRecyclerView.layoutManager = LinearLayoutManager(it.context)
+            it.listRecyclerView.setHasFixedSize(true)
+            it.listRecyclerView.adapter = adapter
+        }
+        adapter.setOnItemClickListener(this)
+    }
+
+    private fun onError(error: String?) {
+        Log.v("APP_GIS", error)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view: View = inflater.inflate(R.layout.fragment_list, container, false)
-
-        // Set Adapter
-        val adapter = ListDetailAdapter(/*listProducts*/arrayOf())
-        view.listRecyclerView.layoutManager = LinearLayoutManager(view.context)
-        view.listRecyclerView.setHasFixedSize(true)
-        view.listRecyclerView.adapter = adapter
-        adapter.setOnItemClickListener(this)
-
-        return view
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
     override fun onStart() {
@@ -82,7 +104,7 @@ class ListDetailFragment : Fragment(), ListDetailAdapter.OnItemClickListener {
 
     // NfcListener for list item clicks (from adapter)
     override fun onItemClick(view: View, position: Int) {
-        val  listProduct: ListProductDTO = listProducts[position]
+        val listProduct: ListProductDTO = listProducts[position]
         listener?.onListProductInteraction(listProduct)
     }
 
