66cdf051535c5f9b271b70088abca2d4652d308d
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-28 11:43:01 +0100

    Update FragmentManagerUtils

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/FragmentManagerUtils.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/FragmentManagerUtils.kt
@@ -33,3 +33,18 @@ fun FragmentManager.replaceCurrentFragmentWith(tag: String, newInstance: (String
         popBackStack(tag, 0)
     }
 }
+
+fun FragmentManager.replaceCurrentFragmentWith(tag: String, newInstance: (args: Map<String, Any>) -> Fragment, args: Map<String, Any>) {
+    var fragment = findFragmentByTag(tag)
+    if (fragment == null) {
+        // Fragment not present in back stack. Instantiates new fragment.
+        fragment = newInstance(args)
+        beginTransaction()
+                .replace(R.id.content, fragment, tag)
+                .addToBackStack(tag)
+                .commit()
+    } else {
+        // Fragment is in the back stack
+        popBackStack(tag, 0)
+    }
+}
