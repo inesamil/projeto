fd89eb3a380f6e9d8ba2000b92830af7bdac6d0a
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-28 11:06:33 +0100

    Update StoragesAdapter

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/adapters/StoragesAdapter.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/adapters/StoragesAdapter.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.model.dtos.StorageDto
 
-class StoragesAdapter(private val data: Array<StorageDto>) : RecyclerView.Adapter<StoragesAdapter.ViewHolder>() {
+class StoragesAdapter() : RecyclerView.Adapter<StoragesAdapter.ViewHolder>() {
+
+    private var data: Array<StorageDto>? = null
 
     // Inflates the cell layout from xml when needed
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,13 +21,20 @@ class StoragesAdapter(private val data: Array<StorageDto>) : RecyclerView.Adapte
 
     // Binds the data to the textview in each cell
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
-        // Fill ViewHolder
-        holder.storageName.text = item.name
-        holder.storageTemperature.text = item.temperature
+        data?.let {
+            val item = it[position]
+            // Fill ViewHolder
+            holder.storageName.text = item.name
+            holder.storageTemperature.text = item.temperature
+        }
     }
 
-    override fun getItemCount() = data.size
+    override fun getItemCount() = data?.size ?: 0
+
+    fun setData(data: Array<StorageDto>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
 
     // Stores and recycles views as they are scrolled off screen
     inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
