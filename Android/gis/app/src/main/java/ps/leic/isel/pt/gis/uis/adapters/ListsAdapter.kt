4a3b2801a7aaf8f10ed43856490b5d517266c361
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-28 11:03:32 +0100

    Update ListsAdapter

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/adapters/ListsAdapter.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/adapters/ListsAdapter.kt
@@ -8,10 +8,11 @@ import android.widget.TextView
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.model.dtos.ListDto
 
-class ListsAdapter(private val data: Array<ListDto>) :
+class ListsAdapter :
         RecyclerView.Adapter<ListsAdapter.ViewHolder>() {
 
     private lateinit var mOnItemClickListener: OnItemClickListener
+    private var data: Array<ListDto>? = null
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,15 +23,22 @@ class ListsAdapter(private val data: Array<ListDto>) :
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item: ListDto = data[position]
+        data?.let {
+            val item = it[position]
+            // Fill ViewHolder
+            //holder.itemsText.text =
+            holder.listNameText.text = item.listName
 
-        // Fill ViewHolder
-        //holder.itemsText.text =
-        holder.listNameText.text = item.listName
+        }
+    }
+
+    fun setData(data: Array<ListDto>) {
+        this.data = data
+        notifyDataSetChanged()
     }
 
     // Total number of cells
-    override fun getItemCount() = data.size
+    override fun getItemCount() = data?.size ?: 0
 
     // Stores and recycles views as they are scrolled off screen
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
