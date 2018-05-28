00b7af8dd57c99b8bd2953c862d8f1887c50120e
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-28 11:17:49 +0100

    Update WritingNfcTagFragment

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/WritingNfcTagFragment.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/WritingNfcTagFragment.kt
@@ -43,13 +43,12 @@ class WritingNfcTagFragment : DialogFragment() {
      */
     companion object {
         val TAG = WritingNfcTagFragment::class.java.simpleName
-        val messageArg: String = "message"
 
         @JvmStatic
-        fun newInstance(args: Map<String, Any>) =
+        fun newInstance(message: String) =
                 WritingNfcTagFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ExtraUtils.NFC_MESSAGE, args[messageArg] as String)
+                        putString(ExtraUtils.NFC_MESSAGE, message)
                     }
                 }
     }
