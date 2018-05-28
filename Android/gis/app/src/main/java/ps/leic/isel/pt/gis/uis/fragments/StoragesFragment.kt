d446f42e32c11d79cac1970f619d2204e18ea08e
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-28 10:56:08 +0100

    Update StoragesFragment

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/StoragesFragment.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/fragments/StoragesFragment.kt
@@ -29,47 +29,49 @@ import ps.leic.isel.pt.gis.viewModel.StoragesViewModel
  */
 class StoragesFragment : Fragment() {
 
-    private var houseId: Long = 0
-    private lateinit var storages: StoragesDto
-    private var storagesViewModel: StoragesViewModel? = null
+    private lateinit var url: String
+    private lateinit var storagesViewModel: StoragesViewModel
+    private val adapter = StoragesAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            houseId = it.getLong(ExtraUtils.HOUSE_ID)
+            url = it.getString(ExtraUtils.URL)
         }
         storagesViewModel = ViewModelProviders.of(this).get(StoragesViewModel::class.java)
         // for demo
-        val url = "http://10.0.2.2:8081/v1/houses/1"
-        storagesViewModel?.init(url)
-        storagesViewModel?.getStorages()?.observe(this, Observer {
+        val url = "http://10.0.2.2:8081/v1/houses/1"    //TODO: delete
+        storagesViewModel.init(url)
+        storagesViewModel.getStorages()?.observe(this, Observer {
             if (it?.status == Status.SUCCESS)
                 onSuccess(it.data!!)
             else if (it?.status == Status.ERROR)
                 onError(it.message)
         })
-        //TODO: get data
-        //storages = arrayOf(StorageDTO(1, 1, "Fridge", TemperatureStorageDTO(0F, 5F)))
-    }
-
-    private fun onSuccess(storages: StoragesDto) {
-        // Set Adapter
-        val adapter = StoragesAdapter(storages.storages)
-        view?.let {
-            it.storagesRecyclerView.layoutManager = LinearLayoutManager(it.context)
-            it.storagesRecyclerView.setHasFixedSize(true)
-            it.storagesRecyclerView.adapter = adapter
-        }
-    }
-
-    private fun onError(error: String?) {
-        Log.v("APP_GIS", error)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_storages, container, false)
+        val view = inflater.inflate(R.layout.fragment_storages, container, false)
+
+        view.storagesRecyclerView.layoutManager = LinearLayoutManager(view.context)
+        view.storagesRecyclerView.setHasFixedSize(true)
+        view.storagesRecyclerView.adapter = adapter
+
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        savedInstanceState?.let {
+            url = it.getString(ExtraUtils.URL)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(ExtraUtils.URL, url)
     }
 
     override fun onStart() {
@@ -79,26 +81,33 @@ class StoragesFragment : Fragment() {
 
     override fun onStop() {
         super.onStop()
-        RequestQueue.getInstance(activity?.applicationContext).requestQueue.cancelAll("STORAGES_FRAGMENT")
+        storagesViewModel.cancel()
+    }
+
+    private fun onSuccess(storages: StoragesDto) {
+        adapter.setData(storages.storages)
+    }
+
+    private fun onError(error: String?) {
+        Log.v("APP_GIS", error)
     }
 
     /**
      * StoragesFragment Factory
      */
     companion object {
-        val houseIdArg: String = "houseid"
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param houseId House ID
+         * @param url URL
          * @return A new instance of fragment StoragesFragment.
          */
         @JvmStatic
-        fun newInstance(args: Map<String, Any>) =
+        fun newInstance(url: String) =
                 StoragesFragment().apply {
                     arguments = Bundle().apply {
-                        putLong(ExtraUtils.HOUSE_ID, args[houseIdArg] as Long)
+                        putString(ExtraUtils.URL, url)
                     }
                 }
     }
