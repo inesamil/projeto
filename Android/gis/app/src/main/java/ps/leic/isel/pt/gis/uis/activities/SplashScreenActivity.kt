e42f94aeb898786a6cfccf0aacea529c4eb63aef
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-29 15:29:26 +0100

    Update SplashScreenActivity

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/SplashScreenActivity.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/SplashScreenActivity.kt
@@ -1,5 +1,7 @@
 package ps.leic.isel.pt.gis.uis.activities
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -7,44 +9,79 @@ import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.credentials.Credential
+import ps.leic.isel.pt.gis.GisApplication
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.ServiceLocator
+import ps.leic.isel.pt.gis.model.dtos.IndexDto
+import ps.leic.isel.pt.gis.repositories.Status
+import ps.leic.isel.pt.gis.viewModel.SplashScreenViewModel
 
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private var firstTime: Boolean? = null
-    private val FIRST_TIME_TAG = "first_time"
+    private lateinit var splashScreenViewModel: SplashScreenViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        if (isFirstTime()) {
+        splashScreenViewModel = ViewModelProviders.of(this).get(SplashScreenViewModel::class.java)
+        splashScreenViewModel.init()
+        splashScreenViewModel.getIndex()?.observe(this, Observer {
+            if (it?.status == Status.SUCCESS)
+                onSuccess(it.data!!)
+            else if (it?.status == Status.ERROR)
+                onError(it.message)
+        })
+    }
+
+    private fun onCredentialsRetrieved(credential: Credential) {
+        // Credentials retrieved
+        Log.d(TAG, "Credentials retrieved.")
+        if (true) {    //TODO: validateCredentials
+            // Valid Credentials
+            finish()
+            startActivity(Intent(this, HomeActivity::class.java))
+        } else {
+            // Invalid Credentials
+            ServiceLocator.getSmartLock(applicationContext).deleteCredentials(credential)
+            Log.d(TAG, "Retrieved invalid credential, so delete retrieved credential.")
+            Toast.makeText(this, "Retrieved credentials are invalid.", Toast.LENGTH_SHORT).show()
+            // Wrong credentials
+            goToLoginActivity()
+        }
+    }
+
+    private fun goToLoginActivity() {
+        // No credentials retrieved
+        finish()
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    private fun onSuccess(index: IndexDto) {
+        val gisApplication = application as GisApplication
+        gisApplication.index = index
+
+        if (isFirstTime()){
             finish()
             startActivity(Intent(this, RegisterActivity::class.java))
         } else {
             Log.d(TAG, "Try to retrieve credentials.")
-            ServiceLocator.getSmartLock(applicationContext).retrieveCredentials({
-                // Credentials retrieved
-                Log.d(TAG, "Credentials retrieved.")
-                if (true) {    //TODO: validateCredentials
-                    // Valid Credentials
-                    finish()
-                    startActivity(Intent(this, HomeActivity::class.java))
-                } else {
-                    // Invalid Credentials
-                    ServiceLocator.getSmartLock(applicationContext).deleteCredentials(it)
-                    Log.d(TAG, "Retrieved invalid credential, so delete retrieved credential.")
-                    Toast.makeText(this, "Retrieved credentials are invalid.", Toast.LENGTH_SHORT).show()
-                }
-                //ServiceLocator.getSmartLock(applicationContext).deleteCredentials(it)   //TODO: delete
-            }, {
-                // No credentials retrieved
-                finish()
-                startActivity(Intent(this, LoginActivity::class.java))
-            })
+            ServiceLocator
+                    .getSmartLock(applicationContext)
+                    .retrieveCredentials(::onCredentialsRetrieved, ::goToLoginActivity)
         }
+
+    }
+
+    private fun onError(message: String?) {
+        message?.let {
+            Log.i(TAG, it)
+        }
+        Toast.makeText(this, "Couldn't connect to server", Toast.LENGTH_SHORT).show()
+        finish()    //TODO: kill app
     }
 
     private fun isFirstTime(): Boolean {
@@ -57,5 +94,6 @@ class SplashScreenActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG: String = "SplashScreenActivity"
+        private val FIRST_TIME_TAG = "first_time"
     }
 }
