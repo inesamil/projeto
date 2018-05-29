706233700179eb7d762f7fdb959b5b8747a2ac84
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-29 15:28:23 +0100

    Update GisApplication

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/GisApplication.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/GisApplication.kt
@@ -2,12 +2,16 @@ package ps.leic.isel.pt.gis
 
 import android.app.Application
 import android.content.res.Configuration
+import ps.leic.isel.pt.gis.model.dtos.IndexDto
 import java.util.*
 
 class GisApplication : Application() {
 
     @Volatile
     lateinit var language: String
+
+    @Volatile
+    lateinit var index: IndexDto
 
     override fun onCreate() {
         super.onCreate()
