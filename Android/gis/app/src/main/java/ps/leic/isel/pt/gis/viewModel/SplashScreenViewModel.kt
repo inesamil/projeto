740dcfffe18aa34a35a46d89dc26da64472c6283
isel42162 <42162@alunos.isel.ipl.pt>
2018-05-29 15:29:02 +0100

    Add SplashScreenViewModel

---
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/SplashScreenViewModel.kt
@@ -0,0 +1,32 @@
+package ps.leic.isel.pt.gis.viewModel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.ServiceLocator
+import ps.leic.isel.pt.gis.model.dtos.IndexDto
+import ps.leic.isel.pt.gis.repositories.Resource
+
+class SplashScreenViewModel(private val app: Application) : AndroidViewModel(app) {
+
+    private var index: LiveData<Resource<IndexDto>>? = null
+
+    fun init() {
+        if (index != null) return
+        index = ServiceLocator.getRepository(app.applicationContext).get(IndexDto::class.java, url, TAG)
+    }
+
+    fun getIndex(): LiveData<Resource<IndexDto>>? {
+        return index
+    }
+
+    fun cancel() {
+        ServiceLocator.getRepository(app.applicationContext).cancelAllPendingRequests(TAG)
+        index = null
+    }
+
+    companion object {
+        private const val url: String = "http://10.0.2.2:8081/v1"
+        const val TAG: String = "SplashScreenViewModel"
+    }
+}
