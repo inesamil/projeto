87543876c933aad870532b3a5a208d08691d78c1
nveloso <42181@alunos.isel.ipl.pt>
2018-05-30 23:35:24 +0100

    Update field subentity siren

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/hypermedia/siren/subentities/Field.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/hypermedia/siren/subentities/Field.kt
@@ -1,14 +1,51 @@
 package ps.leic.isel.pt.gis.hypermedia.siren.subentities
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Field(val name: String, val type: Type?, val value: Any?, var title: String?) {
 
     enum class Type constructor(private val type: String) {
-        Hidden("Hidden"), Text("text"), Search("search"), Tel("tel"), Url("url"), Email("email"), Password("password"),
-        Datetime("datetime"), Date("date"), Month("month"), Week("week"), Time("time"), DatetimeLocal("datetimeLocal"),
-        Number("number"), Range("range"), Color("color"), Checkbox("checkbox"), Radio("radio"), File("file"),
+        @JsonProperty("hidden")
+        Hidden("hidden"),
+        @JsonProperty("text")
+        Text("text"),
+        @JsonProperty("search")
+        Search("search"),
+        @JsonProperty("tel")
+        Tel("tel"),
+        @JsonProperty("url")
+        Url("url"),
+        @JsonProperty("email")
+        Email("email"),
+        @JsonProperty("password")
+        Password("password"),
+        @JsonProperty("datetime")
+        Datetime("datetime"),
+        @JsonProperty("date")
+        Date("date"),
+        @JsonProperty("month")
+        Month("month"),
+        @JsonProperty("week")
+        Week("week"),
+        @JsonProperty("time")
+        Time("time"),
+        @JsonProperty("datetimeLocal")
+        DatetimeLocal("datetimeLocal"),
+        @JsonProperty("number")
+        Number("number"),
+        @JsonProperty("range")
+        Range("range"),
+        @JsonProperty("color")
+        Color("color"),
+        @JsonProperty("checkbox")
+        Checkbox("checkbox"),
+        @JsonProperty("radio")
+        Radio("radio"),
+        @JsonProperty("file")
+        File("file"),
+        @JsonProperty("boolean")
         Bool("boolean");
 
         override fun toString(): String {
