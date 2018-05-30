1f75c8a500e1377eb1f258d2fbf555f72b8175a4
nveloso <42181@alunos.isel.ipl.pt>
2018-05-30 15:25:43 +0100

    Update not found exception

--- API/gis/src/main/java/pt/isel/ps/gis/exceptions/NotFoundException.java
+++ API/gis/src/main/java/pt/isel/ps/gis/exceptions/NotFoundException.java
@@ -1,7 +1,10 @@
 package pt.isel.ps.gis.exceptions;
 
-public class NotFoundException extends Exception {
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends ProblemDetailsException {
 
     public NotFoundException() {
+        super("Resource not found.", HttpStatus.NOT_FOUND, "This resource can not be found.");
     }
 }
