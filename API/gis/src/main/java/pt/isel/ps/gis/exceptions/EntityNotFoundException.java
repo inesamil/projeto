d532d0923ae8d69eeb9822b42fd8123e124c5cdd
nveloso <42181@alunos.isel.ipl.pt>
2018-05-30 22:13:07 +0100

    Add todos to some exceptions

--- API/gis/src/main/java/pt/isel/ps/gis/exceptions/EntityNotFoundException.java
+++ API/gis/src/main/java/pt/isel/ps/gis/exceptions/EntityNotFoundException.java
@@ -1,6 +1,6 @@
 package pt.isel.ps.gis.exceptions;
 
-public class EntityNotFoundException extends Exception {
+public class EntityNotFoundException extends Exception { // TODO map in problem detail
 
     public EntityNotFoundException(String message) {
         super(message);
