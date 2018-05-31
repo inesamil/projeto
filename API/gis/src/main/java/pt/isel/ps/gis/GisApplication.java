2945635e1b2f8ade45b5d6b77cff5366600a6353
nveloso <42181@alunos.isel.ipl.pt>
2018-05-31 14:36:31 +0100

    Remove log from gis application spring boot

--- API/gis/src/main/java/pt/isel/ps/gis/GisApplication.java
+++ API/gis/src/main/java/pt/isel/ps/gis/GisApplication.java
@@ -1,14 +1,10 @@
 package pt.isel.ps.gis;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GisApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(GisApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(GisApplication.class, args);
