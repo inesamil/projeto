f06177f60f334bc76e41748f4f16cdbd0a28c79a
isel42142 <42142@alunos.isel.ipl.pt>
2018-05-31 23:44:59 +0100

    Update UserListsOutputModel

--- API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/UserListsOutputModel.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/outputModel/UserListsOutputModel.java
@@ -33,8 +33,8 @@ public class UserListsOutputModel {
         this.klass = initKlass();
         this.properties = initProperties(lists);
         this.entities = initEntities(username, lists);
-        this.actions = initActions();
-        this.links = initLinks();
+        this.actions = initActions(username);
+        this.links = initLinks(username);
     }
 
     // Initters
@@ -42,7 +42,7 @@ public class UserListsOutputModel {
         return new String[]{ENTITY_CLASS, "collection"};
     }
 
-    private Map<String, Object> initProperties(java.util.List<List> userLists) {
+    private Map<String, Object> initProperties(java.util.List<UserList> userLists) {
         HashMap<String, Object> properties = new HashMap<>();
         properties.put("size", userLists.size());
 
@@ -56,36 +56,35 @@ public class UserListsOutputModel {
             UserListId id = userList.getId();
 
             HashMap<String, Object> properties = new HashMap<>();
-            properties.put("house-id", id.getHouseId());
             properties.put("list-id", id.getListId());
             properties.put("list-name", userList.getList().getListName());
-            properties.put("user-username", userList.getUsersUsername());
+            properties.put("user-username", username);
             properties.put("list-shareable", userList.getListShareable());
 
-            String userListUri = UriBuilderUtils.buildUserListsUri(username);
+            String userListsUri = UriBuilderUtils.buildUserListsUri(username);
             entities[i] = new Entity(
                     new String[]{"user-list"},
                     new String[]{"item"},
                     properties,
                     null,
-                    new Link[]{new Link(new String[]{"self"}, new String[]{"user-list"}, userListUri)});
+                    new Link[]{new Link(new String[]{"self"}, new String[]{"user-list"}, userListsUri)});
         }
         return entities;
     }
 
-    private Action[] initActions(long houseId) {
+    private Action[] initActions(String username) {
         // Type
         String type = "application/json";
 
         // URIs
-        String listsUri = UriBuilderUtils.buildListsUri(houseId);
+        String userListsUri = UriBuilderUtils.buildUserListsUri(username);
 
         // POST List
-        Action postList = new Action(
+        Action postUserList = new Action(
                 "add-list",
                 "Add List",
                 Method.POST,
-                listsUri,
+                userListsUri,
                 type,
                 new Field[]{
                         new Field("list-name", Field.Type.number, null, "Name"),
@@ -93,18 +92,18 @@ public class UserListsOutputModel {
                 }
         );
 
-        return new Action[]{postList};
+        return new Action[]{postUserList};
     }
 
-    private Link[] initLinks(long houseId) {
+    private Link[] initLinks(String username) {
         // URIs
-        String houseUri = UriBuilderUtils.buildHouseUri(houseId);
-        String listsUri = UriBuilderUtils.buildListsUri(houseId);
+        String userUri = UriBuilderUtils.buildUserUri(username);
+        String listsUri = UriBuilderUtils.buildUserListsUri(username);
 
         // Link-self
         Link self = new Link(new String[]{"self"}, new String[]{ENTITY_CLASS, "collection"}, listsUri);
         //Link-related-house
-        Link houseLink = new Link(new String[]{"related"}, new String[]{"house"}, houseUri);
+        Link houseLink = new Link(new String[]{"related"}, new String[]{"user"}, userUri);
 
         return new Link[]{self, houseLink};
     }
