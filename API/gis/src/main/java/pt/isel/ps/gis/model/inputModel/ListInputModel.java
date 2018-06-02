56cfd7404054ee2f071959b13df81090adf50b3f
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-03 00:29:19 +0100

    Update ListInputModel

--- API/gis/src/main/java/pt/isel/ps/gis/model/inputModel/ListInputModel.java
+++ API/gis/src/main/java/pt/isel/ps/gis/model/inputModel/ListInputModel.java
@@ -4,25 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ListInputModel {
 
-    @JsonProperty("house-id")
-    private Long houseId;
-
-    @JsonProperty("user-username")
-    private String username;
-
     @JsonProperty("list-name")
     private String name;
 
     @JsonProperty("list-shareable")
     private Boolean shareable;
-
-    public Long getHouseId() {
-        return houseId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
 
     public String getName() {
         return name;
