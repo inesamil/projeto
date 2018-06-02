0773e2ef3beb3239e8629836235895c4a688f79a
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-02 19:06:06 +0100

    Add getCredentialsStore to ServiceLocator

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/ServiceLocator.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/ServiceLocator.kt
@@ -7,25 +7,22 @@ import ps.leic.isel.pt.gis.httpRequest.HttpWebService
 import ps.leic.isel.pt.gis.httpRequest.HttpWebServiceImpl
 import ps.leic.isel.pt.gis.repositories.Repository
 import ps.leic.isel.pt.gis.repositories.RepositoryImpl
+import ps.leic.isel.pt.gis.utils.CredentialsStore
+import ps.leic.isel.pt.gis.utils.CredentialsStoreImpl
 import ps.leic.isel.pt.gis.utils.SmartLock
 import ps.leic.isel.pt.gis.utils.SmartLockImpl
 
 object ServiceLocator {
 
     private var httpWebService: HttpWebService? = null
-    private const val CREDENTIALS_TAG = "credentials"
     private var repository: Repository? = null
     private var smartLock: SmartLock? = null
+    private var credentialsStore: CredentialsStore? = null
 
     fun getWebService(context: Context): HttpWebService {
         if (httpWebService == null)
             httpWebService = HttpWebServiceImpl(context)
         return httpWebService as HttpWebService
-    }
-
-    fun getUserCredentials(context: Context): String {
-        val mPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        return mPreferences.getString(CREDENTIALS_TAG, "")
     }
 
     fun getRepository(context: Context): Repository {
@@ -40,5 +37,12 @@ object ServiceLocator {
             smartLock = SmartLockImpl(context)
         }
         return smartLock as SmartLock
+    }
+
+    fun getCredentialsStore(context: Context) : CredentialsStore {
+        if (credentialsStore == null) {
+            credentialsStore = CredentialsStoreImpl(context)
+        }
+        return credentialsStore as CredentialsStore
     }
 }
