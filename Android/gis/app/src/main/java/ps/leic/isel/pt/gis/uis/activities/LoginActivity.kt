68db24cb80b3ceae45deb2c76f2da5efeda020d8
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-02 19:06:20 +0100

    Update LoginActivity

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/LoginActivity.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/LoginActivity.kt
@@ -1,17 +1,15 @@
 package ps.leic.isel.pt.gis.uis.activities
 
-import android.app.Activity
 import android.content.Intent
-import android.content.IntentSender
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.Toast
-import com.google.android.gms.common.api.ResolvableApiException
 import kotlinx.android.synthetic.main.activity_login.*
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.ServiceLocator
-import ps.leic.isel.pt.gis.utils.SmartLock
+import ps.leic.isel.pt.gis.utils.CredentialsStore
 
 class LoginActivity : AppCompatActivity() {
 
@@ -24,52 +22,27 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
-        signinBtn.setOnClickListener {
-            Log.i(TAG, usernameEditText.text.toString())
-            val username = usernameEditText.text.toString()
-            val password = passwordEditText.text.toString()
-            if (true) { //TODO: validate credentials
-                ServiceLocator.getSmartLock(applicationContext).storeCredentials(username, password,
-                        {
-                            // On Success
-                            Log.d(TAG, "SAVE: OK")
-                            Toast.makeText(this, "Credentials saved", Toast.LENGTH_SHORT).show()
-                        },
-                        {
-                            // On Exception
-                            if (it is ResolvableApiException) {
-                                // Try to resolve the save request. This will prompt the user if
-                                // the credential is new.
-                                val rae = it as ResolvableApiException?
-                                try {
-                                    rae!!.startResolutionForResult(this, SmartLock.RC_SAVE)
-                                } catch (e: IntentSender.SendIntentException) {
-                                    // Could not resolve the request
-                                    Log.e(TAG, "Failed to send resolution.", e)
-                                    Toast.makeText(this, "Save failed", Toast.LENGTH_SHORT).show()
-                                }
-                            } else {
-                                // Request has no resolution
-                                Toast.makeText(this, "Save failed", Toast.LENGTH_SHORT).show()
-                            }
-                        })
-                startActivity(Intent(this, HomeActivity::class.java))
-            } else {
-                Log.i(TAG, "Wrong credentials")
-                Toast.makeText(this, "Wrong Credentials", Toast.LENGTH_SHORT).show()
-            }
-        }
+        signinBtn.setOnClickListener(::onLoginClick)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SmartLock.RC_SAVE) {
-            if (resultCode == Activity.RESULT_OK) {
-                Log.d(TAG, "SAVE: OK")
-                Toast.makeText(this, "Credentials saved", Toast.LENGTH_SHORT).show()
-            } else {
-                Log.e(TAG, "SAVE: Canceled by user")
-            }
+    fun onLoginClick(view: View) {
+        val username = usernameEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        if (username.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please fill in the required fields", Toast.LENGTH_LONG).show()    //TODO: msg nas strings
+            return
+        }
+
+        if (true) { //TODO: validate credentials
+            val credentials: CredentialsStore.Credentials = CredentialsStore.Credentials(username, password)
+            ServiceLocator.getCredentialsStore(applicationContext).storeCredentials(credentials)
+            Log.i(TAG, "Credentials stored. Login succeeded.")
+            finish()
+            startActivity(Intent(this, HomeActivity::class.java))
+        } else {
+            Log.i(TAG, "Wrong credentials.")
+            Toast.makeText(this, "Wrong Credentials", Toast.LENGTH_SHORT).show()    //TODO: msg nas strings
         }
     }
 
