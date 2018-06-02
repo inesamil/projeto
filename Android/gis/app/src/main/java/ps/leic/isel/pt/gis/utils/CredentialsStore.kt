f66df5b52d882d1fb2466e01d9196b2b9cf6438c
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-02 19:05:36 +0100

    Add CredentialsStore and CredentialsStoreImpl

---
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/CredentialsStore.kt
@@ -0,0 +1,19 @@
+package ps.leic.isel.pt.gis.utils
+
+interface CredentialsStore {
+
+    fun getUsername() : String?
+
+    fun getCredentials() : Credentials?
+
+    fun storeCredentials(credentials: Credentials) : Boolean
+
+    fun deleteCredentials(credentials: Credentials) : Boolean
+
+    data class Credentials (val username: String, val password: String)
+
+    companion object {
+        const val USERNAME: String = "USERNAME"
+        const val PASSWORD: String = "PASSWORD"
+    }
+}
