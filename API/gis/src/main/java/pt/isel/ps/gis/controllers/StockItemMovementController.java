b8679279175361295195d7de1e7193f7673b4908
nveloso <42181@alunos.isel.ipl.pt>
2018-06-03 14:16:09 +0100

    Update stock item movement controller

--- API/gis/src/main/java/pt/isel/ps/gis/controllers/StockItemMovementController.java
+++ API/gis/src/main/java/pt/isel/ps/gis/controllers/StockItemMovementController.java
@@ -4,10 +4,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pt.isel.ps.gis.bll.HouseService;
 import pt.isel.ps.gis.bll.StockItemMovementService;
 import pt.isel.ps.gis.exceptions.BadRequestException;
 import pt.isel.ps.gis.exceptions.EntityException;
+import pt.isel.ps.gis.exceptions.EntityNotFoundException;
+import pt.isel.ps.gis.exceptions.NotFoundException;
 import pt.isel.ps.gis.model.StockItemMovement;
 import pt.isel.ps.gis.model.outputModel.MovementsOutputModel;
 import pt.isel.ps.gis.model.requestParams.StockItemMovementRequestParam;
@@ -20,22 +21,17 @@ import static pt.isel.ps.gis.utils.HeadersUtils.setSirenContentType;
 @RequestMapping("/v1/houses/{house-id}/movements")
 public class StockItemMovementController {
 
-    private static final String HOUSE_NOT_EXIST = "House does not exist.";
-
     private final StockItemMovementService stockItemMovementService;
-    private final HouseService houseService;
 
-    public StockItemMovementController(StockItemMovementService stockItemMovementService, HouseService houseService) {
+    public StockItemMovementController(StockItemMovementService stockItemMovementService) {
         this.stockItemMovementService = stockItemMovementService;
-        this.houseService = houseService;
     }
 
     @GetMapping("")
     public ResponseEntity<MovementsOutputModel> getMovements(
             @PathVariable("house-id") long houseId,
             StockItemMovementRequestParam params
-    ) throws BadRequestException {
-        checkHouse(houseId);
+    ) throws BadRequestException, NotFoundException {
         List<StockItemMovement> movements;
         try {
             if (params.isNull())
@@ -51,6 +47,8 @@ public class StockItemMovementController {
             }
         } catch (EntityException e) {
             throw new BadRequestException(e.getMessage());
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundException(e.getMessage());
         }
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<>(new MovementsOutputModel(houseId, movements), setSirenContentType(headers),
@@ -60,25 +58,18 @@ public class StockItemMovementController {
     @PostMapping("")
     public ResponseEntity<MovementsOutputModel> postMovement(
             @PathVariable("house-id") long houseId
-    ) throws BadRequestException {
+    ) throws BadRequestException, NotFoundException {
         // TODO falta definir o body
         List<StockItemMovement> movements;
         try {
             movements = stockItemMovementService.getStockItemMovementsByHouseId(houseId);
         } catch (EntityException e) {
             throw new BadRequestException(e.getMessage());
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundException(e.getMessage());
         }
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<>(new MovementsOutputModel(houseId, movements), setSirenContentType(headers),
                 HttpStatus.OK);
-    }
-
-    private void checkHouse(long houseId) throws BadRequestException {
-        try {
-            if (!houseService.existsHouseByHouseId(houseId))
-                throw new BadRequestException(HOUSE_NOT_EXIST);
-        } catch (EntityException e) {
-            throw new BadRequestException(e.getMessage());
-        }
     }
 }
