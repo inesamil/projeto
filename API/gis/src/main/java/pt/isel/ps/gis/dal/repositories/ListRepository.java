b4c27bc1eb37620c539720a582073cb80fde653d
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-03 12:34:02 +0100

    Add findAllById_HouseId(house_id) to ListRepository

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/ListRepository.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/ListRepository.java
@@ -5,4 +5,12 @@ import pt.isel.ps.gis.model.List;
 import pt.isel.ps.gis.model.ListId;
 
 public interface ListRepository extends CrudRepository<List, ListId>, ListRepositoryCustom {
+
+    /**
+     * Finds all lists by house ID
+     *
+     * @param houseId identificador da casa
+     * @return java.util.List of list of the house with ID {houseId}
+     */
+    java.util.List<List> findAllById_HouseId(long houseId); //TODO
 }
