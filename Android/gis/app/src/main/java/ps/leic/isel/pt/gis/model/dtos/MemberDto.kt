5ab264cd6ee69a221e3ac1be16c6d8c1c12bac53
isel42142 <42142@alunos.isel.ipl.pt>
2018-06-05 15:40:24 +0100

    Update MemberDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/MemberDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/MemberDto.kt
@@ -1,32 +1,11 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Link
 import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Siren
 
-class MemberDto(siren: Siren) {
-    val houseId: Long
-    val username: String
-    val administrator: Boolean
-    val links: MemberLynks
-
-    init {
-        val properties = siren.properties
-        houseId = (properties?.get(houseIdLabel) as Int).toLong()
-        username = properties[usernameLabel] as String
-        administrator = properties[adminstratorLabel] as Boolean
-        links = MemberLynks(siren.links)
-    }
-
-    class MemberLynks(links: Array<Link>?) {
-        val userLink: String? = links?.find {
-            it.klass?.contains(userLabel) ?: false
-        }?.href
-    }
-
-    companion object {
-        private const val houseIdLabel: String = "house-id"
-        private const val usernameLabel: String = "user-username"
-        private const val adminstratorLabel: String = "household-administrator"
-        private const val userLabel: String = "user"
-    }
-}
+data class MemberDto (
+    @field:JsonProperty(value = "house-id") val houseId: Long,
+    @field:JsonProperty(value = "user-username") val username: String,
+    @field:JsonProperty(value = "administrator") val administrator: Boolean
+)
