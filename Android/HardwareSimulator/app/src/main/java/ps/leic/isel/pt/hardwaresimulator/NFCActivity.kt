dcacbb5bcb5e7f2c0653a961cdd1e0525c2127cb
isel42142 <42142@alunos.isel.ipl.pt>
2018-06-06 20:50:14 +0100

    update simulator

--- Android/HardwareSimulator/app/src/main/java/ps/leic/isel/pt/hardwaresimulator/NFCActivity.kt
+++ Android/HardwareSimulator/app/src/main/java/ps/leic/isel/pt/hardwaresimulator/NFCActivity.kt
@@ -5,6 +5,7 @@ import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_nfc.*
 import ps.leic.isel.pt.hardwaresimulator.utils.NFCUtils
@@ -19,6 +20,19 @@ class NFCActivity : AppCompatActivity(), Listener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfc)
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        val houses = arrayOf("Santos", "Oliveira")
+        val storages = arrayOf("Frigorífico")
+
+        val housesSpinnerAdapter = ArrayAdapter<String>(housesSpinner.context, android.R.layout.simple_spinner_item, houses)
+        housesSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        housesSpinner.adapter = housesSpinnerAdapter
+        housesSpinner.setSelection(0)
+
+        val storagesSpinnerAdapter = ArrayAdapter<String>(storagesSpinner.context, android.R.layout.simple_spinner_item, storages)
+        storagesSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        storagesSpinner.adapter = storagesSpinnerAdapter
+        storagesSpinner.setSelection(0)
 
         readBtn.setOnClickListener {
             readFragment = supportFragmentManager.findFragmentByTag(ReadFragment.TAG) as? ReadFragment
