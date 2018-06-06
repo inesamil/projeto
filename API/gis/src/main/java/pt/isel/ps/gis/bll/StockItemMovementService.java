361a262f4ea68db3bf624a63186bd67328f727d8
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-06 12:22:22 +0100

    Update StockItemMovementService

--- API/gis/src/main/java/pt/isel/ps/gis/bll/StockItemMovementService.java
+++ API/gis/src/main/java/pt/isel/ps/gis/bll/StockItemMovementService.java
@@ -2,6 +2,7 @@ package pt.isel.ps.gis.bll;
 
 import pt.isel.ps.gis.exceptions.EntityException;
 import pt.isel.ps.gis.exceptions.EntityNotFoundException;
+import pt.isel.ps.gis.model.StockItem;
 import pt.isel.ps.gis.model.StockItemMovement;
 
 import java.sql.Timestamp;
@@ -45,18 +46,23 @@ public interface StockItemMovementService {
     /**
      * Adicionar um movimento à casa
      *
-     * @param houseId
-     * @param storageId
-     * @param productName
-     * @param brand
-     * @param variety
-     * @param segment
-     * @param quantity
-     * @return
+     * @param houseId                identificador da casa
+     * @param storageId              identificador do local de armazenamento
+     * @param movementType           indicador do tipo de movimento (true - entrada; false - saída)
+     * @param quantity               quantidade de entrada ou de saída
+     * @param productName            nome do produto
+     * @param brand                  marca
+     * @param variety                variedade
+     * @param segment                segmento
+     * @param conservationConditions condições de conservação (Opcional)
+     * @param description            descrição (Opcional)
+     * @param date                   data de validade
+     * @return StockItem
      * @throws EntityException
      * @throws EntityNotFoundException
      */
-    StockItemMovement addStockItemMovement(long houseId, short storageId, String productName, String brand, String variety, String segment, String date, boolean movementType, short quantity) throws EntityException, EntityNotFoundException;
+    StockItem addStockItemMovement(long houseId, short storageId, boolean movementType, short quantity, String productName, String brand, String variety, String segment,
+                                   String conservationConditions, String description, String date) throws EntityException, EntityNotFoundException;
 
     /**
      * Filtros - filtragem dos movimentos
