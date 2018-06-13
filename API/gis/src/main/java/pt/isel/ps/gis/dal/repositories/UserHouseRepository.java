cc4a95fc654c54c82d0480b7e39d1e266c2681b0
nveloso <42181@alunos.isel.ipl.pt>
2018-06-13 16:55:28 +0100

    Add exists by house id and username method

--- API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/UserHouseRepository.java
+++ API/gis/src/main/java/pt/isel/ps/gis/dal/repositories/UserHouseRepository.java
@@ -9,6 +9,15 @@ import java.util.List;
 public interface UserHouseRepository extends CrudRepository<UserHouse, UserHouseId> {
 
     /**
+     * Verify if house member exists in house with param houseId and with param username
+     *
+     * @param houseId  The id of the house
+     * @param username The username of the user
+     * @return true if user is member of the house, otherwise false
+     */
+    boolean existsById_HouseIdAndUsersByUsersId_UsersUsername(Long houseId, String username);
+
+    /**
      * Find all household associated with specific house
      *
      * @param houseId The id of the house
