8f12bc97ec35ae1f5fb722ea4187a98fe4b5c841
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-13 18:45:05 +0100

    Update and Rename HouseAllergyBody

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/body/HouseAllergyBody.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/body/HouseAllergyBody.kt
@@ -2,7 +2,7 @@ package ps.leic.isel.pt.gis.model.body
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class AllergyBody(
-        @field:JsonProperty(value = "allergy") val allergen: String,
+data class HouseAllergyBody(
+        @field:JsonProperty(value = "allergy") val allergy: String,
         @field:JsonProperty(value = "allergics-number") val allergics: Short
 )
