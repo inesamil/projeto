d6faa0e3c06b5b38114e5d98de8b94281c6cdc00
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-13 23:52:55 +0100

    Update HeaderUtils

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/HeadersUtils.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/HeadersUtils.kt
@@ -4,8 +4,47 @@ import android.util.Base64
 
 object HeadersUtils {
 
-    fun getAuthorizationHeader(username: String, password: String) : String {
+    /**
+     * AUTHORIZATION HEADER
+     */
+    const val AUTHORIZATION: String = "Authorization"
+
+    fun getAuthorizationHeader(headers: Map<String, String>) : String? {
+        return headers[AUTHORIZATION]
+    }
+
+    fun setAuthorizationHeader(headers: MutableMap<String, String>, username: String, password: String) {
         val encodedUsernamePassword = Base64.encodeToString("$username:$password".toByteArray(), Base64.DEFAULT)
-        return "Basic $encodedUsernamePassword"
+        headers[AUTHORIZATION] = "Basic $encodedUsernamePassword"
+    }
+
+    /**
+     * COOKIE HEADER
+     */
+    const val COOKIE: String = "Cookie"
+
+    fun getCookieHeader(headers: Map<String, String>) : String? {
+        return headers[COOKIE]
+    }
+
+    fun setCookieHeader(headers: MutableMap<String, String>, cookies: List<Pair<String, String>>) {
+        CookieUtils.getCookieString(cookies)?.let {
+            headers[COOKIE] = it
+        }
+    }
+
+    /**
+     * CONTENT-TYPE HEADER
+     */
+    const val CONTENT_TYPE: String = "Content-Type"
+
+    fun getContentTypeHeader(headers: Map<String, String>) : String? {
+        return headers[CONTENT_TYPE]
+    }
+
+    fun setContentTypeHeader(headers: MutableMap<String, String>, contentType: String?) {
+        contentType?.let {
+            headers[CONTENT_TYPE] = it
+        }
     }
 }
