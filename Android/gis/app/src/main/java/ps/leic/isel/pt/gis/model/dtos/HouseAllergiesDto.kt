337b44b08c7b19cd5611592e6db330332db36402
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-13 18:46:03 +0100

    Add actions to HouseAllergiesDto

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/HouseAllergiesDto.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/dtos/HouseAllergiesDto.kt
@@ -1,10 +1,13 @@
 package ps.leic.isel.pt.gis.model.dtos
 
+import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Action
 import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Link
 import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Siren
+import ps.leic.isel.pt.gis.model.ActionDto
 
 class HouseAllergiesDto(siren: Siren) {
     val houseAllergies: Array<HouseAllergyDto>
+    val actions: HouseAllergiesActions
     val links: HouseAllergiesLinks
 
     init {
@@ -12,7 +15,22 @@ class HouseAllergiesDto(siren: Siren) {
         houseAllergies = entities?.map {
             HouseAllergyDto(Siren(it.klass, it.properties, null, it.actions, it.links))
         }.orEmpty().toTypedArray()
+        actions = HouseAllergiesActions(siren.actions)
         links = HouseAllergiesLinks(siren.links)
+    }
+
+    class HouseAllergiesActions(actions: Array<Action>?) {
+        var updateHouseAllergies: ActionDto? = null
+        var deleteHouseAllergies: ActionDto? = null
+
+        init {
+            actions?.find { it.name == updateHouseAllergiesLabel }?.let {
+                updateHouseAllergies = ActionDto(it.name, it.href, it.type)
+            }
+            actions?.find { it.name == deleteHouseAllergiesLabel }?.let {
+                deleteHouseAllergies = ActionDto(it.name, it.href, it.type)
+            }
+        }
     }
 
     class HouseAllergiesLinks(links: Array<Link>?) {
@@ -27,6 +45,8 @@ class HouseAllergiesDto(siren: Siren) {
     companion object {
         private const val houseAllergiesClassLabel: String = "house-allergie"
         private const val houseLabel: String = "house"
+        private const val updateHouseAllergiesLabel: String = "update-house-allergies"
+        private const val deleteHouseAllergiesLabel: String = "delete-house-allergies"
     }
 
 }
