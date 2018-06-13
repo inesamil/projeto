ab5efcbb5c71ae660e3cb32bb7d019bfcab8d42a
nveloso <42181@alunos.isel.ipl.pt>
2018-06-13 19:19:46 +0100

    Add entity already exists exception

---
+++ API/gis/src/main/java/pt/isel/ps/gis/exceptions/EntityAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package pt.isel.ps.gis.exceptions;
+
+public class EntityAlreadyExistsException extends Exception {
+
+    public EntityAlreadyExistsException(String message) {
+        super(message);
+    }
+}
