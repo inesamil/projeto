9ce2dc6debfdc903170d3132c4a50e45028c111b
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-13 18:44:03 +0100

    Update AllergiesViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/AllergiesViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/AllergiesViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import ps.leic.isel.pt.gis.ServiceLocator
+import ps.leic.isel.pt.gis.model.body.HouseAllergiesBody
 import ps.leic.isel.pt.gis.model.dtos.HouseAllergiesDto
 import ps.leic.isel.pt.gis.model.dtos.HouseAllergyDto
 import ps.leic.isel.pt.gis.repositories.Resource
@@ -21,14 +22,21 @@ class AllergiesViewModel(private val app: Application) : AndroidViewModel(app) {
         return allergies
     }
 
-    fun addHouse(allergy: Any): LiveData<Resource<HouseAllergyDto>>? {
-        //TODO
-        /*allergies?.value?.data?.actions?.addHouse?.let {
-            val str = mapper.writeValueAsString(house)
+    fun addHouseAllergies(houseAllergies: HouseAllergiesBody): LiveData<Resource<HouseAllergyDto>>? {
+        allergies?.value?.data?.actions?.updateHouseAllergies?.let {
             return ServiceLocator
                     .getRepository(app.applicationContext)
-                    .create(HouseDto::class.java, it.url, it.contentType, str, TAG)
-        }*/
+                    .create(HouseAllergyDto::class.java, it.url, it.contentType, houseAllergies, TAG)
+        }
+        return null
+    }
+
+    fun deleteAllHouseAllergies() : LiveData<Resource<HouseAllergiesDto>>? {
+        allergies?.value?.data?.actions?.deleteHouseAllergies?.let {
+            return ServiceLocator
+                    .getRepository(app.applicationContext)
+                    .delete(HouseAllergiesDto::class.java, it.url, TAG)
+        }
         return null
     }
 
