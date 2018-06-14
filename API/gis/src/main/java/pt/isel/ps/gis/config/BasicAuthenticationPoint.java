717c280cc82ad1265319616f20a6a1b111961e4b
nveloso <42181@alunos.isel.ipl.pt>
2018-06-14 18:41:17 +0100

    response is done in basic authentication endpoint

--- API/gis/src/main/java/pt/isel/ps/gis/config/BasicAuthenticationPoint.java
+++ API/gis/src/main/java/pt/isel/ps/gis/config/BasicAuthenticationPoint.java
@@ -1,13 +1,18 @@
 package pt.isel.ps.gis.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import pt.isel.ps.gis.hypermedia.problemPlusJson.ProblemPlusJson;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class BasicAuthenticationPoint extends BasicAuthenticationEntryPoint {
@@ -24,7 +29,15 @@ public class BasicAuthenticationPoint extends BasicAuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-        // TODO enviar mensagem em problem detail
+        response.setContentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE);
+        HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
+        ProblemPlusJson problemPlusJson = new ProblemPlusJson(
+                "Requires authentication.",
+                httpStatus.value(),
+                "You need to authenticate first.");
+        String body = new ObjectMapper().writeValueAsString(problemPlusJson);
+        PrintWriter writer = response.getWriter();
+        writer.println(body);
+        writer.flush();
     }
 }
