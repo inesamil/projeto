a3f15c797998a7f1a165cdceee75141d4fd2e45e
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-14 18:54:24 +0100

    Update BasicInformationViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/BasicInformationViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/BasicInformationViewModel.kt
@@ -3,7 +3,9 @@ package ps.leic.isel.pt.gis.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import ps.leic.isel.pt.gis.GisApplication
 import ps.leic.isel.pt.gis.ServiceLocator
+import ps.leic.isel.pt.gis.model.body.UserBody
 import ps.leic.isel.pt.gis.model.dtos.UserDto
 import ps.leic.isel.pt.gis.repositories.Resource
 
@@ -19,6 +21,18 @@ class BasicInformationViewModel(private val app: Application) : AndroidViewModel
 
     fun getUser(): LiveData<Resource<UserDto>>? {
         return users
+    }
+
+
+    fun addUser(user: UserBody) : LiveData<Resource<UserDto>>? {
+        val gisApplication = app as GisApplication
+        val index = gisApplication.index
+        index.getUsersAction()?.let {
+            return ServiceLocator
+                    .getRepository(app.applicationContext)
+                    .create(UserDto::class.java, it.url, it.contentType, user, SplashScreenViewModel.TAG)
+        }
+        return null
     }
 
     fun cancel() {
