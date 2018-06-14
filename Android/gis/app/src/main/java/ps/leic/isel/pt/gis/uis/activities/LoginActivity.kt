15c93367adca9a554d1682134be234fed60633b4
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-14 18:02:58 +0100

    Update LoginActivity

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/LoginActivity.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/LoginActivity.kt
@@ -1,5 +1,7 @@
 package ps.leic.isel.pt.gis.uis.activities
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -7,11 +9,17 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import ps.leic.isel.pt.gis.GisApplication
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.ServiceLocator
-import ps.leic.isel.pt.gis.utils.CredentialsStore
+import ps.leic.isel.pt.gis.model.dtos.UserDto
+import ps.leic.isel.pt.gis.repositories.Status
+import ps.leic.isel.pt.gis.viewModel.BasicInformationViewModel
 
 class LoginActivity : AppCompatActivity() {
+
+    private var url: String? = null
+    private var basicInformationViewModel: BasicInformationViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,27 +33,49 @@ class LoginActivity : AppCompatActivity() {
         signinBtn.setOnClickListener(::onLoginClick)
     }
 
-    fun onLoginClick(view: View) {
+    private fun onLoginClick(view: View) {
         val username = usernameEditText.text.toString()
         val password = passwordEditText.text.toString()
 
-        if (username.isEmpty() || password.isEmpty()){
+        if (username.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, getString(R.string.please_fill_in_all_required_fields), Toast.LENGTH_LONG).show()
             return
         }
 
-        if (true) { //TODO: validate credentials
-            ServiceLocator.getCredentialsStore(applicationContext).storeUsername(username)
-            Log.i(TAG, "Credentials stored. Login succeeded.")
-            finish()
-            startActivity(Intent(this, HomeActivity::class.java))
-        } else {
-            Log.i(TAG, "Wrong credentials.")
-            Toast.makeText(this, getString(R.string.wrong_credentials), Toast.LENGTH_SHORT).show()
+        val gisApplication = application as GisApplication
+        gisApplication.index
+
+        url = gisApplication.index.getUserUrl(username)
+
+        url?.let {
+            basicInformationViewModel = ViewModelProviders.of(this).get(BasicInformationViewModel::class.java)
+            basicInformationViewModel?.init(it)
+            basicInformationViewModel?.getUser()?.observe(this, Observer {
+                when {
+                    it?.status == Status.SUCCESS -> onSuccess(it.data!!)
+                    it?.status == Status.ERROR -> onError(it.message!!)
+                }
+            })
         }
     }
 
+    private fun onSuccess(user: UserDto) {
+        user.username?.let {
+            ServiceLocator.getCredentialsStore(applicationContext).storeUsername(user.username)
+            Log.i(TAG, "Credentials stored. Login succeeded.")
+            finish()
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+
+    private fun onError(message: String) {
+        //TODO: pode não ter sido wrong credentials é preciso verificar
+        Log.i(TAG, "Wrong credentials.")
+        Toast.makeText(this, getString(R.string.wrong_credentials), Toast.LENGTH_SHORT).show()
+    }
+
     companion object {
-        private const val TAG: String = "LoginActivity"
+        const val TAG: String = "LoginActivity"
+
     }
 }
