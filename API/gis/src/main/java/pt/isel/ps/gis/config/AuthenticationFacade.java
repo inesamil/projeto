5f840827689327fa24f384d28fa8c4eeb040e14d
nveloso <42181@alunos.isel.ipl.pt>
2018-06-14 16:49:16 +0100

    Update authentication facade

--- API/gis/src/main/java/pt/isel/ps/gis/config/AuthenticationFacade.java
+++ API/gis/src/main/java/pt/isel/ps/gis/config/AuthenticationFacade.java
@@ -2,13 +2,7 @@ package pt.isel.ps.gis.config;
 
 import org.springframework.security.core.Authentication;
 
-import java.util.Optional;
-
 public interface AuthenticationFacade {
 
     Authentication getAuthentication();
-
-    boolean isAuthenticated();
-
-    Optional<String> getCurrentUsernameIfAuthenticated();
 }
