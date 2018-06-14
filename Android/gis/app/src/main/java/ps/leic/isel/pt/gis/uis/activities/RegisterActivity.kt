be8af7ef45ea17f0b38d7c6a5747c5bee766ef66
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-14 19:04:52 +0100

    Update RegisterActivity

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/RegisterActivity.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/RegisterActivity.kt
@@ -1,22 +1,33 @@
 package ps.leic.isel.pt.gis.uis.activities
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_register.*
+import ps.leic.isel.pt.gis.GisApplication
 import ps.leic.isel.pt.gis.R
+import ps.leic.isel.pt.gis.ServiceLocator
+import ps.leic.isel.pt.gis.model.body.UserBody
+import ps.leic.isel.pt.gis.model.dtos.UserDto
+import ps.leic.isel.pt.gis.repositories.Status
 import ps.leic.isel.pt.gis.utils.SmartLock
+import ps.leic.isel.pt.gis.viewModel.BasicInformationViewModel
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var url: String? = null
+    private var basicInformationViewModel: BasicInformationViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        signupBtn.setOnClickListener {
-
-        }
+        signupBtn.setOnClickListener(::onSignup)
 
         signinBtn.setOnClickListener {
             finish()
@@ -24,17 +35,40 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SmartLock.RC_SAVE)
-            Log.d(TAG, "SAVE: OK")
-        else
-            Log.e(TAG, "SAVE: Canceled by user")
+    private fun onSignup(view: View) {
+        val name = fullnameEditText.text.toString()
+        val username = usernameEditText.text.toString()
+        val email = emailEditText.text.toString()
+        val age = ageEditText.text.toString().toShort()
+        val password = passwordEditText.text.toString()
+
+        if (name.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, getString(R.string.please_fill_in_all_required_fields), Toast.LENGTH_LONG).show()
+            return
+        }
+
+        basicInformationViewModel = ViewModelProviders.of(this).get(BasicInformationViewModel::class.java)
+        basicInformationViewModel?.addUser(UserBody(username, name, email, age, password))?.observe(this, Observer {
+            when {
+                it?.status == Status.SUCCESS -> onSuccess(it.data!!)
+                it?.status == Status.ERROR -> onError(it.message!!)
+            }
+        })
     }
 
-    private fun goToInitialSetup() {
-        finish()
-        startActivity(Intent(this, InitialSetupActivity::class.java))
+    private fun onSuccess(user: UserDto) {
+        Log.i(TAG, "Registe user with success")
+        user.username?.let {
+            ServiceLocator.getCredentialsStore(applicationContext).storeUsername(user.username)
+            Log.i(TAG, "Credentials stored. Login succeeded.")
+            finish()
+            startActivity(Intent(this, InitialSetupActivity::class.java))
+        }
+    }
+
+    private fun onError(message: String) {
+        Log.i(LoginActivity.TAG, "Unable to registe user")
+        Toast.makeText(this, getString(R.string.registe_user_failed), Toast.LENGTH_SHORT).show()
     }
 
     companion object {
