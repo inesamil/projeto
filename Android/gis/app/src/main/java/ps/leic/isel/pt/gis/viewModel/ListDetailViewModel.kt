6e75b5caed554f30da0889d4121c0922a24603c4
isel42162 <42162@alunos.isel.ipl.pt>
2018-06-24 23:26:16 +0100

    Update ListDetailViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ListDetailViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/ListDetailViewModel.kt
@@ -4,20 +4,20 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import ps.leic.isel.pt.gis.ServiceLocator
-import ps.leic.isel.pt.gis.model.dtos.ProductsListDto
+import ps.leic.isel.pt.gis.model.dtos.ListDto
 import ps.leic.isel.pt.gis.repositories.Resource
 
 class ListDetailViewModel(private val app: Application) : AndroidViewModel(app) {
 
-    private var listDetail: LiveData<Resource<ProductsListDto>>? = null
+    private var listDetail: LiveData<Resource<ListDto>>? = null
 
     fun init(url: String) {
         if (listDetail != null) return
         listDetail = ServiceLocator.getRepository(app.applicationContext)
-                .get(ProductsListDto::class.java, url, TAG)
+                .get(ListDto::class.java, url, TAG)
     }
 
-    fun getListDetail(): LiveData<Resource<ProductsListDto>>? {
+    fun getListDetail(): LiveData<Resource<ListDto>>? {
         return listDetail
     }
 
