7f207c8ac19d6b24a54f3376a269ac2402204381
nveloso <42181@alunos.isel.ipl.pt>
2018-06-24 16:34:42 +0100

    Add example of internationalization to api

--- API/gis/src/main/java/pt/isel/ps/gis/GisApplication.java
+++ API/gis/src/main/java/pt/isel/ps/gis/GisApplication.java
@@ -2,16 +2,21 @@ package pt.isel.ps.gis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pt.isel.ps.gis.stockAlgorithm.BasicStockManagementAlgorithm;
 import pt.isel.ps.gis.stockAlgorithm.StockManagementAlgorithm;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class GisApplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(GisApplication.class, args);
     }
 
@@ -23,5 +28,12 @@ public class GisApplication {
     @Bean
     public StockManagementAlgorithm stockManagementAlgorithm() {
         return new BasicStockManagementAlgorithm();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("locales/messages");
+        return messageSource;
     }
 }
--- API/gis/src/main/java/pt/isel/ps/gis/controllers/IndexController.java
+++ API/gis/src/main/java/pt/isel/ps/gis/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package pt.isel.ps.gis.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.isel.ps.gis.model.outputModel.IndexOutputModel;
 
+import java.util.Locale;
+
 import static pt.isel.ps.gis.utils.HeadersUtils.setJsonHomeContentType;
 
 @RestController
 @RequestMapping("/v1")
 public class IndexController {
 
+    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
+    private final MessageSource messageSource;
+
+    public IndexController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("")
-    public ResponseEntity<IndexOutputModel> getIndex() {
+    public ResponseEntity<IndexOutputModel> getIndex(Locale locale) {
+        log.info(messageSource.getMessage("test", null, locale));
+        log.info(messageSource.getMessage("test.abc", new Object[]{"xpto"}, locale));
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<>(new IndexOutputModel(), setJsonHomeContentType(headers), HttpStatus.OK);
     }
