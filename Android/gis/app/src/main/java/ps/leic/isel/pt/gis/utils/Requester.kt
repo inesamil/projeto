8e2f4a2742efbb1a43d194f9c0ce8f0d3b745d77
nveloso <42181@alunos.isel.ipl.pt>
2018-06-27 21:44:33 +0100

    Add problem details to requester

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/Requester.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/Requester.kt
@@ -6,38 +6,46 @@ import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonRequest
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import ps.leic.isel.pt.gis.hypermedia.jsonHome.subentities.JsonHome
-import ps.leic.isel.pt.gis.hypermedia.siren.subentities.Siren
+import ps.leic.isel.pt.gis.hypermedia.jsonHome.JsonHome
+import ps.leic.isel.pt.gis.hypermedia.problemDetails.ProblemDetails
+import ps.leic.isel.pt.gis.hypermedia.siren.Siren
+import ps.leic.isel.pt.gis.repositories.Resource
 import java.io.IOException
 
 
-class Requester<DTO>(method: Int, url: String, body: Any?, private val headers: MutableMap<String, String>,
-                     private val dtoType: Class<DTO>, onSuccess: (DTO) -> Unit,
-                     onError: (VolleyError?) -> Unit)
-    : JsonRequest<DTO>(method, UrlUtils.parseUrl(url), mapper.writeValueAsString(body), onSuccess, onError) {
+class Requester<T>(
+        method: Int, url: String, body: Any?, private val headers: MutableMap<String, String>,
+        private val dtoType: Class<T>, onSuccess: (Resource<T>) -> Unit,
+        onError: (VolleyError?) -> Unit
+) : JsonRequest<Resource<T>>(
+        method, UrlUtils.parseUrl(url), mapper.writeValueAsString(body), onSuccess, onError
+) {
 
     companion object {
         val mapper: ObjectMapper = jacksonObjectMapper()
-        val hypermediaClasses: HashMap<String, Class<*>> = hashMapOf(
-                Pair("application/vnd.siren+json", Siren::class.java),
-                Pair("application/home+json", JsonHome::class.java)
+        private const val SIREN_JSON = "application/vnd.siren+json"
+        private const val HOME_JSON = "application/home+json"
+        private const val PROBLEM_JSON = "application/problem+json"
+        private val hypermediaClasses: HashMap<String, Class<*>> = hashMapOf(
+                Pair(SIREN_JSON, Siren::class.java),
+                Pair(HOME_JSON, JsonHome::class.java),
+                Pair(PROBLEM_JSON, ProblemDetails::class.java)
         )
-        const val encoding: String = "UTF-8"
     }
 
     /**
      *  The function that parses the response to a DTO (generic)
      */
-    override fun parseNetworkResponse(response: NetworkResponse): Response<DTO> {
+    override fun parseNetworkResponse(response: NetworkResponse): Response<Resource<T>> {
         return try {
-            val cookies = response.headers["set-cookie"]
-
-
             val hypermediaType = response.headers["content-type"]
             val hypermedia = mapper.readValue(response.data, hypermediaClasses[hypermediaType])
             val constructor = dtoType.getConstructor(hypermediaClasses[hypermediaType])
             val dto = constructor.newInstance(hypermedia)
-            Response.success(dto, null)
+            if (hypermediaType == PROBLEM_JSON)
+                Response.success(Resource.error(dto), null)
+            else
+                Response.success(Resource.success(dto), null)
         } catch (e: IOException) {
             Response.error(VolleyError(e))
         }
