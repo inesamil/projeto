4b499698c8fdbb25d9b1302bc6c9ceb24596f633
nveloso <42181@alunos.isel.ipl.pt>
2018-06-27 21:44:47 +0100

    Add more methods to resource class

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/Resource.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/Resource.kt
@@ -14,8 +14,24 @@ class Resource<out T> private constructor(val status: Status, val data: T?, val 
             return Resource(Status.SUCCESS, data, null)
         }
 
+        fun <T> success(msg: String?): Resource<T> {
+            return Resource(Status.SUCCESS, null, msg)
+        }
+
+        fun <T> success(data: T, msg: String?): Resource<T> {
+            return Resource(Status.SUCCESS, data, msg)
+        }
+
         fun <T> error(msg: String?): Resource<T> {
             return Resource(Status.ERROR, null, msg)
+        }
+
+        fun <T> error(error: T): Resource<T> {
+            return Resource(Status.ERROR, error, null)
+        }
+
+        fun <T> error(error: T, msg: String?): Resource<T> {
+            return Resource(Status.ERROR, error, msg)
         }
 
         fun <T> loading(): Resource<T> {
