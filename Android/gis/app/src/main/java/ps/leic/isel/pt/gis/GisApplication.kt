e7d1dab426a52d5a05ba78568392a2e4713926c8
nveloso <42181@alunos.isel.ipl.pt>
2018-06-27 12:21:48 +0100

    Adds check internet connection

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/GisApplication.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/GisApplication.kt
@@ -1,11 +1,13 @@
 package ps.leic.isel.pt.gis
 
+import android.content.Context
 import android.content.res.Configuration
+import android.net.ConnectivityManager
 import android.support.multidex.MultiDexApplication
 import ps.leic.isel.pt.gis.model.dtos.IndexDto
 import java.util.*
 
-class GisApplication : MultiDexApplication () {
+class GisApplication : MultiDexApplication() {
 
     @Volatile
     lateinit var language: String
@@ -21,5 +23,16 @@ class GisApplication : MultiDexApplication () {
     override fun onConfigurationChanged(newConfig: Configuration?) {
         super.onConfigurationChanged(newConfig)
         language = Locale.getDefault().language
+    }
+
+    /**
+     * Checks if the device has internet connection.
+     *
+     * @return true if the phone is connected to the internet, otherwise false
+     */
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 }
