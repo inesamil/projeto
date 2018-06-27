eca9088f690e9c6298c24a5463c11b1ed871b54e
nveloso <42181@alunos.isel.ipl.pt>
2018-06-27 21:43:29 +0100

    Move siren class from subentity to siren

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/hypermedia/siren/subentities/Siren.kt
+++
@@ -1,9 +0,0 @@
-package ps.leic.isel.pt.gis.hypermedia.siren.subentities
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-class Siren(@field:JsonProperty("class") val klass: Array<String>?,
-            val properties: HashMap<String, Any>?, val entities: Array<Entity>?,
-            val actions: Array<Action>?, val links: Array<Link>?)
