2c0129faf1f22a08a0b8ec3ee6c20892dd3780f3
nveloso <42181@alunos.isel.ipl.pt>
2018-07-01 13:18:46 +0100

    Update invitation controller

--- API/gis/src/main/java/pt/isel/ps/gis/controllers/InvitationController.java
+++ API/gis/src/main/java/pt/isel/ps/gis/controllers/InvitationController.java
@@ -1,19 +1,13 @@
 package pt.isel.ps.gis.controllers;
 
-
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pt.isel.ps.gis.bll.InvitationService;
-import pt.isel.ps.gis.components.AuthenticationFacade;
-import pt.isel.ps.gis.exceptions.BadRequestException;
-import pt.isel.ps.gis.exceptions.EntityException;
-import pt.isel.ps.gis.exceptions.EntityNotFoundException;
-import pt.isel.ps.gis.exceptions.NotFoundException;
+import pt.isel.ps.gis.exceptions.*;
 import pt.isel.ps.gis.model.Invitation;
-import pt.isel.ps.gis.model.inputModel.InvitationInputModel;
 import pt.isel.ps.gis.model.outputModel.InvitationsOutputModel;
 
 import java.util.List;
@@ -25,47 +19,23 @@ import static pt.isel.ps.gis.utils.HeadersUtils.setSirenContentType;
 @RequestMapping("/v1/invitations")
 public class InvitationController {
 
+    private static final String ACCEPT_ERROR_MSG = "Accept must be non-null";
+
     private final InvitationService invitationService;
-
-    private final AuthenticationFacade authenticationFacade;
-
     private final MessageSource messageSource;
 
-    public InvitationController(InvitationService invitationService, AuthenticationFacade authenticationFacade, MessageSource messageSource) {
+    public InvitationController(InvitationService invitationService, MessageSource messageSource) {
         this.invitationService = invitationService;
-        this.authenticationFacade = authenticationFacade;
         this.messageSource = messageSource;
     }
 
-    @PostMapping("")
-    public void postInvitation(
-            @RequestBody InvitationInputModel body,
-            Locale locale
-    ) throws BadRequestException, NotFoundException {
-        try {
-            invitationService.sentInvitation(body.getUsername(), body.getHouseId(), locale);
-        } catch (EntityException e) {
-            throw new BadRequestException(e.getMessage(), e.getMessage());
-        } catch (EntityNotFoundException e) {
-            throw new NotFoundException(e.getMessage(), e.getMessage());
-        }
-    }
-
-    @PutMapping("/houses/{house-id}/users/{username}")
-    public ResponseEntity<InvitationsOutputModel> putInvitation(
-            @PathVariable("house-id") Long houseId,
-            @PathVariable("username") String username,
-            @RequestBody Boolean accept,
-            Locale locale
+    @GetMapping("/users/{username}")
+    public ResponseEntity<InvitationsOutputModel> getInvitations(
+            @PathVariable("username") String username
     ) throws BadRequestException, NotFoundException {
         List<Invitation> invitations;
         try {
-            if (accept) {
-                invitationService.acceptInvitation(username, houseId, locale);
-            } else {
-                invitationService.declineInvitation(username, houseId, locale);
-            }
-            invitations = invitationService.getReceivedInvitationsByUserUsername(authenticationFacade.getAuthentication().getName(), locale);
+            invitations = invitationService.getSentInvitationsByUserUsername(username);
         } catch (EntityException e) {
             throw new BadRequestException(e.getMessage(), e.getMessage());
         } catch (EntityNotFoundException e) {
@@ -76,4 +46,45 @@ public class InvitationController {
                 HttpStatus.OK);
     }
 
+    @PostMapping("/houses/{house-id}")
+    public ResponseEntity postInvitation(
+            @PathVariable("house-id") long houseId,
+            @RequestBody String username,
+            Locale locale
+    ) throws BadRequestException, NotFoundException, ConflictException, ForbiddenException {
+        try {
+            invitationService.sentInvitation(username, houseId, locale);
+        } catch (EntityException e) {
+            throw new BadRequestException(e.getMessage(), e.getMessage());
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundException(e.getMessage(), e.getMessage());
+        } catch (EntityAlreadyExistsException e) {
+            throw new ConflictException(e.getMessage(), e.getMessage());
+        } catch (InsufficientPrivilegesException e) {
+            throw new ForbiddenException(e.getMessage(), e.getMessage());
+        }
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/houses/{house-id}/users/{username}")
+    public ResponseEntity putInvitation(
+            @PathVariable("house-id") Long houseId,
+            @PathVariable("username") String username,
+            @RequestBody Boolean accept,
+            Locale locale
+    ) throws BadRequestException, NotFoundException {
+        if (accept == null)
+            throw new BadRequestException(ACCEPT_ERROR_MSG, messageSource.getMessage("body_Error_Msg", null, locale));
+        try {
+            if (accept)
+                invitationService.acceptInvitation(username, houseId, locale);
+            else
+                invitationService.declineInvitation(username, houseId, locale);
+        } catch (EntityException e) {
+            throw new BadRequestException(e.getMessage(), e.getMessage());
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundException(e.getMessage(), e.getMessage());
+        }
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
