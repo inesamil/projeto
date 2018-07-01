31c75a60e9339ae16cf0dc4d9f9d5b68681f8ae7
nveloso <42181@alunos.isel.ipl.pt>
2018-07-01 15:17:46 +0100

    Update inserts

--- BD/inserts _pt.sql
+++ BD/inserts _pt.sql
@@ -18,8 +18,8 @@ INSERT INTO public.role(role_name) VALUES ('ROLE_USER');
 
 --USERS
 INSERT INTO public.users(users_username, users_email, users_age, users_name, users_password) VALUES ('pedro', 'pedro@example.com', 21, 'Pedro Oliveira', '$2a$04$zPaKKoIxTHUcNCUXk0/.VeqQ.lrYmd1jDF.5fVdqhoDlLwuzClpva');
-INSERT INTO public.users(users_username, users_email, users_age, users_name, users_password) VALUES ('alice', 'alice@example.com', 21, 'Alice Oliveira', '$2a$04$zPaKKoIxTHUcNCUXk0/.VeqQ.lrYmd1jDF.5fVdqhoDlLwuzClpva');
-INSERT INTO public.users(users_username, users_email, users_age, users_name, users_password) VALUES ('xavier', 'xavier@example.com', 21, 'Xavier Santos', '$2a$04$zPaKKoIxTHUcNCUXk0/.VeqQ.lrYmd1jDF.5fVdqhoDlLwuzClpva');
+INSERT INTO public.users(users_username, users_email, users_age, users_name, users_password) VALUES ('alice', 'alice@example.com', 21, 'Alice Oliveira', '$2a$04$L0KJGuuVT7zCfrX8FQg5XeuZxaCz9MMBlO67iwwPiEyD2J3J4cOKG');
+INSERT INTO public.users(users_username, users_email, users_age, users_name, users_password) VALUES ('xavier', 'xavier@example.com', 21, 'Xavier Santos', '$2a$04$/bbnfYLs0szIUgHSsF.ixumynyCAUiCWLsablvd0E7Kpn4G2eEjUa');
 
 --USER-HOUSE
 INSERT INTO public.userhouse(house_id, users_id, userhouse_administrator) VALUES (1, 1, true);
