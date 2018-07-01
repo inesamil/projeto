deff70f67862736db82174484538e0140bf9fc22
isel42162 <42162@alunos.isel.ipl.pt>
2018-07-01 23:39:12 +0100

    Add removeElement to ArrayUtils

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/ArrayUtils.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/utils/ArrayUtils.kt
@@ -13,3 +13,9 @@ inline fun <reified E> Array<E>.addElement(elem: E) : Array<E>{
     list.add(elem)
     return list.toTypedArray()
 }
+
+inline fun <reified E> Array<E>.removeElement(elem: E) : Array<E>{
+    val list = toMutableList()
+    list.remove(elem)
+    return list.toTypedArray()
+}
