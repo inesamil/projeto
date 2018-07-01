9d7d2c426f6c7c4aa717a1a3112a1fad6d44a880
isel42162 <42162@alunos.isel.ipl.pt>
2018-07-01 23:37:50 +0100

    Add InvitationBody

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/body/InvitationBody.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/model/body/InvitationBody.kt
@@ -1,4 +1,11 @@
 package ps.leic.isel.pt.gis.model.body
 
-class Invitationbody {
-}
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class InvitationBody1(
+        @field:JsonProperty(value = "user-username") val username: String
+)
+
+data class InvitationBody2(
+        @field:JsonProperty(value = "accept") val accept: Boolean
+)
