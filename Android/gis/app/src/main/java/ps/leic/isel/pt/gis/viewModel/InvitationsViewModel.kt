6e62d793e3b7b33b161af27886fc74775c310d13
isel42162 <42162@alunos.isel.ipl.pt>
2018-07-01 23:39:29 +0100

    Update InvitationsViewModel

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/InvitationsViewModel.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/viewModel/InvitationsViewModel.kt
@@ -4,7 +4,11 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import ps.leic.isel.pt.gis.ServiceLocator
+import ps.leic.isel.pt.gis.model.body.InvitationBody1
+import ps.leic.isel.pt.gis.model.body.InvitationBody2
 import ps.leic.isel.pt.gis.model.dtos.ErrorDto
+import ps.leic.isel.pt.gis.model.dtos.HouseDto
+import ps.leic.isel.pt.gis.model.dtos.InvitationDto
 import ps.leic.isel.pt.gis.model.dtos.InvitationsDto
 import ps.leic.isel.pt.gis.repositories.Resource
 
@@ -23,6 +27,35 @@ class InvitationsViewModel(private val app: Application) : AndroidViewModel(app)
 
     fun getInvitations(): LiveData<Resource<InvitationsDto, ErrorDto>>? {
         return invitations
+    }
+
+    fun sendInvitation(from_house: HouseDto, to_username: String) : LiveData<Resource<Any, ErrorDto>>? {
+        from_house.actions.sendInvitation?.let {
+            val invitationBody = InvitationBody1(to_username)
+            return ServiceLocator.getRepository(app.applicationContext)
+                    .create(Any::class.java, ErrorDto::class.java, it.url, it.contentType, invitationBody, TAG)
+        }
+        return null
+    }
+
+    fun acceptInvitation(invitation: InvitationDto) : LiveData<Resource<Any, ErrorDto>>? {
+        invitation.actions.updateInvitation?.let {
+            val invitationBody = InvitationBody2(true)
+            return ServiceLocator.getRepository(app.applicationContext)
+                    .update(Any::class.java, ErrorDto::class.java, it.url, it.contentType, invitationBody, TAG)
+
+        }
+        return null
+    }
+
+    fun declineInvitation(invitation: InvitationDto) : LiveData<Resource<Any, ErrorDto>>? {
+        invitation.actions.updateInvitation?.let {
+            val invitationBody = InvitationBody2(false)
+            return ServiceLocator.getRepository(app.applicationContext)
+                    .update(Any::class.java, ErrorDto::class.java, it.url, it.contentType, invitationBody, TAG)
+
+        }
+        return null
     }
 
     fun cancel() {
