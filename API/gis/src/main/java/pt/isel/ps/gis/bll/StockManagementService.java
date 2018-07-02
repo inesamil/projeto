a7a65107a2afec9c978ab8856e7fe2750ff9ee3c
isel42162 <42162@alunos.isel.ipl.pt>
2018-07-02 22:47:52 +0100

    Add StockManagementService

--- API/gis/src/main/java/pt/isel/ps/gis/bll/StockManagementService.java
+++ API/gis/src/main/java/pt/isel/ps/gis/bll/StockManagementService.java
@@ -1,4 +1,16 @@
 package pt.isel.ps.gis.bll;
 
 public interface StockManagementService {
+
+    /**
+     * Aplicar o algoritmo para o produto com ID @productId
+     *
+     * @param productId identificador do produto
+     */
+    void processOneItem(int productId);
+
+    /**
+     * Aplicar o algoritmo para todos os itens
+     */
+    void processAllItems();
 }
