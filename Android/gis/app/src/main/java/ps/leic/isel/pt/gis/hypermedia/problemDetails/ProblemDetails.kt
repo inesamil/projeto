ccfd5b69cf33ae43802373b7ef49015175e12217
nveloso <42181@alunos.isel.ipl.pt>
2018-07-02 17:42:28 +0100

    Update problem details in android

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/hypermedia/problemDetails/ProblemDetails.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/hypermedia/problemDetails/ProblemDetails.kt
@@ -3,11 +3,11 @@ package ps.leic.isel.pt.gis.hypermedia.problemDetails
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class ProblemDetails (
+class ProblemDetails(
         val type: String,
         val title: String,
         val status: Int,
         val detail: String,
-        val instance: String,
+        val instance: String?,
         val userFriendlyMessage: String
 )
