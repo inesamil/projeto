b831f5bf0a96e7585179fa48d67f0768e66020d1
isel42162 <42162@alunos.isel.ipl.pt>
2018-07-03 00:09:49 +0100

    Update HttpWebServiceImpl

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/httpRequest/HttpWebServiceImpl.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/httpRequest/HttpWebServiceImpl.kt
@@ -3,6 +3,8 @@ package ps.leic.isel.pt.gis.httpRequest
 import android.content.Context
 import com.android.volley.*
 import ps.leic.isel.pt.gis.R
+import ps.leic.isel.pt.gis.hypermedia.problemDetails.ProblemDetails
+import ps.leic.isel.pt.gis.model.dtos.ErrorDto
 import ps.leic.isel.pt.gis.repositories.Resource
 import ps.leic.isel.pt.gis.volley.RequestQueue
 import ps.leic.isel.pt.gis.volley.Requester
@@ -38,9 +40,18 @@ class HttpWebServiceImpl(private val applicationContext: Context) : HttpWebServi
 
     private fun <T, E> onVolleyError(error: VolleyError?, onError: (Resource<T, E>) -> Unit, errorType: Class<E>) {
         val resource: Resource<T, E> = when (error) {
-            is ClientError -> Resource.apiError(Requester.mapper.readValue(error.networkResponse.data, errorType))
-        // Unauthorized e forbidden é auth failure
-            is AuthFailureError -> Resource.error("") // TODO o que fazer se as credenciais estiverem erradas ou se forem expiradas? lancar a activity para fazer login?
+            is ClientError -> {
+                val problemDetails = Requester.mapper.readValue(error.networkResponse.data, ProblemDetails::class.java)
+                val constructor = errorType.getConstructor(ProblemDetails::class.java)
+                val errorDto = constructor.newInstance(problemDetails)
+                Resource.apiError(errorDto)
+            }
+            // Unauthorized e forbidden é auth failure
+            is AuthFailureError -> {
+                val constructor = errorType.getConstructor(String::class.java, Int::class.java, String::class.java, String::class.java)
+                val errorDto = constructor.newInstance("Unauthorized", 401, "Wrong credentials", applicationContext.getString(R.string.wrong_credentials))
+                Resource.apiError(errorDto)
+            }
             is ParseError -> Resource.error("") // TODO ver o que fazer quando da erro no parse da resposta do pedido
             is NetworkError, is NoConnectionError -> Resource.error(applicationContext.getString(R.string.network_error_has_occurred))
             is ServerError, is TimeoutError -> Resource.error(applicationContext.getString(R.string.could_not_connect_to_server))
