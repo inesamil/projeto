8d151d011f77a3df70a0428fff444db8eab8e4cd
isel42162 <42162@alunos.isel.ipl.pt>
2018-07-03 00:56:51 +0100

    Update SpashScreenActivity

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/SplashScreenActivity.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/SplashScreenActivity.kt
@@ -13,15 +13,22 @@ import android.widget.Toast
 import ps.leic.isel.pt.gis.GisApplication
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.ServiceLocator
+import ps.leic.isel.pt.gis.model.dtos.ErrorDto
 import ps.leic.isel.pt.gis.model.dtos.IndexDto
 import ps.leic.isel.pt.gis.repositories.Status
+import ps.leic.isel.pt.gis.stores.CredentialsStore
+import ps.leic.isel.pt.gis.utils.State
 import ps.leic.isel.pt.gis.viewModel.SplashScreenViewModel
+import ps.leic.isel.pt.gis.viewModel.UserViewModel
 import java.lang.Thread.sleep
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private var firstTime: Boolean? = null
     private lateinit var splashScreenViewModel: SplashScreenViewModel
+    private var userViewModel: UserViewModel? = null
+
+    private var retry: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,69 +42,51 @@ class SplashScreenActivity : AppCompatActivity() {
 
         splashScreenViewModel = ViewModelProviders.of(this).get(SplashScreenViewModel::class.java)
         splashScreenViewModel.init()
-        splashScreenViewModel.getIndex()?.observe(this, Observer {
-            if (it?.status == Status.SUCCESS)
-                onSuccess(it.data!!)
-            else if (it?.status == Status.ERROR)
-                onError(it.message)
-        })
+        getIndex()
+
     }
 
     override fun onResume() {
         super.onResume()
 
-        val gisApplication = application as GisApplication
-        if (!gisApplication.isNetworkAvailable()) {
-            val handler = Handler()
-            val run = Runnable {
-                finish()
+        if (retry < MAX_RETRY) {
+            val gisApplication = application as GisApplication
+            if (!gisApplication.isNetworkAvailable()) {
+                val handler = Handler()
+                val run = Runnable {
+                    ++retry
+                    getIndex()
+                }
+                handler.postDelayed(run, 3000)
+                return
             }
-            handler.postDelayed(run, 3000)
-            return
         }
-
-        splashScreenViewModel = ViewModelProviders.of(this).get(SplashScreenViewModel::class.java)
-        splashScreenViewModel.init()
-        splashScreenViewModel.getIndex()?.observe(this, Observer {
-            if (it?.status == Status.SUCCESS)
-                onSuccess(it.data!!)
-            else if (it?.status == Status.ERROR)
-                onError(it.message)
-        })
     }
 
-    private fun onSuccess(index: IndexDto) {
-        val gisApplication = application as GisApplication
-        gisApplication.index = index
+    private fun onSuccess(index: IndexDto?) {
+        index?.let {
+            val gisApplication = application as GisApplication
+            gisApplication.index = it
 
-        if (isFirstTime()) {
-            // First time using the app
-            Log.i(TAG, "First time using the app.")
-            val mPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-            mPreferences.edit().putBoolean(FIRST_TIME_TAG, false).apply()
-            finish()
-            startActivity(Intent(this, RegisterActivity::class.java))
-        } else {
-            // Already used the app
-            Log.d(TAG, "Try to retrieve credentials.")
-            val username = ServiceLocator.getCredentialsStore(applicationContext).getUsername()
-            username?.let {
-                Log.d(TAG, "Credentials retrieved.")
-                if (true) {   //TODO: validar credenciais
-                    // Valid Credentials
-                    Log.i(TAG, "Retrieved valid credentials, so logged in user.")
-                    finish()
-                    startActivity(Intent(this, HomeActivity::class.java))
-                    return
-                } else {
-                    // Invalid Credentials
-                    Log.d(TAG, "Retrieved invalid credentials, so delete retrieved credential.")
-                    ServiceLocator.getCredentialsStore(applicationContext).deleteCredentials()
+            if (isFirstTime()) {
+                // First time using the app
+                Log.i(TAG, "First time using the app.")
+                val mPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+                mPreferences.edit().putBoolean(FIRST_TIME_TAG, false).apply()
+                finish()
+                startActivity(Intent(this, RegisterActivity::class.java))
+            } else {
+                // Already used the app
+                Log.d(TAG, "Try to retrieve credentials.")
+                val credentials = ServiceLocator.getCredentialsStore(applicationContext).getCredentials()
+                credentials?.let {
+                    Log.d(TAG, "Credentials retrieved.")
+                    validateCredentials(credentials)
                 }
+                // No credentials retrieved
+                finish()
+                startActivity(Intent(this, LoginActivity::class.java))
             }
-            // No credentials retrieved
-            finish()
-            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 
@@ -106,7 +95,59 @@ class SplashScreenActivity : AppCompatActivity() {
             Log.i(TAG, it)
         }
         Toast.makeText(this, getString(R.string.could_not_connect_to_server), Toast.LENGTH_SHORT).show()
-        finish()
+        if (retry >= MAX_RETRY)
+            finish()
+    }
+
+    private fun getIndex() {
+        splashScreenViewModel.getIndex()?.observe(this, Observer {
+            when (it?.status) {
+                Status.SUCCESS -> onSuccess(it.data)
+                Status.UNSUCCESS -> onUnsuccess(it.apiError)
+                Status.ERROR -> onError(it.message)
+            }
+        })
+    }
+
+    private fun validateCredentials(credentials: CredentialsStore.Credentials) {
+        val gisApplication = application as GisApplication
+        gisApplication.index
+
+        val url = gisApplication.index.getUserUrl(credentials.username)
+
+        url?.let {
+            userViewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
+            userViewModel?.init(it)
+            userViewModel?.getUser()?.observe(this, Observer {
+                when (it?.status) {
+                    Status.SUCCESS -> {
+                        // Valid Credentials
+                        Log.i(TAG, "Retrieved valid credentials, so logged in user.")
+                        finish()
+                        startActivity(Intent(this, HomeActivity::class.java))
+                    }
+                    Status.UNSUCCESS -> {
+                        // Invalid Credentials
+                        Log.d(TAG, "Retrieved invalid credentials, so delete retrieved credential.")
+                        ServiceLocator.getCredentialsStore(applicationContext).deleteCredentials()
+                        onUnsuccess(it.apiError)
+                    }
+                    Status.ERROR -> {
+                        // Invalid Credentials
+                        Log.d(TAG, "Retrieved invalid credentials, so delete retrieved credential.")
+                        ServiceLocator.getCredentialsStore(applicationContext).deleteCredentials()
+                        onError(it.message)
+                    }
+                }
+            })
+        }
+    }
+
+    private fun onUnsuccess(error: ErrorDto?) {
+        error?.let {
+            Log.e(TAG, it.developerErrorMessage)
+            onError(it.message)
+        }
     }
 
     private fun isFirstTime(): Boolean {
@@ -120,5 +161,6 @@ class SplashScreenActivity : AppCompatActivity() {
     companion object {
         private const val TAG: String = "SplashScreenActivity"
         private const val FIRST_TIME_TAG = "first_time"
+        private const val MAX_RETRY = 1
     }
 }
