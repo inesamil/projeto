5e996842f9c34de25e150560b1284e6b2060c628
nveloso <42181@alunos.isel.ipl.pt>
2018-07-05 12:07:48 +0100

    Update repository impl

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/RepositoryImpl.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/repositories/RepositoryImpl.kt
@@ -12,70 +12,82 @@ class RepositoryImpl(private val applicationContext: Context) : Repository {
     override fun <T, E> create(dtoType: Class<T>, errorType: Class<E>, url: String, contentType: String?, body: Any?, tag: String): LiveData<Resource<T, E>> {
         val data = MutableLiveData<Resource<T, E>>()
         data.value = Resource.loading()
-
-        val credentials = ServiceLocator.getCredentialsStore(applicationContext).getCredentials()
-
-        val headers = mutableMapOf<String, String>()
-        HeadersUtils.setContentTypeHeader(headers, contentType)
-        credentials?.let {
-            HeadersUtils.setAuthorizationHeader(headers, it.username, it.password)
-        }
-
-        ServiceLocator.getWebService(applicationContext).fetch(
-                HttpWebService.Method.POST, url, body, headers, dtoType, errorType,
-                { data.value = it }, tag)
+        ServiceLocator.getSmartLock(applicationContext).retrieveCredentials(null, {
+            val headers = mutableMapOf<String, String>()
+            HeadersUtils.setContentTypeHeader(headers, contentType)
+            val password = it.password
+            if (password == null) {
+                data.value = Resource.error("Password can not be null.")// TODO mensagem
+                return@retrieveCredentials
+            }
+            HeadersUtils.setAuthorizationHeader(headers, it.id, password)
+            ServiceLocator.getWebService(applicationContext).fetch(
+                    HttpWebService.Method.POST, url, body, headers, dtoType, errorType,
+                    { data.value = it }, tag)
+        }, {
+            data.value = Resource.error("Cannot retrieve credentials")// TODO mensagem
+        })
         return data
     }
 
     override fun <T, E> get(dtoType: Class<T>, errorType: Class<E>, url: String, tag: String): LiveData<Resource<T, E>> {
         val data = MutableLiveData<Resource<T, E>>()
         data.value = Resource.loading()
-
-        val credentials = ServiceLocator.getCredentialsStore(applicationContext).getCredentials()
-
-        val headers = mutableMapOf<String, String>()
-        credentials?.let {
-            HeadersUtils.setAuthorizationHeader(headers, it.username, it.password)
-        }
-
-        ServiceLocator.getWebService(applicationContext).fetch(
-                HttpWebService.Method.GET, url, null, headers, dtoType, errorType,
-                { data.value = it }, tag)
+        ServiceLocator.getSmartLock(applicationContext).retrieveCredentials(null, {
+            val headers = mutableMapOf<String, String>()
+            val password = it.password
+            if (password == null) {
+                data.value = Resource.error("Password can not be null.")// TODO mensagem
+                return@retrieveCredentials
+            }
+            HeadersUtils.setAuthorizationHeader(headers, it.id, password)
+            ServiceLocator.getWebService(applicationContext).fetch(
+                    HttpWebService.Method.GET, url, null, headers, dtoType, errorType,
+                    { data.value = it }, tag)
+        }, {
+            data.value = Resource.error("Cannot retrieve credentials")// TODO mensagem
+        })
         return data
     }
 
     override fun <T, E> update(dtoType: Class<T>, errorType: Class<E>, url: String, contentType: String?, body: Any?, tag: String): LiveData<Resource<T, E>> {
         val data = MutableLiveData<Resource<T, E>>()
         data.value = Resource.loading()
-
-        val credentials = ServiceLocator.getCredentialsStore(applicationContext).getCredentials()
-
-        val headers = mutableMapOf<String, String>()
-        HeadersUtils.setContentTypeHeader(headers, contentType)
-        credentials?.let {
-            HeadersUtils.setAuthorizationHeader(headers, it.username, it.password)
-        }
-
-        ServiceLocator.getWebService(applicationContext).fetch(
-                HttpWebService.Method.PUT, url, body, headers, dtoType, errorType,
-                { data.value = it }, tag)
+        ServiceLocator.getSmartLock(applicationContext).retrieveCredentials(null, {
+            val headers = mutableMapOf<String, String>()
+            HeadersUtils.setContentTypeHeader(headers, contentType)
+            val password = it.password
+            if (password == null) {
+                data.value = Resource.error("Password can not be null.")// TODO mensagem
+                return@retrieveCredentials
+            }
+            HeadersUtils.setAuthorizationHeader(headers, it.id, password)
+            ServiceLocator.getWebService(applicationContext).fetch(
+                    HttpWebService.Method.PUT, url, body, headers, dtoType, errorType,
+                    { data.value = it }, tag)
+        }, {
+            data.value = Resource.error("Cannot retrieve credentials")// TODO mensagem
+        })
         return data
     }
 
     override fun <T, E> delete(dtoType: Class<T>, errorType: Class<E>, url: String, tag: String): LiveData<Resource<T, E>> {
         val data = MutableLiveData<Resource<T, E>>()
         data.value = Resource.loading()
-
-        val credentials = ServiceLocator.getCredentialsStore(applicationContext).getCredentials()
-
-        val headers = mutableMapOf<String, String>()
-        credentials?.let {
-            HeadersUtils.setAuthorizationHeader(headers, it.username, it.password)
-        }
-
-        ServiceLocator.getWebService(applicationContext).fetch(
-                HttpWebService.Method.DELETE, url, null, headers, dtoType, errorType,
-                { data.value = it }, tag)
+        ServiceLocator.getSmartLock(applicationContext).retrieveCredentials(null, {
+            val headers = mutableMapOf<String, String>()
+            val password = it.password
+            if (password == null) {
+                data.value = Resource.error("Password can not be null.")// TODO mensagem
+                return@retrieveCredentials
+            }
+            HeadersUtils.setAuthorizationHeader(headers, it.id, password)
+            ServiceLocator.getWebService(applicationContext).fetch(
+                    HttpWebService.Method.DELETE, url, null, headers, dtoType, errorType,
+                    { data.value = it }, tag)
+        }, {
+            data.value = Resource.error("Cannot retrieve credentials")// TODO mensagem
+        })
         return data
     }
 
