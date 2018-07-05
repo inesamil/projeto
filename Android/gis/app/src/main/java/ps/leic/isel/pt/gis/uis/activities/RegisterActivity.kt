6b44207b7a322b9716e8e8a69fa44d04fd4601ea
nveloso <42181@alunos.isel.ipl.pt>
2018-07-05 11:30:56 +0100

    Update register activity

--- Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/RegisterActivity.kt
+++ Android/gis/app/src/main/java/ps/leic/isel/pt/gis/uis/activities/RegisterActivity.kt
@@ -8,13 +8,11 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import com.google.android.gms.auth.api.credentials.Credential
 import kotlinx.android.synthetic.main.activity_register.*
 import ps.leic.isel.pt.gis.R
 import ps.leic.isel.pt.gis.ServiceLocator
 import ps.leic.isel.pt.gis.model.body.UserBody
 import ps.leic.isel.pt.gis.model.dtos.ErrorDto
-import ps.leic.isel.pt.gis.model.dtos.UserDto
 import ps.leic.isel.pt.gis.repositories.Status
 import ps.leic.isel.pt.gis.stores.SmartLock
 import ps.leic.isel.pt.gis.viewModel.UsersViewModel
@@ -43,13 +41,7 @@ class RegisterActivity : AppCompatActivity() {
             } else {
                 Log.e(TAG, "SAVE: Canceled by user")
             }
-            val name = fullnameEditText.text.toString()
-            val username = usernameEditText.text.toString()
-            val email = emailEditText.text.toString()
-            val age = ageEditText.text.toString().toShort()
-            val password = passwordEditText.text.toString()
-            val credential: Credential = data?.getParcelableExtra(Credential.EXTRA_KEY)!!
-            onComplete(credential, username, name, email, age, password)
+            onComplete()
         }
     }
 
@@ -59,44 +51,50 @@ class RegisterActivity : AppCompatActivity() {
         val email = emailEditText.text.toString()
         val age = ageEditText.text.toString().toShort()
         val password = passwordEditText.text.toString()
+        val confirmationPassword = confirmPasswordEditText.text.toString()
 
         if (name.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, getString(R.string.please_fill_in_all_required_fields), Toast.LENGTH_LONG).show()
             return
         }
+        if (password != confirmationPassword) {
+            Toast.makeText(this, "Passwords devem ser iguais", Toast.LENGTH_LONG).show()
+            return
+        }
 
-        ServiceLocator
-                .getSmartLock(applicationContext)
-                .storeCredentials(this,
-                        username,
-                        password,
-                        { onComplete(it, username, name, email, age, password) },
-                        ::onUncomplete
-                )
-    }
-
-    private fun onComplete(credential: Credential, username: String, name: String, email: String, age: Short, password: String) {
         usersViewModel = ViewModelProviders.of(this).get(UsersViewModel::class.java)
         usersViewModel?.addUser(UserBody(username, name, email, age, password))?.observe(this, Observer {
             when {
-                it?.status == Status.SUCCESS -> onSuccess(it.data)
-                it?.status == Status.UNSUCCESS -> onUnsuccess(it.apiError, credential)
-                it?.status == Status.ERROR -> onError(it.message, credential)
+                it?.status == Status.SUCCESS -> onSuccess(username, password)
+                it?.status == Status.UNSUCCESS -> onUnsuccess(it.apiError)
+                it?.status == Status.ERROR -> onError(it.message)
             }
         })
     }
 
-    private fun onUncomplete(exception: Exception?) {
-        // TODO o qe fazer?
-    }
-
-    private fun onSuccess(user: UserDto?) {
+    private fun onComplete() {
         Log.i(TAG, "Register user with success")
         finish()
         startActivity(Intent(this, HomeActivity::class.java))
     }
 
-    private fun onUnsuccess(error: ErrorDto?, credential: Credential) {
+    private fun onUncomplete(exception: Exception?) {
+        Log.i(TAG, "exception")
+        // TODO o qe fazer?
+    }
+
+    private fun onSuccess(username: String, password: String) {
+        ServiceLocator
+                .getSmartLock(applicationContext)
+                .storeCredentials(this,
+                        username,
+                        password,
+                        ::onComplete,
+                        ::onUncomplete
+                )
+    }
+
+    private fun onUnsuccess(error: ErrorDto?) {
         error?.let {
             Log.e(TAG, it.developerErrorMessage)
             if (it.statusCode == 401) {
@@ -105,17 +103,14 @@ class RegisterActivity : AppCompatActivity() {
                 Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
             }
         }
-        onError(null, credential)
+        onError(null)
     }
 
-    private fun onError(message: String?, credential: Credential) {
+    private fun onError(message: String?) {
         message?.let {
             Log.e(LoginActivity.TAG, it)
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
-        ServiceLocator
-                .getSmartLock(applicationContext)
-                .deleteCredentials(credential)
     }
 
     companion object {
