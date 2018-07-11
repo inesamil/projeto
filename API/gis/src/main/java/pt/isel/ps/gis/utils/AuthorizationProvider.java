af4f3211be5dcde1006ada58c82763f4a7e073f4
nveloso <42181@alunos.isel.ipl.pt>
2018-07-11 12:58:11 +0100

    Remove locale

--- API/gis/src/main/java/pt/isel/ps/gis/utils/AuthorizationProvider.java
+++ API/gis/src/main/java/pt/isel/ps/gis/utils/AuthorizationProvider.java
@@ -3,5 +3,6 @@ package pt.isel.ps.gis.utils;
 import pt.isel.ps.gis.exceptions.InsufficientPrivilegesException;
 
 public interface AuthorizationProvider {
+
     void checkUserAuthorizationToAccessHouse(String username, long houseId) throws InsufficientPrivilegesException;
 }
--- API/gis/src/main/java/pt/isel/ps/gis/utils/AuthorizationProviderImpl.java
+++ API/gis/src/main/java/pt/isel/ps/gis/utils/AuthorizationProviderImpl.java
@@ -3,17 +3,12 @@ package pt.isel.ps.gis.utils;
 import pt.isel.ps.gis.dal.repositories.UserHouseRepository;
 import pt.isel.ps.gis.exceptions.InsufficientPrivilegesException;
 
-import java.util.Locale;
-
 public class AuthorizationProviderImpl implements AuthorizationProvider {
 
     private final UserHouseRepository userHouseRepository;
 
-    private final Locale locale;
-
-    public AuthorizationProviderImpl(UserHouseRepository userHouseRepository, Locale locale) {
+    public AuthorizationProviderImpl(UserHouseRepository userHouseRepository) {
         this.userHouseRepository = userHouseRepository;
-        this.locale = locale;
     }
 
     @Override
